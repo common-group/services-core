3aefde71edf12441a030e4e52f4628e3ab88d235
Vinícius Andrade <vchavesster@gmail.com>
2015-06-15 17:12:46 -0300

    Starts specs by implementing Address model specs

--- gruntfile.js
+++ gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function( grunt ) {
       full: {
         src: "src/**/*.js",
         options: {
-          specs: "spec/*[S|s]pec.js",
+          specs: "spec/**/*[S|s]pec.js",
           vendor: [
             "spec/lib/jasmine-species/jasmine-grammar.js",
             "bower_components/**/*.js"
