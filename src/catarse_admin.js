170471ab0c5ea31aee9ea41a6ba8a844ecde29f5
Vinícius Andrade <vchavesster@gmail.com>
2015-06-23 01:27:34 -0300

    Initializes m.postgrest

--- src/catarse_admin.js
+++ src/catarse_admin.js
@@ -1,7 +1,9 @@
 var adminApp = window.adminApp = {};
 
+m.postgrest.init("http://api.catarse.me/", {method: "GET", url: "/api_token"});
+
 adminApp.models = {};
 
 adminApp.submodule = function(module, args) {
   return module.view.bind(this, new module.controller(args))
-} 
+};
