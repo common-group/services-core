db665002aceaa816d26feaa0cbbb86fa51b0bda9
Vinícius Andrade <vchavesster@gmail.com>
2015-06-23 18:02:51 -0300

    Updates dist

--- dist/catarse_admin.js
+++ dist/catarse_admin.js
@@ -8,19 +8,59 @@ var adminApp = window.adminApp = {};
 
 adminApp.models = {}, adminApp.submodule = function(module, args) {
     return module.view.bind(this, new module.controller(args));
-}, adminApp.AdminContributions = {
+};
+
+var ContributionDetail = function(data) {
+    data = data || {}, this.id = m.prop(data.id), this.contribution_id = m.prop(data.contribution_id), 
+    this.user_id = m.prop(data.user_id), this.project_id = m.prop(data.project_id), 
+    this.reward_id = m.prop(data.reward_id), this.payment_id = m.prop(data.payment_id), 
+    this.permalink = m.prop(data.permalink), this.project_name = m.prop(data.project_name), 
+    this.user_name = m.prop(data.user_name), this.email = m.prop(data.email), this.uploaded_image = m.prop(data.uploaded_image), 
+    this.key = m.prop(data.key), this.value = m.prop(data.value), this.installments = m.prop(data.installments), 
+    this.installment_value = m.prop(data.installment_value), this.state = m.prop(data.state), 
+    this.anonymous = m.prop(data.anonymous), this.payer_email = m.prop(data.payer_email), 
+    this.gateway = m.prop(data.gateway), this.gateway_id = m.prop(data.gateway_id), 
+    this.gateway_fee = m.prop(data.gateway_fee), this.gateway_data = m.prop(data.gateway_data), 
+    this.payment_method = m.prop(data.payment_method), this.project_state = m.prop(data.project_state), 
+    this.has_rewards = m.prop(data.has_rewards), this.pending_at = m.prop(data.pending_at), 
+    this.paid_at = m.prop(data.paid_at), this.refused_at = m.prop(data.refused_at), 
+    this.pending_refund_at = m.prop(data.pending_refund_at), this.refunded_at = m.prop(data.refunded_at), 
+    this.created_at = m.prop(data.created_at);
+};
+
+ContributionDetail.get = function(filters, page) {
+    var toRange = function() {
+        var pageSize = 10;
+        return page * pageSize + "-" + (page * pageSize + pageSize);
+    }, xhrConfig = function(xhr) {
+        xhr.setHeader("Range-unit", "items"), xhr.setHeader("Range", toRange());
+    };
+    return filters = filters || {}, m.postgrest.requestWithToken({
+        method: "GET",
+        url: "/contribution_details",
+        config: xhrConfig
+    });
+}, adminApp.models.ContributionDetail = ContributionDetail, adminApp.AdminContributions = {
     controller: function() {
-        var that = this;
-        this.filterContributions = function(filter) {
-            that.contributions = adminApp.models.ContributionDetail.get(filter);
-        }, this.filterContributions();
+        var vm = this.vm = adminApp.AdminContributions.VM;
+        this.filterContributions = function(filters) {
+            vm.filter(filters);
+        }, this.nextPage = function() {
+            vm.nextPage();
+        };
     },
     view: function(ctrl) {
         return [ m.component(adminApp.AdminContributionsFilter, {
             onFilter: ctrl.filterContributions
         }), m(".w-section.section", [ m.component(adminApp.AdminContributionsList, {
-            contributions: ctrl.contributions
-        }) ]), m(".w-section.section", [ m(".w-container", [ m(".w-row", [ m(".w-col.w-col-5"), m(".w-col.w-col-2", [ m("a.btn.btn-medium.btn-terciary[href='#']", "Carregar mais") ]), m(".w-col.w-col-5") ]) ]) ]) ];
+            contributions: ctrl.vm.contributions()
+        }) ]), m(".w-section.section", [ m(".w-container", [ m(".w-row", [ m(".w-col.w-col-5"), m(".w-col.w-col-2", [ ctrl.vm.isLoading ? m("button.btn.btn-medium.btn-terciary[href='#']", {
+            onclick: ctrl.filter
+        }, "Carregar mais") : m("img[alt='Loader'][src='/assets/catarse_bootstrap/loader-eff2ad1eeb09a19c9afb5b143e1dd62b.gif']", {
+            style: {
+                display: " none"
+            }
+        }) ]), m(".w-col.w-col-5") ]) ]) ]) ];
     }
 }, adminApp.AdminContributionsFilter = {
     controller: function(args) {
@@ -33,7 +73,7 @@ adminApp.models = {}, adminApp.submodule = function(module, args) {
         return m("#admin-contributions-filter.w-section.page-header", [ m(".w-container", [ m(".fontsize-larger.u-text-center.u-marginbottom-30", "Apoios"), m(".w-form", [ m("form[data-name='Email Form'][id='email-form'][name='email-form']", [ m(".w-row.u-marginbottom-20", [ m(".w-col.w-col-2", [ m("label.fontsize-small[for='permalink']", "Permalink"), m("input.w-input.text-field.positive[id='permalink'][name='permalink'][placeholder='permalink do projeto'][required='required'][type='text']", {
             onchange: m.withAttr("value", ctrl.vm.permalink),
             value: ctrl.vm.permalink()
-        }) ]), m(".w-col.w-col-4", [ m("label.fontsize-small[for='expiration']", "Expiram entre"), m("input.w-input.text-field.positive[data-name='Field 2'][id='expiration'][name='expiration'][placeholder='Expiram entre'][required='required'][type='text']") ]), m(".w-col.w-col-2", [ m("label.fontsize-small[for='progress']", "Por progresso %"), m("input.w-input.text-field.positive[data-name='Field 2'][id='progress'][name='progress'][placeholder='Progresso em %'][required='required'][type='text']") ]), m(".w-col.w-col-2", [ m("label.fontsize-small[for='field-3']", "Com o estado"), m("select.w-select.text-field.positive[id='field-3'][name='field-3']", [ m("option[value='']", "Select one..."), "\n", m("option[value='First']", "First Choice"), "\n", m("option[value='Second']", "Second Choice"), "\n", m("option[value='Third']", "Third Choice") ]) ]), m(".w-col.w-col-2", [ m("label.fontsize-small[for='field-3']", "Meio de pag."), m("select.w-select.text-field.positive[id='field-3'][name='field-3']", [ m("option[value='']", "Select one..."), "\n", m("option[value='First']", "First Choice"), "\n", m("option[value='Second']", "Second Choice"), "\n", m("option[value='Third']", "Third Choice") ]) ]) ]) ]), m(".w-form-done", [ m("p", "Thank you! Your submission has been received!") ]), m(".w-form-fail", [ m("p", "Oops! Something went wrong while submitting the form :(") ]) ]), m(".w-row", [ m(".w-col.w-col-4"), m(".w-col.w-col-4", [ m("button.btn.btn-small", {
+        }) ]), m(".w-col.w-col-4", [ m("label.fontsize-small[for='expiration']", "Expiram entre"), m("input.w-input.text-field.positive[data-name='Field 2'][id='expiration'][name='expiration'][placeholder='Expiram entre'][required='required'][type='text']") ]), m(".w-col.w-col-2", [ m("label.fontsize-small[for='progress']", "Por progresso %"), m("input.w-input.text-field.positive[data-name='Field 2'][id='progress'][name='progress'][placeholder='Progresso em %'][required='required'][type='text']") ]), m(".w-col.w-col-2", [ m("label.fontsize-small[for='field-3']", "Com o estado"), m("select.w-select.text-field.positive[id='field-3'][name='field-3']", [ m("option[value='']", "Select one..."), "\n", m("option[value='First']", "First Choice"), "\n", m("option[value='Second']", "Second Choice"), "\n", m("option[value='Third']", "Third Choice") ]) ]), m(".w-col.w-col-2", [ m("label.fontsize-small[for='field-3']", "Meio de pag."), m("select.w-select.text-field.positive[id='field-3'][name='field-3']", [ m("option[value='']", "Select one..."), "\n", m("option[value='First']", "First Choice"), "\n", m("option[value='Second']", "Second Choice"), "\n", m("option[value='Third']", "Third Choice") ]) ]) ]) ]), m(".w-form-done", [ m("p", "Thank you! Your submission has been received!") ]), m(".w-form-fail", [ m("p", "Oops! Something went wrong while submitting the form :(") ]) ]), m(".w-row", [ m(".w-col.w-col-4"), m(".w-col.w-col-4", [ m("button#filter-btn.btn.btn-small", {
             onclick: ctrl.filter
         }, "Filtrar") ]), m(".w-col.w-col-4") ]) ]) ]);
     }
@@ -51,7 +91,7 @@ adminApp.models = {}, adminApp.submodule = function(module, args) {
     };
 }(), adminApp.AdminContributionsList = {
     controller: function(args) {
-        this.contributions = m.prop(args.contributions || new adminApp.models.Contribution());
+        this.contributions = m.prop(args.contributions || new adminApp.models.ContributionDetail());
     },
     view: function(ctrl, args) {
         return m("#admin-contributions-list.w-container", [ m(".u-marginbottom-30.fontsize-base", [ m("span.fontweight-semibold", "125"), " apoios encontrados, totalizando ", m("span.fontweight-semibold", [ "R$27.090.655,00     ", m("a.fa.fa-download.fontcolor-dashboard[href='#']", ".") ]) ]), ctrl.contributions().map(function(contribution) {
@@ -111,26 +151,26 @@ adminApp.models = {}, adminApp.submodule = function(module, args) {
             }
         }, [ m(".w-form", [ m("form[data-name='Email Form 4'][id='email-form-4'][name='email-form-4']", [ m(".w-radio", [ m("input.w-radio-input[data-name='Radio'][id='radio'][name='radio'][type='radio'][value='Radio']"), m("label.w-form-label[for='radio']", "Anônimo") ]), m(".w-radio", [ m("input.w-radio-input[data-name='Radio 2'][id='radio'][name='radio-2'][type='radio'][value='Radio']"), m("label.w-form-label[for='radio']", "Público") ]) ]), m(".w-form-done", [ m("p", "Thank you! Your submission has been received!") ]), m(".w-form-fail", [ m("p", "Oops! Something went wrong while submitting the form") ]) ]) ]) ]) ]), m(".w-row.card.card-terciary.u-radius", [ m(".w-col.w-col-4", [ m(".fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20", "Detalhes do apoio"), m(".fontsize-smallest.lineheight-looser", [ "Valor: R$80\n", m("br"), "Taxa: R$3,35\n", m("br"), "Recompensa: R$80\n", m("br"), "Anônimo: Não\n", m("br"), "Id pagamento: 638912", m("br"), "Apoio: 54545454", m("br"), "Chave: \n", m("br"), "7809d09d-6325-442e-876e-b9a0846c526f\n", m("br"), "Meio: Pagarme\n", m("br"), "Operadora: STONE", m("br"), m("a.link-hidden[href='#']", "Boleto bancário"), " ", m("span.badge", "2a via") ]) ]), m(".w-col.w-col-4", [ m(".fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20", "Histórico da transação"), m(".w-row.fontsize-smallest.lineheight-looser", [ m(".w-col.w-col-6", [ m(".fontcolor-secondary", "19/05/2015, 01:20 h") ]), m(".w-col.w-col-6", [ m("div", "Apoio criado") ]) ]), m(".w-row.fontsize-smallest.lineheight-looser", [ m(".w-col.w-col-6", [ m(".fontcolor-secondary", "19/05/2015, 01:20 h") ]), m(".w-col.w-col-6", [ m("div", "Apoio criado") ]) ]), m(".w-row.fontsize-smallest.lineheight-looser", [ m(".w-col.w-col-6", [ m(".fontcolor-secondary", "19/05/2015, 01:20 h") ]), m(".w-col.w-col-6", [ m("div", [ m("span.badge.badge-attention.fontsize-smallest", "Estorno realizado") ]) ]) ]), m(".w-row.fontsize-smallest.lineheight-looser", [ m(".w-col.w-col-6", [ m(".fontcolor-secondary", "19/05/2015, 01:20 h") ]), m(".w-col.w-col-6", [ m("div", "Apoio criado") ]) ]), m(".w-row.fontsize-smallest.lineheight-looser", [ m(".w-col.w-col-6", [ m(".fontcolor-secondary", "19/05/2015, 01:20 h") ]), m(".w-col.w-col-6", [ m("div", "Apoio criado") ]) ]), m(".w-row.fontsize-smallest.lineheight-looser", [ m(".w-col.w-col-6", [ m(".fontcolor-secondary", "19/05/2015, 01:20 h") ]), m(".w-col.w-col-6", [ m("div", "Apoio criado"), m(".fontsize-smallest.lineheight-tighter", [ m("span.badge", "Luis Otavio Ribeiro") ]) ]) ]) ]), m(".w-col.w-col-4") ]) ]);
     }
-};
-
-var ContributionDetail = function(data) {
-    data = data || {}, this.id = m.prop(data.id), this.contribution_id = m.prop(data.contribution_id), 
-    this.user_id = m.prop(data.user_id), this.project_id = m.prop(data.project_id), 
-    this.reward_id = m.prop(data.reward_id), this.payment_id = m.prop(data.payment_id), 
-    this.permalink = m.prop(data.permalink), this.project_name = m.prop(data.project_name), 
-    this.user_name = m.prop(data.user_name), this.email = m.prop(data.email), this.uploaded_image = m.prop(data.uploaded_image), 
-    this.key = m.prop(data.key), this.value = m.prop(data.value), this.installments = m.prop(data.installments), 
-    this.installment_value = m.prop(data.installment_value), this.state = m.prop(data.state), 
-    this.anonymous = m.prop(data.anonymous), this.payer_email = m.prop(data.payer_email), 
-    this.gateway = m.prop(data.gateway), this.gateway_id = m.prop(data.gateway_id), 
-    this.gateway_fee = m.prop(data.gateway_fee), this.gateway_data = m.prop(data.gateway_data), 
-    this.payment_method = m.prop(data.payment_method), this.project_state = m.prop(data.project_state), 
-    this.has_rewards = m.prop(data.has_rewards), this.pending_at = m.prop(data.pending_at), 
-    this.paid_at = m.prop(data.paid_at), this.refused_at = m.prop(data.refused_at), 
-    this.pending_refund_at = m.prop(data.pending_refund_at), this.refunded_at = m.prop(data.refunded_at), 
-    this.created_at = m.prop(data.created_at);
-};
-
-ContributionDetail.get = function(filter) {
-    return [];
-}, adminApp.models.ContributionDetail = ContributionDetail;
+}, adminApp.AdminContributions.VM = function() {
+    var contributions = m.prop(""), filters = m.prop(""), isLoading = m.prop(!1), page = m.prop(1), getContributions = function() {
+        return adminApp.models.ContributionDetail.get(filters(), page());
+    }, filter = function(input) {
+        isLoading(!0);
+        var input = input || {}, d = m.deferred();
+        return filters(input), getContributions().then(function(data) {
+            contributions(data), d.resolve(contributions()), isLoading(!1);
+        }), d.promise;
+    }, nextPage = function() {
+        var d = m.deferred();
+        return isLoading(!0), page(page() + 1), getContributions().then(function(data) {
+            d.resolve(contributions(_.union(contributions(), data))), isLoading(!1);
+        }), d.promise;
+    };
+    return filter(), {
+        contributions: contributions,
+        filter: filter,
+        filters: filters,
+        nextPage: nextPage,
+        page: page
+    };
+}();
