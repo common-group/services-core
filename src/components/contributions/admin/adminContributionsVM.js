fc7a633f87cbf4289d7f98e3f17ae93a2bcc5362
Vinícius Andrade <vchavesster@gmail.com>
2015-06-23 21:48:44 -0300

    Refactors AdminContributions View-Model

--- src/components/contributions/admin/adminContributionsVM.js
+++ src/components/contributions/admin/adminContributionsVM.js
@@ -4,35 +4,29 @@ adminApp.AdminContributions.VM = (function(){
       isLoading = m.prop(false),
       page = m.prop(1);
 
-  var getContributions = function(){
-    return adminApp.models.ContributionDetail.get(filters(), page());
+  var fetch = function(){
+    var d = m.deferred();
+    isLoading(true);
+    adminApp.models.ContributionDetail.get(filters(), page())
+      .then(function(data){
+        contributions(_.union(contributions(), data);
+        d.resolve(contributions());
+        isLoading(false);
+      });
+    return d.promise;
   };
 
   var filter = function(input){
-    isLoading(true);
-    var input = input || {},
-        d = m.deferred();
-    filters(input); 
-    getContributions().then(function(data){
-      contributions(data);
-      d.resolve(contributions());
-      isLoading(false);
-    });  
-    return d.promise;
+    filters(input);
+    return fetch();
   };
 
   var nextPage = function(){
-    var d = m.deferred();
-    isLoading(true);
     page(page()+1);
-    getContributions().then(function(data){
-      d.resolve(contributions(_.union(contributions(), data)));
-      isLoading(false);
-    });
-    return d.promise;
+    return fetch();
   };
 
-  filter();
+  fetch();
 
   return {
     contributions: contributions,
