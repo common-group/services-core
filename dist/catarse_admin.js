9d13309fbed175cb102736f28010045568f7f111
Vinícius Andrade <vchavesster@gmail.com>
2015-06-23 22:44:02 -0300

    Fixes isLoading view logic

--- dist/catarse_admin.js
+++ dist/catarse_admin.js
@@ -54,9 +54,9 @@ ContributionDetail.get = function(filters, page) {
             onFilter: ctrl.filterContributions
         }), m(".w-section.section", [ m.component(adminApp.AdminContributionsList, {
             contributions: ctrl.vm.contributions()
-        }) ]), m(".w-section.section", [ m(".w-container", [ m(".w-row", [ m(".w-col.w-col-5"), m(".w-col.w-col-2", [ ctrl.vm.isLoading() ? m("button.btn.btn-medium.btn-terciary[href='#']", {
+        }) ]), m(".w-section.section", [ m(".w-container", [ m(".w-row", [ m(".w-col.w-col-5"), m(".w-col.w-col-2", [ ctrl.vm.isLoading() ? m("img[alt='Loader'][src='/assets/catarse_bootstrap/loader-eff2ad1eeb09a19c9afb5b143e1dd62b.gif']") : m("button.btn.btn-medium.btn-terciary[href='#']", {
             onclick: ctrl.filter
-        }, "Carregar mais") : m("img[alt='Loader'][src='/assets/catarse_bootstrap/loader-eff2ad1eeb09a19c9afb5b143e1dd62b.gif']") ]), m(".w-col.w-col-5") ]) ]) ]) ];
+        }, "Carregar mais") ]), m(".w-col.w-col-5") ]) ]) ]) ];
     }
 }, adminApp.AdminContributionsFilter = {
     controller: function(args) {
