54490bb556c3cd5b0d80260fa5921bde86462fdb
Vinícius Andrade <vchavesster@gmail.com>
2015-07-01 01:24:10 -0300

    Creates app global momentify function

--- src/catarse_admin.js
+++ src/catarse_admin.js
@@ -5,3 +5,8 @@ adminApp.models = {};
 adminApp.submodule = function(module, args) {
   return module.view.bind(this, new module.controller(args))
 };
+
+var momentify = function(date, format) {
+  format = format || "DD/MM/YYYY";
+  return (date) ? moment(new Date(date)).format(format) : "no date";
+};
