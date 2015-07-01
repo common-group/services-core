63293d0efc5e364622e8a1767f19f2dfd136bcfc
Vinícius Andrade <vchavesster@gmail.com>
2015-07-01 02:45:29 -0300

    Sets contribution_details request order

--- src/components/contributions/admin/adminContributionsFilterVM.js
+++ src/components/contributions/admin/adminContributionsFilterVM.js
@@ -1,1 +1,2 @@
-adminApp.AdminContributionsFilter.VM = m.postgrest.filtersVM({permalink: 'eq'});
+var vm = adminApp.AdminContributionsFilter.VM = m.postgrest.filtersVM({permalink: 'eq'});
+vm.order({id: "desc"});
