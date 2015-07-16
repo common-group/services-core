fde17555ae5e8594c3da0bf9b78bdda331d558fa
Antônio Roberto <forevertonny@gmail.com>
2015-07-16 12:05:10 -0300

    added toggleDiv component :)

---
+++ spec/components/toggleDiv.spec.js
@@ -0,0 +1,35 @@
+describe('toggleDiv component', function() {
+  var toggleProp = null, toggleComponent = null, $output = null;
+
+  beforeAll(function(){
+    toggleProp = adminApp.ToggleDiv.toggleProp('none', 'block');
+    $output = mq(adminApp.ToggleDiv, {display: toggleProp});
+  });
+
+  describe('toggleProp', function() {
+    it("should return a property with default state", function(){
+      expect(toggleProp()).toEqual('none')
+    })
+
+    it("should change a property to alternative state", function(){
+      toggleProp.toggle()
+      expect(toggleProp()).toEqual('block')
+    })
+  });
+
+  describe('view', function() {
+    it('should render .toggleDiv', function() {
+      toggleProp('none');
+      $output.redraw();
+      expect($output.find('.toggleDiv')[0].attrs.style.display).toEqual('none');
+    });
+
+    it('should toggle div', function() {
+      toggleProp('block');
+      $output.redraw();
+      expect($output.find('.toggleDiv')[0].attrs.style.display).toEqual('block');
+    })
+  });
+
+});
+
