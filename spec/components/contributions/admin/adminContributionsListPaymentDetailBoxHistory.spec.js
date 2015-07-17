10f3823204cdb2cdfe864ac3ad474dd30c0976dd
Antônio Roberto <forevertonny@gmail.com>
2015-07-17 00:30:11 -0300

    added history box with reduce and ordered dates

---
+++ spec/components/contributions/admin/adminContributionsListPaymentDetailBoxHistory.spec.js
@@ -0,0 +1,25 @@
+describe('AdminContributionsListPaymentDetailBoxHistory component', function() {
+  beforeAll(function(){
+    contribution = m.prop(ContributionDetailMockery(1));
+    historyBox = m.component(adminApp.AdminContributionsListPaymentDetailBoxHistory,  {contribution: contribution()[0]})
+    ctrl = historyBox.controller();
+    view = historyBox.view(ctrl, {contribution: contribution});
+    $output = mq(view);
+  });
+
+  describe('controller', function(){
+    it("should have orderedEvents", function() {
+      expect(ctrl.orderedEvents.length).toEqual(2);
+    });
+
+    it("should have formated the date on orderedEvents", function() {
+      expect(ctrl.orderedEvents[0].date).toEqual('15/01/2015, 17:25');
+    });
+  });
+
+  describe('view', function() {
+    it('should render fetched orderedEvents', function() {
+      expect($output.find('.date-event').length).toEqual(2);
+    });
+  });
+});
--- src/components/contributions/admin/adminContributionsListPaymentDetailBoxHistory.js
+++ src/components/contributions/admin/adminContributionsListPaymentDetailBoxHistory.js
@@ -1,61 +1,39 @@
 adminApp.AdminContributionsListPaymentDetailBoxHistory = {
+  controller: function(args) {
+    var contribution = args.contribution;
+    var mapEvents = _.reduce([
+      { date: contribution.paid_at, name: 'Apoio confirmado' },
+      { date: contribution.pending_refund_at, name: 'Reembolso solicitado' },
+      { date: contribution.refunded_at, name: 'Estorno realizado' },
+      { date: contribution.created_at, name: 'Apoio criado' },
+      { date: contribution.refused_at, name: 'Apoio cancelado' },
+    ], function(memo, item){
+      if(item.date != null && item.date != undefined) {
+        item.originalDate = item.date
+        item.date = momentify(item.date, "DD/MM/YYYY, HH:mm");
+        return memo.concat(item);
+      }
+
+      return memo;
+    }, []);
+
+    this.orderedEvents = _.sortBy(mapEvents, 'originalDate');
+  },
+
   view: function(ctrl, args) {
     var contribution = args.contribution;
     return m(".w-col.w-col-4",[
       m(".fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20", "Histórico da transação"),
-      m(".w-row.fontsize-smallest.lineheight-looser",[
-        m(".w-col.w-col-6",[
-          m(".fontcolor-secondary", "19/05/2015, 01:20 h")
-        ]),
-        m(".w-col.w-col-6",[
-          m("div", "Apoio criado")
-        ])
-      ]),
-      m(".w-row.fontsize-smallest.lineheight-looser",[
-        m(".w-col.w-col-6",[
-          m(".fontcolor-secondary", "19/05/2015, 01:20 h")
-        ]),
-        m(".w-col.w-col-6",[
-          m("div", "Apoio criado")
-        ])
-      ]),
-      m(".w-row.fontsize-smallest.lineheight-looser",[
-        m(".w-col.w-col-6",[
-          m(".fontcolor-secondary", "19/05/2015, 01:20 h")
-        ]),
-        m(".w-col.w-col-6",[
-          m("div",[
-            m("span.badge.badge-attention.fontsize-smallest", "Estorno realizado")
+      ctrl.orderedEvents.map(function(cEvent) {
+        return m(".w-row.fontsize-smallest.lineheight-looser.date-event",[
+          m(".w-col.w-col-6",[
+            m(".fontcolor-secondary", cEvent.date)
+          ]),
+          m(".w-col.w-col-6",[
+            m("div", cEvent.name)
           ])
-        ])
-      ]),
-      m(".w-row.fontsize-smallest.lineheight-looser",[
-        m(".w-col.w-col-6",[
-          m(".fontcolor-secondary", "19/05/2015, 01:20 h")
-        ]),
-        m(".w-col.w-col-6",[
-          m("div", "Apoio criado")
-        ])
-      ]),
-      m(".w-row.fontsize-smallest.lineheight-looser",[
-        m(".w-col.w-col-6",[
-          m(".fontcolor-secondary", "19/05/2015, 01:20 h")
-        ]),
-        m(".w-col.w-col-6",[
-          m("div", "Apoio criado")
-        ])
-      ]),
-      m(".w-row.fontsize-smallest.lineheight-looser",[
-        m(".w-col.w-col-6",[
-          m(".fontcolor-secondary", "19/05/2015, 01:20 h")
-        ]),
-        m(".w-col.w-col-6",[
-          m("div", "Apoio criado"),
-          m(".fontsize-smallest.lineheight-tighter",[
-            m("span.badge", "Luis Otavio Ribeiro")
-          ])
-        ])
-      ])
+        ]);
+      })
     ])
   }
 }
