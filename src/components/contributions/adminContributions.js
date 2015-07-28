199ca17089d6b604f6c0c8aa37ab17c664cdcc36
Vinícius Andrade <vchavesster@gmail.com>
2015-07-28 15:57:31 -0300

    Refactores Admin Contributions to match new components logic

--- src/components/contributions/adminContributions.js
+++ src/components/contributions/adminContributions.js
@@ -1,22 +1,18 @@
 adminApp.AdminContributions = {
   controller: function(args){
     this.error = m.prop();
+    var listVM = this.listVM = adminApp.ContributionListVM,
+        filterVM = this.filterVM = adminApp.ContributionFilterVM;
+
+    this.submit = function(){
+      listVM.firstPage(filterVM.parameters());
+      return false;
+    }
   },
   view: function(ctrl) {
     return  [
-      m.component(adminApp.AdminFilter,{vm: adminApp.ContributionFilterVM}),
-      m(".w-section.section",[
-        m(".w-container",[
-          m(".w-row.u-marginbottom-20", [
-            m(".w-col.w-col-9", [
-              m(".fontsize-base", [
-                m("span.fontweight-semibold", adminApp.ContributionListVM.total())," apoios encontrados"]
-               )
-            ])
-          ]),
-          (ctrl.error() ? m(".card.card-error.u-radius.fontweight-bold", adminApp.error()) : m.component(adminApp.AdminList, {vm: adminApp.ContributionListVM})),
-        ])
-      ])
+      m.component(adminApp.AdminFilter,{form: ctrl.filterVM.formDescriber, submit: ctrl.submit }),
+      m.component(adminApp.AdminList, {vm: ctrl.listVM })
     ];
   }
 };
