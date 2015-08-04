2e9000306c1a881f54ceb9cb360c7d7c8ce50cb8
Diogo Biazus <diogob@gmail.com>
2015-08-04 18:43:03 -0400

    Changes format of contribution date for HH

--- src/components/admin/admin-item.js
+++ src/components/admin/admin-item.js
@@ -76,7 +76,7 @@ adminApp.AdminItem = {
         ]),
         m('.w-col.w-col-2',[
           m('.fontweight-semibold.lineheight-tighter.u-marginbottom-10.fontsize-small', 'R$' + contribution.value),
-          m('.fontsize-smallest.fontcolor-secondary', h.momentify(contribution.paid_at, 'DD/MM/YYYY hh:mm[h]')),
+          m('.fontsize-smallest.fontcolor-secondary', h.momentify(contribution.paid_at, 'DD/MM/YYYY HH:mm[h]')),
           m('.fontsize-smallest', ['ID do Gateway: ',
             m('a.alt-link[target="_blank"][href="https://dashboard.pagar.me/#/transactions/' + contribution.gateway_id + '"]', contribution.gateway_id)
           ])
