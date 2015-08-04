d577ab7de5330773e76b3660ac2ae76a59817ae3
Vinícius Andrade <vchavesster@gmail.com>
2015-08-04 16:47:48 -0300

    Adds jshint and jscs to build scripts and fixes style issues

--- src/components/transactions/admin-transaction-history.js
+++ src/components/transactions/admin-transaction-history.js
@@ -2,15 +2,15 @@ adminApp.AdminTransactionHistory = {
   controller: function(args) {
     var contribution = args.contribution;
     var mapEvents = _.reduce([
-      { date: contribution.paid_at, name: 'Apoio confirmado' },
-      { date: contribution.pending_refund_at, name: 'Reembolso solicitado' },
-      { date: contribution.refunded_at, name: 'Estorno realizado' },
-      { date: contribution.created_at, name: 'Apoio criado' },
-      { date: contribution.refused_at, name: 'Apoio cancelado' },
+      {date: contribution.paid_at, name: 'Apoio confirmado'},
+      {date: contribution.pending_refund_at, name: 'Reembolso solicitado'},
+      {date: contribution.refunded_at, name: 'Estorno realizado'},
+      {date: contribution.created_at, name: 'Apoio criado'},
+      {date: contribution.refused_at, name: 'Apoio cancelado'},
     ], function(memo, item){
-      if(item.date !== null && item.date !== undefined) {
+      if (item.date !== null && item.date !== undefined) {
         item.originalDate = item.date;
-        item.date = h.momentify(item.date, "DD/MM/YYYY, HH:mm");
+        item.date = h.momentify(item.date, 'DD/MM/YYYY, HH:mm');
         return memo.concat(item);
       }
 
@@ -22,15 +22,15 @@ adminApp.AdminTransactionHistory = {
 
   view: function(ctrl, args) {
     var contribution = args.contribution;
-    return m(".w-col.w-col-4",[
-      m(".fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20", "Histórico da transação"),
+    return m('.w-col.w-col-4',[
+      m('.fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20', 'Histórico da transação'),
       ctrl.orderedEvents.map(function(cEvent) {
-        return m(".w-row.fontsize-smallest.lineheight-looser.date-event",[
-          m(".w-col.w-col-6",[
-            m(".fontcolor-secondary", cEvent.date)
+        return m('.w-row.fontsize-smallest.lineheight-looser.date-event',[
+          m('.w-col.w-col-6',[
+            m('.fontcolor-secondary', cEvent.date)
           ]),
-          m(".w-col.w-col-6",[
-            m("div", cEvent.name)
+          m('.w-col.w-col-6',[
+            m('div', cEvent.name)
           ])
         ]);
       })
--- src/components/transactions/admin-transaction.js
+++ src/components/transactions/admin-transaction.js
@@ -1,30 +1,30 @@
 adminApp.AdminTransaction = {
   view: function(ctrl, args) {
     var contribution = args.contribution;
-    return m(".w-col.w-col-4",[
-      m(".fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20", "Detalhes do apoio"),
-      m(".fontsize-smallest.lineheight-looser",[
-        "Valor: R$" + formatNumber(contribution.value, 2, 3),
-        m("br"),
-        "Taxa: R$" + formatNumber(contribution.gateway_fee, 2, 3),
-        m("br"),
-        "Anônimo: " + (contribution.anonymous ? 'Sim' : 'Não'),
-        m("br"),
-        "Id pagamento: " + contribution.gateway_id,
-        m("br"),
-        "Apoio: " + contribution.contribution_id,
-        m("br"),
-        "Chave: \n",
-        m("br"),
+    return m('.w-col.w-col-4',[
+      m('.fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20', 'Detalhes do apoio'),
+      m('.fontsize-smallest.lineheight-looser',[
+        'Valor: R$' + formatNumber(contribution.value, 2, 3),
+        m('br'),
+        'Taxa: R$' + formatNumber(contribution.gateway_fee, 2, 3),
+        m('br'),
+        'Anônimo: ' + (contribution.anonymous ? 'Sim' : 'Não'),
+        m('br'),
+        'Id pagamento: ' + contribution.gateway_id,
+        m('br'),
+        'Apoio: ' + contribution.contribution_id,
+        m('br'),
+        'Chave: \n',
+        m('br'),
         contribution.key,
-        m("br"),
-        "Meio: " + contribution.gateway,
-        m("br"),
-        "Operadora: " + (contribution.gateway_data && contribution.gateway_data.acquirer_name),
-        m("br"),
+        m('br'),
+        'Meio: ' + contribution.gateway,
+        m('br'),
+        'Operadora: ' + (contribution.gateway_data && contribution.gateway_data.acquirer_name),
+        m('br'),
         (function(){
-          if(contribution.is_second_slip) {
-            return [m("a.link-hidden[href='#']", "Boleto bancário"), " ", m("span.badge", "2a via")];
+          if (contribution.is_second_slip) {
+            return [m('a.link-hidden[href=\'#\']', 'Boleto bancário'), ' ', m('span.badge', '2a via')];
           }
         })(),
       ])
