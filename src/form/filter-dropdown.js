1261ab00235f206e362ed5eb5b7e41635f68300f
Diogo Biazus <diogob@gmail.com>
2015-08-05 12:07:15 -0400

    Moves form components to form namespace and tidy up indentation

--- src/form/filter-dropdown.js
+++ src/form/filter-dropdown.js
@@ -1,4 +1,4 @@
-adminApp.filterDropdown = {
+form.filterDropdown = {
   view: function(ctrl, args){
     return m('.w-col.w-col-3.w-col-small-6', [
       m('label.fontsize-smaller[for="' + args.index + '"]', args.label),
@@ -6,9 +6,9 @@ adminApp.filterDropdown = {
         onchange: m.withAttr('value', args.vm),
         value: args.vm()
       },[
-          _.map(args.options, function(data){
-            return m('option[value="' + data.value + '"]', data.option);
-          })
+        _.map(args.options, function(data){
+          return m('option[value="' + data.value + '"]', data.option);
+        })
       ])
     ]);
   }
