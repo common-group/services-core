b7bf8c6247f00b077470568a1b1325cdd4e53d2f
Antônio Roberto <forevertonny@gmail.com>
2015-08-06 11:47:44 -0300

    fix model name

--- src/admin/contributions/contribution-list-vm.js
+++ src/admin/contributions/contribution-list-vm.js
@@ -1,3 +1,3 @@
 window.c.adminApp.ContributionListVM = (function(m, models) {
-  m.postgrest.paginationVM(models.ContributionDetail.getPageWithToken);
+  m.postgrest.paginationVM(models.contributionDetail.getPageWithToken);
 }(window.m, window.c.models));
