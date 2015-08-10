da8847a2f5113d14728349c6375286df9cf01639
Vinícius Andrade <vchavesster@gmail.com>
2015-08-10 08:30:06 -0300

    Adds form constructor tests to admin filter spec

--- spec/components/admin-filter.spec.js
+++ spec/components/admin-filter.spec.js
@@ -1,7 +1,8 @@
-describe('AdminFilter', function() {
+describe('AdminFilter', function(){
   var ctrl,
       submit,
       fakeForm,
+      formDesc,
       c = window.c,
       vm = c.admin.contributionFilterVM,
       AdminFilter = c.AdminFilter;
@@ -20,7 +21,8 @@ describe('AdminFilter', function() {
     beforeAll(function(){
       spyOn(m, 'component').and.callThrough();
       submit = jasmine.createSpy('submit');
-      fakeForm = FormDescriberMock(['main', 'dropdown', 'numberRange', 'dateRange']);
+      formDesc = ['main', 'dropdown', 'numberRange', 'dateRange'];
+      fakeForm = FormDescriberMock(formDesc);
       $output = mq(AdminFilter.view(AdminFilter.controller(), {form: fakeForm, submit: submit}));
     });
 
@@ -28,11 +30,9 @@ describe('AdminFilter', function() {
       expect(m.component).toHaveBeenCalledWith(c.FilterMain, fakeForm[0].data);
     });
 
-    it('should build a form from a FormDescriber', function(){
+    it('should build a form from a FormDescriber when clicking the advanced filter', function(){
       $output.click('button');
-      expect(m.component).toHaveBeenCalledWith(c.FilterDropdown, fakeForm[1].data);
-      expect(m.component).toHaveBeenCalledWith(c.FilterNumberRange, fakeForm[2].data);
-      expect(m.component).toHaveBeenCalledWith(c.FilterDateRange, fakeForm[3].data);
+      expect(m.component.calls.count()).toEqual(formDesc.length);
     });
 
     it('should trigger a submit function when submitting the form', function(){
