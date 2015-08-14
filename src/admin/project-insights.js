3fdcf09d0b9fa3fa5a0cdd4c89f034e27f6e113c
Antônio Roberto <forevertonny@gmail.com>
2015-08-14 17:39:36 -0300

    basics request to project_details

--- src/admin/project-insights.js
+++ src/admin/project-insights.js
@@ -1,16 +1,32 @@
-window.c.admin.ProjectInsights = (function(m, c){
+window.c.admin.ProjectInsights = (function(m, c, models){
   return {
-    view: function() {
+    controller: function(args) {
+      var vm = m.postgrest.filtersVM({project_id: 'eq'}),
+          resource = m.prop({}),
+          resourceId = args.root.getAttribute('data-id');
+
+      vm.project_id(resourceId);
+
+      models.projectDetail.getRow(vm.parameters()).then(function(data){
+        resource(data[0]);
+      });
+
+      return {
+        vm: vm,
+        resource: resource
+      };
+    },
+    view: function(ctrl) {
       return m('.project-insights',[
         m('.w-row', [
           m('.w-col.w-col-2'),
           m('.w-col.w-col-8.dashboard-header.u-text-center', [
             m('.fontweight-semibold.fontsize-larger.lineheight-looser.u-marginbottom-10', 'Minha campanha'),
-            m.component(c.AdminProjectDetailsCard)
+            m.component(c.AdminProjectDetailsCard, {resource: ctrl.resource})
           ]),
           m('.w-col.w-col-2')
         ])
       ]);
     }
   };
-}(window.m, window.c));
+}(window.m, window.c, window.c.models));
--- src/c/admin-project-details-card.js
+++ src/c/admin-project-details-card.js
@@ -1,27 +1,28 @@
 window.c.AdminProjectDetailsCard = (function(m){
   return {
-    view: function() {
+    view: function(ctrl, args) {
+      var project = args.resource();
       return m('.card.u-radius.card-terciary.u-marginbottom-20', [
           m('.fontsize-small.fontweight-semibold.u-marginbottom-20', [
-            m('span.fontcolor-secondary', 'Status:'),' ',m('span.text-success', 'NO AR'),' '
+            m('span.fontcolor-secondary', 'Status:'),' ',m('span.text-success', project.state),' '
           ]),
           m('.meter.u-marginbottom-10', [
             m('.meter-fill')
           ]),
           m('.w-row', [
             m('.w-col.w-col-3.w-col-small-3.w-col-tiny-6', [
-              m('.fontweight-semibold.fontsize-large.lineheight-tight', '54%'),
-              m('.fontcolor-secondary.lineheight-tighter.fontsize-small.u-marginbottom-10', 'financiado')
+              m('.fontweight-semibold.fontsize-large.lineheight-tight', project.progress + '%'),
+              m('.fontcolor-secondary.lineheight-tighter.fontsize-small.u-marginbottom-10', project.state)
             ]),
             m('.w-col.w-col-3.w-col-small-3.w-col-tiny-6', [
               m('.fontweight-semibold.fontsize-large.lineheight-tight', [
-                'R$514.000',
-                m('span.fontsize-smaller', ',00')
+                project.pledged,
+                //m('span.fontsize-smaller', ',00')
               ]),
               m('.fontcolor-secondary.lineheight-tighter.fontsize-small.u-marginbottom-10', 'levantados')
             ]),
             m('.w-col.w-col-3.w-col-small-3.w-col-tiny-6', [
-              m('.fontweight-semibold.fontsize-large.lineheight-tight', '1222'),
+              m('.fontweight-semibold.fontsize-large.lineheight-tight', project.total_contributions),
               m('.fontcolor-secondary.lineheight-tighter.fontsize-small', 'apoios')
             ]),
             m('.w-col.w-col-3.w-col-small-3.w-col-tiny-6', [
