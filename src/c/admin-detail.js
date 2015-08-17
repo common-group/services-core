b6e2d37fc42be7868afa4b4d37692c17fdc999d9
Vinícius Andrade <vchavesster@gmail.com>
2015-08-17 16:26:33 -0300

    Changes item var name and adds item as a parameter to the action component

--- src/c/admin-detail.js
+++ src/c/admin-detail.js
@@ -4,18 +4,18 @@ window.c.AdminDetail = (function(m, _, c){
     },
     view: function(ctrl, args){
       var actions = args.actions,
-          contribution = args.item;
+          item = args.item;
       return m('#admin-contribution-detail-box', [
         m('.divider.u-margintop-20.u-marginbottom-20'),
         m('.w-row.u-marginbottom-30',
           _.map(actions, function(action){
-            return m.component(c[action.component], {data: action.data});
+            return m.component(c[action.component], {data: action.data, item: args.item});
           })
         ),
         m('.w-row.card.card-terciary.u-radius',[
-          m.component(c.AdminTransaction, {contribution: contribution}),
-          m.component(c.AdminTransactionHistory, {contribution: contribution}),
-          m.component(c.AdminReward, {contribution: contribution, key: contribution.key})
+          m.component(c.AdminTransaction, {contribution: item}),
+          m.component(c.AdminTransactionHistory, {contribution: item}),
+          m.component(c.AdminReward, {contribution: item, key: item.key})
         ])
       ]);
     }
