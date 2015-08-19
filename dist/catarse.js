23b5e05fee9e43ec1375e117f4609f15663f7044
Igor P. Leroy <ip.leroy@gmail.com>
2015-08-19 16:53:23 -0300

    Fix ProjectReminderCount resource

--- dist/catarse.js
+++ dist/catarse.js
@@ -672,7 +672,7 @@ window.c = function() {
 }(window.m, window.Chart, window._), window.c.ProjectReminderCount = function(m) {
     return {
         view: function(ctrl, args) {
-            var project = args.project;
+            var project = args.resource;
             return m(".card.u-radius.u-text-center.medium.u-marginbottom-80", [ m(".fontsize-large.fontweight-semibold", "Total de pessoas que clicaram no botão Lembrar-me"), m(".fontsize-smaller.u-marginbottom-30", "Eles receberão um lembrete por email 48 horas antes do término de sua campanha"), m(".fontsize-jumbo", project.reminder_count) ]);
         }
     };
