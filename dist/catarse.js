7283e222d53cacdd86048102d1ceb06e9b30fb80
Antônio Roberto <forevertonny@gmail.com>
2015-08-20 11:00:47 -0300

    added specs for project-reminder-count component

--- dist/catarse.js
+++ dist/catarse.js
@@ -762,7 +762,7 @@ window.c = function() {
     return {
         view: function(ctrl, args) {
             var project = args.resource;
-            return m(".card.u-radius.u-text-center.medium.u-marginbottom-80", [ m(".fontsize-large.fontweight-semibold", "Total de pessoas que clicaram no botão Lembrar-me"), m(".fontsize-smaller.u-marginbottom-30", "Eles receberão um lembrete por email 48 horas antes do término de sua campanha"), m(".fontsize-jumbo", project.reminder_count) ]);
+            return m("#project-reminder-count.card.u-radius.u-text-center.medium.u-marginbottom-80", [ m(".fontsize-large.fontweight-semibold", "Total de pessoas que clicaram no botão Lembrar-me"), m(".fontsize-smaller.u-marginbottom-30", "Eles receberão um lembrete por email 48 horas antes do término de sua campanha"), m(".fontsize-jumbo", project.reminder_count) ]);
         }
     };
 }(window.m), window.c.TeamMembers = function(_, m, models) {
--- src/c/project-reminder-count.js
+++ src/c/project-reminder-count.js
@@ -2,7 +2,7 @@ window.c.ProjectReminderCount = (function(m){
   return {
     view: function(ctrl, args) {
       var project = args.resource;
-      return m('.card.u-radius.u-text-center.medium.u-marginbottom-80', [
+      return m('#project-reminder-count.card.u-radius.u-text-center.medium.u-marginbottom-80', [
         m('.fontsize-large.fontweight-semibold', 'Total de pessoas que clicaram no botão Lembrar-me'),
         m('.fontsize-smaller.u-marginbottom-30', 'Eles receberão um lembrete por email 48 horas antes do término de sua campanha'),
         m('.fontsize-jumbo', project.reminder_count)
