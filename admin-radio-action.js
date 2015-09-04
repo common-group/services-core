6597fea483e0c15ecc101b0f47a8892e3b3eb0cf
Vinícius Andrade <vchavesster@gmail.com>
2015-09-04 14:38:14 -0300

    [WIP] Starts admin-radio-action component

---
+++ admin-radio-action.js
@@ -0,0 +1,84 @@
+window.c.AdminRadioAction = (function(m, h, c){
+  return {
+    controller: function(args){
+      var builder = args.data,
+          complete = m.prop(false),
+          error = m.prop(false),
+          fail = m.prop(false),
+          data = {},
+          item = args.item,
+          key = builder.getKey,
+          newValue = m.prop(''),
+          updateVM = m.postgrest.filtersVM({contribution_id: 'eq'});
+
+      h.idVM.id(item[builder.updateKey]);
+      updateVM.contribution_id(item.contribution_id);
+
+      var l = m.postgrest.loaderWithToken(builder.model.patchOptions(h.idVM.parameters(), data));
+
+      var updateItem = function(data){
+        _.extend(item, data[0]);
+        complete(true);
+      };
+
+      var submit = function(){
+        data[key] = newValue();
+        l.load().then(updateItem, error);
+        return false;
+      };
+
+      var unload = function(el, isinit, context){
+        context.onunload = function(){
+          complete(false);
+          error(false);
+          newValue('');
+        };
+      };
+
+      return {
+        complete: complete,
+        error: error,
+        l: l,
+        newValue: newValue,
+        submit: submit,
+        toggler: h.toggleProp(false, true),
+        unload: unload
+      };
+    },
+    view: function(ctrl, args){
+      var data = args.data,
+          btnValue = (ctrl.l()) ? 'por favor, aguarde...' : data.callToAction;
+
+      return m('.w-col.w-col-2',[
+        m('button.btn.btn-small.btn-terciary', {
+          onclick: ctrl.toggler.toggle
+        }, data.outerLabel),
+        (ctrl.toggler()) ?
+          m('.dropdown-list.card.u-radius.dropdown-list-medium.zindex-10', {config: ctrl.unload},[
+            m('form.w-form', {
+              onsubmit: ctrl.submit
+            }, (!ctrl.complete()) ? [
+                  _.map(ctrl.radios(), function(radio, index){
+                    return m('.w-radio', [
+                      m('input#r-' + index + '.w-radio-input[type=radio][value="' + radio.id + '"]'),
+                      m('label.w-form-label[for="r-' + index + '"]', 'R$' + radio.value)
+                    ]);
+                  }),
+                  m('p', ctrl.activeText()),
+                  m('input.w-button.btn.btn-small[type="submit"][value="' + btnValue + '"]')
+                ] : (!ctrl.error()) ? [
+                    m('.w-form-done[style="display:block;"]', [
+                      m('p', 'Recompensa alterada com sucesso!')
+                    ])
+                  ] : [
+                    m('.w-form-error[style="display:block;"]', [
+                      m('p', 'Houve um problema na requisição. O apoio não foi transferido!')
+                    ])
+                  ]
+            )
+          ])
+        : ''
+      ]);
+    }
+  };
+}(window.m, window.c.h, window.c));
