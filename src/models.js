4cb21b07115e076ceb4f6eaf98a5ff634edb0cce
Antônio Roberto <forevertonny@gmail.com>
2015-09-14 00:12:15 -0300

    added userDetail model

--- src/models.js
+++ src/models.js
@@ -2,6 +2,7 @@ window.c.models = (function(m){
   var contributionDetail = m.postgrest.model('contribution_details'),
 
   projectDetail = m.postgrest.model('project_details'),
+  userDetail = m.postgrest.model('user_details'),
   contributions = m.postgrest.model('contributions'),
   teamTotal = m.postgrest.model('team_totals'),
   projectContributionsPerDay = m.postgrest.model('project_contributions_per_day'),
@@ -12,6 +13,7 @@ window.c.models = (function(m){
   return {
     contributionDetail: contributionDetail,
     projectDetail: projectDetail,
+    userDetail: userDetail,
     contributions: contributions,
     teamTotal: teamTotal,
     teamMember: teamMember,
