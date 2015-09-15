04832a29c3374ca538f484e03fd905c5978dd489
Antônio Roberto <forevertonny@gmail.com>
2015-09-15 04:04:09 -0300

    added souldOut and Remaning count on project-reward-list

--- src/c/project-reward-list.js
+++ src/c/project-reward-list.js
@@ -18,18 +18,29 @@ window.c.ProjectRewardList = (function(m, h, models, _){
     view: function(ctrl, args) {
       //FIXME: MISSING ADJUSTS
       // - hover effect
-      // - card-gone when reward.sold_out?
-      // - max contributions limit overview
       // - add draft admin modifications
       var project = args.project;
       return m('#rewards.u-marginbottom-30', _.map(ctrl.rewardDetails(), function(reward) {
         var contributionUrlWithReward = '/projects/' + project.id + '/contributions/new?reward_id=' + reward.id;
 
-        return m('a.card-reward.card.card-secondary.u-marginbottom-10[href="' + (project.open_for_contributions ? contributionUrlWithReward : 'js:void(0);') + '"]', [
+        return m('a[class="clickable ' + (h.rewardSouldOut(reward) ? "card-gone" : "card-reward") + ' card card-secondary u-marginbottom-10"][href="' + (project.open_for_contributions ? contributionUrlWithReward : 'js:void(0);') + '"]', [
           m('.u-marginbottom-20', [
             m('.fontsize-base.fontweight-semibold', 'Para R$ ' + h.formatNumber(reward.minimum_value) + ' ou mais'),
             m('.fontsize-smaller.fontweight-semibold', h.pluralize(reward.paid_count, ' apoiador', ' apoiadores')),
-            m('.fontsize-smaller', m.trust(h.simpleFormat(reward.description)))
+            (reward.maximum_contributions > 0 ? [
+              (reward.waiting_payment_count > 0 ? m('.maximum_contributions.in_time_to_confirm.clearfix', [
+                m('.pending.fontsize-smallest.fontcolor-secondary', h.pluralize(reward.waiting_payment_count, ' apoio em prazo de confirmação', ' apoios em prazo de confirmação.'))
+              ]) : ''),
+              (h.rewardSouldOut(reward) ? m('.u-margintop-10', [
+                m('span.badge.badge-gone.fontsize-smaller', 'Esgotada')
+              ]) : m('.u-margintop-10', [
+                m('span.badge.badge-attention.fontsize-smaller', [
+                  m('span.fontweight-bold', 'Limitada'),
+                  ' ( ' + h.rewardRemaning(reward) + ' de ' + reward.maximum_contributions + ' disponíveis)'
+                ])
+              ]))
+            ] : ''),
+            m('.fontsize-smaller.u-margintop-20', m.trust(h.simpleFormat(reward.description)))
           ])
         ]);
       }));
