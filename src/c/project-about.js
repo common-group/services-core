b0275cdd37cb4e09b8a1e436d2f86b7cf35fbe92
Antônio Roberto <forevertonny@gmail.com>
2015-09-15 14:01:01 -0300

    added funding period on project-about

--- src/c/project-about.js
+++ src/c/project-about.js
@@ -1,4 +1,4 @@
-window.c.ProjectAbout = (function(m, c){
+window.c.ProjectAbout = (function(m, c, h){
   return {
     view: function(ctrl, args) {
       var project = args.project;
@@ -13,9 +13,17 @@ window.c.ProjectAbout = (function(m, c){
         ]),
         m('.w-col.w-col-4.w-hidden-small.w-hidden-tiny', [
           m('.w-hidden-small.w-hidden-tiny.fontsize-base.fontweight-semibold.u-marginbottom-30', 'Recompensas'),
-          m.component(c.ProjectRewardList, {project: project})
+          m.component(c.ProjectRewardList, {project: project}),
+          (project.is_published ?
+            m('.funding-period', [
+              m('.fontsize-small.fontweight-semibold.u-text-center-small-only', 'Período de campanha'),
+              m('.fontsize-small.u-text-center-small-only', [
+                h.momentify(project.online_date), ' - ', h.momentify(project.zone_expires_at), ' (' + project.online_days + ' dias) '
+              ])
+            ])
+           : '')
         ])
       ]);
     }
   };
-}(window.m, window.c));
+}(window.m, window.c, window.c.h));
