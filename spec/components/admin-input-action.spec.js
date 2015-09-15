78656731f6cd0d48ec6c4d1c3628c00078d89971
Vinícius Andrade <vchavesster@gmail.com>
2015-09-15 00:22:52 -0300

    Fixes crazy callback on request stub on admin-input-action specs

--- spec/components/admin-input-action.spec.js
+++ spec/components/admin-input-action.spec.js
@@ -63,7 +63,7 @@ describe('AdminInputAction', function(){
 
     describe('on form submit', function(){
       beforeAll(function(){
-        spyOn(m, 'request').and.returnValue( {then: function(callback){ callback([{test: true}]); return {then: function(callback){ return callback(); } } }});
+        spyOn(m, 'request').and.returnValue({then: function(callback){ callback([{test: true}]); }});
       });
       beforeEach(function(){
         $output.click('button');
