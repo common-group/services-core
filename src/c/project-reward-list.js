dbe1be96df09c02f2f120a510a8b635452120ef9
Antônio Roberto <forevertonny@gmail.com>
2015-09-15 14:00:18 -0300

    added hover effect / and deliver_at to project-reward-list

--- src/c/project-reward-list.js
+++ src/c/project-reward-list.js
@@ -17,13 +17,12 @@ window.c.ProjectRewardList = (function(m, h, models, _){
 
     view: function(ctrl, args) {
       //FIXME: MISSING ADJUSTS
-      // - hover effect
       // - add draft admin modifications
       var project = args.project;
       return m('#rewards.u-marginbottom-30', _.map(ctrl.rewardDetails(), function(reward) {
         var contributionUrlWithReward = '/projects/' + project.id + '/contributions/new?reward_id=' + reward.id;
 
-        return m('a[class="clickable ' + (h.rewardSouldOut(reward) ? "card-gone" : "card-reward") + ' card card-secondary u-marginbottom-10"][href="' + (project.open_for_contributions ? contributionUrlWithReward : 'js:void(0);') + '"]', [
+        return m('a[class="' + (h.rewardSouldOut(reward) ? "card-gone" : "card-reward " + (project.open_for_contributions ? 'clickable' : '') ) + ' card card-secondary u-marginbottom-10"][href="' + (project.open_for_contributions ? contributionUrlWithReward : 'js:void(0);') + '"]', [
           m('.u-marginbottom-20', [
             m('.fontsize-base.fontweight-semibold', 'Para R$ ' + h.formatNumber(reward.minimum_value) + ' ou mais'),
             m('.fontsize-smaller.fontweight-semibold', h.pluralize(reward.paid_count, ' apoiador', ' apoiadores')),
@@ -40,8 +39,18 @@ window.c.ProjectRewardList = (function(m, h, models, _){
                 ])
               ]))
             ] : ''),
-            m('.fontsize-smaller.u-margintop-20', m.trust(h.simpleFormat(reward.description)))
-          ])
+          ]),
+          m('.fontsize-smaller.u-margintop-20', m.trust(h.simpleFormat(reward.description))),
+          (!_.isEmpty(reward.deliver_at) ?
+            m('.fontsize-smaller', [
+              m('b', 'Estimativa de Entrega: '),
+              h.momentify(reward.deliver_at, 'MMM/YYYY')
+            ])
+           : ''),
+          (project.open_for_contributions ?
+            m('.project-reward-box-hover', [
+              m('.project-reward-box-select-text.u-text-center', 'Selecione essa recompensa')
+            ]) : '' )
         ]);
       }));
     }
