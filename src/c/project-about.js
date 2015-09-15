68e9e9a0153f622f7fe261507d63cc25734c40ec
Antônio Roberto <forevertonny@gmail.com>
2015-09-15 04:04:43 -0300

    adjust project-about in new styles and render rewards here

--- src/c/project-about.js
+++ src/c/project-about.js
@@ -1,18 +1,21 @@
-window.c.ProjectAbout = (function(m){
+window.c.ProjectAbout = (function(m, c){
   return {
     view: function(ctrl, args) {
       var project = args.project;
-      return m('.project-about', [
-        (project.video_embed_url ? m('.w-embed.w-video.project-video', [
-          m('iframe.embedly-embed[itemprop="video"][src=" ' + project.video_embed_url + '"][width="100%"][height="379"][frameborder="0"][allowFullScreen]')
-        ]) : m('span.no-video')),
-        m('.fontsize-large.u-marginbottom-30[itemprop="name"]', project.headline),
-        m('.no-mobile', [
-          m('.fontsize-base[itemprop="about"]', m.trust(project.about_html))
+      return m('#project-about', [
+        m('.project-about.w-col.w-col-8', [
+          m('p.fontsize-base', [
+            m('strong', 'O projeto'),
+          ]),
+          m('.fontsize-base[itemprop="about"]', m.trust(project.about_html)),
+          m('p.fontsize-large.fontweight-semibold', 'Orçamento'),
+          m('p.fontsize-base', m.trust(project.budget))
         ]),
-        m('p.fontsize-large.fontweight-semibold', 'Orçamento'),
-        m('p.fontsize-base', m.trust(project.budget))
+        m('.w-col.w-col-4.w-hidden-small.w-hidden-tiny', [
+          m('.w-hidden-small.w-hidden-tiny.fontsize-base.fontweight-semibold.u-marginbottom-30', 'Recompensas'),
+          m.component(c.ProjectRewardList, {project: project})
+        ])
       ]);
     }
   };
-}(window.m));
+}(window.m, window.c));
