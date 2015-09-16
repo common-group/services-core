13a9e8f4f8a89acefb49e21d94e48c3a2311351f
Antônio Roberto <forevertonny@gmail.com>
2015-09-15 22:12:45 -0300

    added projectContribution model

--- src/models.js
+++ src/models.js
@@ -6,6 +6,7 @@ window.c.models = (function(m){
   rewardDetail = m.postgrest.model('reward_details'),
   contributions = m.postgrest.model('contributions'),
   teamTotal = m.postgrest.model('team_totals'),
+  projectContribution = m.postgrest.model('project_contributions'),
   projectContributionsPerDay = m.postgrest.model('project_contributions_per_day'),
   projectContributionsPerLocation = m.postgrest.model('project_contributions_per_location'),
   teamMember = m.postgrest.model('team_members');
@@ -22,6 +23,7 @@ window.c.models = (function(m){
     teamTotal: teamTotal,
     teamMember: teamMember,
     projectContributionsPerDay: projectContributionsPerDay,
-    projectContributionsPerLocation: projectContributionsPerLocation
+    projectContributionsPerLocation: projectContributionsPerLocation,
+    projectContribution: projectContribution
   };
 }(window.m));
