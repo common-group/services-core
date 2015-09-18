9609da58d00543d1c00e727d248153e51f152200
Antônio Roberto <forevertonny@gmail.com>
2015-09-17 21:35:38 -0300

    fix reminder bug

--- src/c/project-reminder.js
+++ src/c/project-reminder.js
@@ -3,36 +3,35 @@ window.c.ProjectReminder = (function(m, models, h) {
     controller: function(args) {
       var inReminder = h.toggleProp(false, true),
           filterVM = m.postgrest.filtersVM({project_id: 'eq'}),
-          l, generateSubmitReminder;
+          complete = m.prop(false),
+          submitReminder = function() {
+            var l;
+
+            if (inReminder()) {
+              l = m.postgrest.loaderWithToken(models.projectReminder.deleteOptions(filterVM.parameters()));
+            } else {
+              l = m.postgrest.loaderWithToken(models.projectReminder.postOptions({project_id: args.project.id}));
+            }
+
+            l.load().then(function(){
+              inReminder.toggle();
+            });
+          };
 
       filterVM.project_id(args.project.id);
       inReminder(args.project.in_reminder);
 
-      if (inReminder()) {
-        l = m.postgrest.loaderWithToken(models.projectReminder.deleteOptions(filterVM.parameters()));
-      } else {
-        l = m.postgrest.loaderWithToken(models.projectReminder.postOptions({project_id: args.project.id}));
-      }
-
-      generateSubmitReminder = function(l) {
-        return function() {
-          l.load().then(function(){
-            inReminder.toggle();
-          });
-        };
-      };
-
       return {
         inReminder: inReminder,
-        submitReminder: generateSubmitReminder(l),
-        l: l
+        submitReminder: submitReminder,
+        complete: complete
       };
     },
     view: function(ctrl) {
       return m('.u-text-center.u-marginbottom-30', [
         m('a[class="link-hidden fontsize-small ' + (ctrl.inReminder() ? 'link-hidden-success' : 'fontcolor-secondary') + ' fontweight-semibold"][href="js:void(0);"]', {onclick: ctrl.submitReminder},[
           m('span.fa.fa-clock-o'),
-          (ctrl.l() ? 'aguarde ...' : (ctrl.inReminder() ? ' Lembrete ativo' : '  Lembrar-me'))
+          (ctrl.complete() ? 'aguarde ...' : (ctrl.inReminder() ? ' Lembrete ativo' : '  Lembrar-me'))
         ])
       ]);
     }
