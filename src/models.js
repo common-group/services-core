3cc5ad90394f292d977c4b7c6c634562836fda3e
Igor P. Leroy <ip.leroy@gmail.com>
2015-09-18 01:24:52 -0300

    Add user_details view to models

--- src/models.js
+++ src/models.js
@@ -1,13 +1,13 @@
 window.c.models = (function(m){
   var contributionDetail = m.postgrest.model('contribution_details'),
-
   projectDetail = m.postgrest.model('project_details'),
   contributions = m.postgrest.model('contributions'),
   teamTotal = m.postgrest.model('team_totals'),
   projectContributionsPerDay = m.postgrest.model('project_contributions_per_day'),
   projectContributionsPerLocation = m.postgrest.model('project_contributions_per_location'),
   project = m.postgrest.model('projects'),
-  teamMember = m.postgrest.model('team_members');
+  teamMember = m.postgrest.model('team_members'),
+  userDetail = m.postgrest.model('user_details');
   teamMember.pageSize(40);
   project.pageSize(3);
 
@@ -17,6 +17,7 @@ window.c.models = (function(m){
     contributions: contributions,
     teamTotal: teamTotal,
     teamMember: teamMember,
+    userDetail: userDetail,
     project: project,
     projectContributionsPerDay: projectContributionsPerDay,
     projectContributionsPerLocation: projectContributionsPerLocation
