894d87d6d4c197abf81a3e8123f1ffe771af660e
Antônio Roberto <forevertonny@gmail.com>
2015-09-18 15:43:11 -0300

    fix project-tab reward link style

--- src/c/project-tabs.js
+++ src/c/project-tabs.js
@@ -7,17 +7,17 @@ window.c.ProjectTabs = (function(m, h){
           m('.w-row', [
             m('.w-col.w-col-9', [
               //FIXME: need to adjust rewards on mobile
-              m('a[class="w-hidden-main w-hidden-medium dashboard-nav-link mf ' + (h.hashMatch('#rewards') ? 'selected' : '') + '"][href="#rewards"]', 'Recompensas'),
-              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#about') || h.hashMatch('') ? 'selected' : '') + ' "][href="#about"]', 'Sobre'),
-              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#posts') ? 'selected' : '') + '"][href="#posts"]', [
+              m('a[class="w-hidden-main w-hidden-medium dashboard-nav-link mf ' + (h.hashMatch('#rewards') ? 'selected' : '') + '"][href="#rewards"]', {style: 'float: left;'}, 'Recompensas'),
+              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#about') || h.hashMatch('') ? 'selected' : '') + ' "][href="#about"]', {style: 'float: left;'}, 'Sobre'),
+              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#posts') ? 'selected' : '') + '"][href="#posts"]', {style: 'float: left;'}, [
                 'Novidades',
                 m('span.badge', project.posts_count)
               ]),
-              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#contributions') ? 'selected' : '') + '"][href="#contributions"]', [
+              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#contributions') ? 'selected' : '') + '"][href="#contributions"]', {style: 'float: left;'}, [
                 'Apoios',
                 m('span.badge.w-hidden-small.w-hidden-tiny', project.total_contributions)
               ]),
-              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#comments') ? 'selected' : '') + '"][href="#comments"]', [
+              m('a[class="dashboard-nav-link mf ' + (h.hashMatch('#comments') ? 'selected' : '') + '"][href="#comments"]', {style: 'float: left;'}, [
                 'Comentarios',
                 m('fb:comments-count[href="http://www.catarse.me/' + project.permalink + '"][class="badge project-fb-comment w-hidden-small w-hidden-tiny"][style="display: inline"]', m.trust('&nbsp;'))
               ]),
