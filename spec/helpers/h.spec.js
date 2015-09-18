d28f86484b46f782571eeeeafefc8acfe39a8b44
Antônio Roberto <forevertonny@gmail.com>
2015-09-18 03:44:08 -0300

    es6

--- spec/helpers/h.spec.js
+++ spec/helpers/h.spec.js
@@ -1,8 +1,8 @@
-describe("h.formatNumber", function(){
-  var number = null,
+describe("h.formatNumber", () => {
+  let number = null,
     formatNumber = window.c.h.formatNumber;
 
-  it("should format number", function(){
+  it("should format number", () => {
     number = 120.20;
     expect(formatNumber(number)).toEqual('120');
     expect(formatNumber(number, 2, 3)).toEqual('120,20');
