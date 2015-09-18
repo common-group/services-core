66c0e8b3a3edb9b136a342f3ffb0cf6ec200eab7
Antônio Roberto <forevertonny@gmail.com>
2015-09-18 03:44:31 -0300

    added h.rewardSouldOut spec

--- spec/helpers/h.spec.js
+++ spec/helpers/h.spec.js
@@ -9,3 +9,39 @@ describe("h.formatNumber", () => {
     expect(formatNumber(number, 2, 2)).toEqual('1.20,20');
   });
 });
+
+describe('h.rewardSouldOut', () => {
+  let reward = null,
+    rewardSouldOut = window.c.h.rewardSouldOut;
+
+  it('return true when reward already sould out', () => {
+    reward = {
+      maximum_contributions: 5,
+      paid_count: 3,
+      waiting_payment_count: 2
+    };
+
+    expect(rewardSouldOut(reward)).toEqual(true);
+  });
+
+  it('return false when reward is not sould out', () => {
+    reward = {
+      maximum_contributions: 5,
+      paid_count: 3,
+      waiting_payment_count: 1
+    };
+
+    expect(rewardSouldOut(reward)).toEqual(false);
+  });
+
+  it('return false when reward is not defined maximum_contributions', () => {
+    reward = {
+      maximum_contributions: null,
+      paid_count: 3,
+      waiting_payment_count: 1
+    };
+
+    expect(rewardSouldOut(reward)).toEqual(false);
+  });
+});
+
