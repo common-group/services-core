e0a7a3cf0569f8666f0aa512e819c24d66435c27
Igor P. Leroy <ip.leroy@gmail.com>
2015-09-22 15:49:54 -0300

    Add users component in admin namespace

---
+++ src/admin/users.js
@@ -0,0 +1,34 @@
+window.c.admin.Users = (function(m, c, h){
+  var admin = c.admin;
+  return {
+    controller: function(){
+      var listVM = admin.userListVM,
+          error = m.prop(''),
+          itemBuilder = [
+            {
+              component: 'AdminUser',
+              wrapperClass: '.w-col.w-col-4'
+            },
+          ],
+          submit = function(){
+            listVM.firstPage(filterVM.parameters()).then(null, function(serverError){
+              error(serverError.message);
+            });
+            return false;
+          };
+
+      return {
+        itemActions: [],
+        itemBuilder: itemBuilder,
+        listVM: {list: listVM, error: error},
+        submit: submit
+      };
+    },
+
+    view: function(ctrl){
+      return [
+        m.component(c.AdminList, {vm: ctrl.listVM, itemBuilder: ctrl.itemBuilder, itemActions: ctrl.itemActions})
+      ];
+    }
+  };
+}(window.m, window.c, window.c.h));
