b1798d76dde35b893688d11ab7899d32b4b1175a
Igor P. Leroy <ip.leroy@gmail.com>
2015-09-29 01:22:29 -0300

    Add initial builders to users admin

--- src/admin/users.js
+++ src/admin/users.js
@@ -3,11 +3,21 @@ window.c.admin.Users = (function(m, c, h){
   return {
     controller: function(){
       var listVM = admin.userListVM,
+          filterVM = admin.userFilterVM,
           error = m.prop(''),
           itemBuilder = [
             {
               component: 'AdminUser',
               wrapperClass: '.w-col.w-col-4'
+            },
+          ],
+          filterBuilder = [
+            { //name
+              component: 'FilterMain',
+              data: {
+                vm: filterVM.name,
+                placeholder: 'Busque por nome...',
+              },
             },
           ],
           submit = function(){
@@ -18,16 +28,20 @@ window.c.admin.Users = (function(m, c, h){
           };
 
       return {
+        filterVM: filterVM,
+        filterBuilder: filterBuilder,
         itemActions: [],
         itemBuilder: itemBuilder,
         listVM: {list: listVM, error: error},
+        data: {label: 'Usuários'},
         submit: submit
       };
     },
 
     view: function(ctrl){
       return [
-        m.component(c.AdminList, {vm: ctrl.listVM, itemBuilder: ctrl.itemBuilder, itemActions: ctrl.itemActions})
+        m.component(c.AdminFilter,{form: ctrl.filterVM.formDescriber, filterBuilder: ctrl.filterBuilder, data: ctrl.data, submit: ctrl.submit}),
+        m.component(c.AdminList, {vm: ctrl.listVM, data: ctrl.data, itemBuilder: ctrl.itemBuilder, itemActions: ctrl.itemActions})
       ];
     }
   };
