214e4e2f468d5092c51d4617527fd5d44ba3fcc0
Antônio Roberto <forevertonny@gmail.com>
2015-09-28 22:55:07 -0300

    change generateRoutes for displayTabContent
    passed to es6 too

--- src/c/project-main.js
+++ src/c/project-main.js
@@ -1,39 +1,38 @@
-window.c.ProjectMain = (function(m, c, _, h){
+window.c.ProjectMain = ((m, c, _, h) => {
   return {
-    controller: function(args) {
-      var project = args.project,
-          generateRoutes = function() {
-            var hash = window.location.hash,
-                c_opts = {project: project},
-                routes = {
-                  '#rewards': m('.w-col.w-col-12', m.component(c.ProjectRewardList, _.extend({}, {rewardDetails: args.rewardDetails}, c_opts))),
-                  '#contributions': m.component(c.ProjectContributions, c_opts),
-                  '#about': m.component(c.ProjectAbout, _.extend({}, {rewardDetails: args.rewardDetails}, c_opts)),
-                  '#comments': m.component(c.ProjectComments, c_opts),
-                  '#posts': m.component(c.ProjectPosts, c_opts)
-                };
+    controller: (args) => {
+      const project = args.project,
+          displayTabContent = () => {
+            const hash = window.location.hash,
+                  c_opts = {project: project},
+                  tabs = {
+                    '#rewards': m('.w-col.w-col-12', m.component(c.ProjectRewardList, _.extend({}, {rewardDetails: args.rewardDetails}, c_opts))),
+                    '#contributions': m.component(c.ProjectContributions, c_opts),
+                    '#about': m.component(c.ProjectAbout, _.extend({}, {rewardDetails: args.rewardDetails}, c_opts)),
+                    '#comments': m.component(c.ProjectComments, c_opts),
+                    '#posts': m.component(c.ProjectPosts, c_opts)
+                  };
 
             h.fbParse();
 
             if (_.isEmpty(hash)) {
-              return routes['#about'];
+              return tabs['#about'];
             }
 
-            return routes[hash];
+            return tabs[hash];
           };
 
       window.addEventListener('hashchange', m.redraw, false);
 
       return {
-        project: project,
-        generateRoutes: generateRoutes
+        displayTabContent: displayTabContent
       };
     },
 
-    view: function(ctrl) {
+    view: (ctrl) => {
       return m('section.section[itemtype="http://schema.org/CreativeWork"]', [
         m('.w-container', [
-          m('.w-row', ctrl.generateRoutes())
+          m('.w-row', ctrl.displayTabContent())
         ])
       ]);
     }
