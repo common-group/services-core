b1d80485280cbc3b7c980cba49ca3d7bab4e2557
Antônio Roberto <forevertonny@gmail.com>
2015-10-06 07:38:08 -0300

    fix project image height [#104645568]

--- src/c/project-highlight.js
+++ src/c/project-highlight.js
@@ -1,27 +1,26 @@
-window.c.ProjectHighlight = (function(m, _, h, c){
+window.c.ProjectHighlight = ((m, _, h, c) => {
   return {
-    controller: function() {
+    controller: () => {
       var displayShareBox = h.toggleProp(false, true);
 
       return {
         displayShareBox: displayShareBox
       };
     },
-    //FIXME: Add img when video is not present
-    view: function(ctrl, args) {
+    view: (ctrl, args) => {
       var project = args.project;
       return m('#project-highlight', [
         (project.video_embed_url ? m('.w-embed.w-video.project-video.mf', {style: 'min-height: 240px;'}, [
-          m('iframe.embedly-embed[itemprop="video"][src=" ' + project.video_embed_url + '"][frameborder="0"][allowFullScreen]')
-        ]) : m('img.w-embed.w-video.project-video.mf[src="' + (project.original_image) + '"]')),
+          m('iframe.embedly-embed[itemprop="video"][src="' + project.video_embed_url + '"][frameborder="0"][allowFullScreen]')
+        ]) : m('img.project-video-image.project-video.mf[src="' + (project.original_image) + '"]', {style: 'height: 400px !important;'})),
         m('.project-blurb', project.headline),
         m('.u-text-center-small-only.u-marginbottom-30', [
           (!_.isNull(project.address) ?
             m('a.btn.btn-inline.btn-small.btn-transparent.link-hidden-light.u-marginbottom-10[href="js:void(0);"]', [
-              m('span.fa.fa-map-marker'), ' ' + project.address.city + ', ' + project.address.state_acronym
+              m('span.fa.fa-map-marker'), ` ${project.address.city}, ${project.address.state_acronym}`
             ]) : ''
           ),
-          m('a.btn.btn-inline.btn-small.btn-transparent.link-hidden-light[href="/explore/by_category_id/#"' + project.category_id + ']', [
+          m(`a.btn.btn-inline.btn-small.btn-transparent.link-hidden-light[href="/explore/by_category_id/#${project.category_id}"]`, [
             m('span.fa.fa-tag'), ' ',
             project.category_name
           ]),
