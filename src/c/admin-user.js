64f1c51d4fab130b3d476765f684f3506ba5bab7
Vinícius Andrade <vchavesster@gmail.com>
2015-10-07 19:25:08 -0300

    Fixes admin-user output

--- src/c/admin-user.js
+++ src/c/admin-user.js
@@ -7,6 +7,20 @@ window.c.AdminUser = (function(m, h){
         var user = item.user;
         return m('.w-row.admin-user',[
           m('.w-col.w-col-3.w-col-small-3.u-marginbottom-10',[
+            m('img.user-avatar[src="' + h.useAvatarOrDefault(user.profile_img_thumbnail) + '"]')
+          ]),
+          m('.w-col.w-col-9.w-col-small-9',[
+            m('.fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-10', [
+              m('a.alt-link[target="_blank"][href="/users/' + user.id + '/edit"]', user.name)
+            ]),
+            m('.fontsize-smallest', 'Usuário: ' + user.id),
+            m('.fontsize-smallest.fontcolor-secondary', 'Email: ' + user.email)
+          ])
+        ]);
+      } else {
+        var user = item;
+        return m('.w-row.admin-user',[
+          m('.w-col.w-col-3.w-col-small-3.u-marginbottom-10',[
             m('img.user-avatar[src="' + h.useAvatarOrDefault(user.user_profile_img) + '"]')
           ]),
           m('.w-col.w-col-9.w-col-small-9',[
@@ -16,20 +30,6 @@ window.c.AdminUser = (function(m, h){
             m('.fontsize-smallest', 'Usuário: ' + user.user_id),
             m('.fontsize-smallest.fontcolor-secondary', 'Catarse: ' + user.email),
             m('.fontsize-smallest.fontcolor-secondary', 'Gateway: ' + user.payer_email)
-          ])
-        ]);
-      } else {
-        var user = item;
-        return m('.w-row.admin-user',[
-          m('.w-col.w-col-3.w-col-small-3.u-marginbottom-10',[
-            m('img.user-avatar[src="' + h.useAvatarOrDefault(user.profile_img_thumbnail) + '"]')
-          ]),
-          m('.w-col.w-col-9.w-col-small-9',[
-            m('.fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-10', [
-              m('a.alt-link[target="_blank"][href="/users/' + user.id + '/edit"]', user.name)
-            ]),
-            m('.fontsize-smallest', 'Usuário: ' + user.id),
-            m('.fontsize-smallest.fontcolor-secondary', 'Email: ' + user.email)
           ])
         ]);
       }
