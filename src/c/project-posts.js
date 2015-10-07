ee27d2bf42201667e2448bfbf3f204a1fe36eea0
Antônio Roberto <forevertonny@gmail.com>
2015-10-07 06:49:16 -0300

    added text when project does not have any posts [#104645568]

--- src/c/project-posts.js
+++ src/c/project-posts.js
@@ -21,14 +21,17 @@ window.c.ProjectPosts = ((m, models, h, _) => {
 
       return m('.project-posts.w-section', [
         m('.w-container.u-margintop-20', [
-          (project.is_owner_or_admin ?
+          (project.is_owner_or_admin ? [
+            (_.isEmpty(list.collection()) ? m('.w-hidden-small.w-hidden-tiny', [
+              m('.fontsize-base.u-marginbottom-30.u-margintop-20', 'Toda novidade publicada no Catarse é enviada diretamente para o email de quem já apoiou seu projeto e também fica disponível para visualização no site. Você pode optar por deixá-la pública, ou visível somente para seus apoiadores aqui nesta aba.')
+            ]) : ''),
             m('.w-row.u-marginbottom-20', [
               m('.w-col.w-col-4'),
               m('.w-col.w-col-4', [
                 m(`a.btn.btn-edit.btn-small[href='/pt/projects/${project.id}/edit#posts']`, 'Escrever novidade')
               ]),
               m('.w-col.w-col-4'),
-            ])
+            ])]
            : ''),
           (_.map(list.collection(), (post) => {
             return m('.w-row', [
