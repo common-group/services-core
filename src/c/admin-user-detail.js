b2e776e5e1a378396fa5c1fe4f0d3e3a90942bca
Igor P. Leroy <ip.leroy@gmail.com>
2015-10-14 01:10:38 -0300

    Add reset password action

--- src/c/admin-user-detail.js
+++ src/c/admin-user-detail.js
@@ -1,14 +1,30 @@
 window.c.AdminUserDetail = (function(m, _, c){
   return {
     controller: function(){
+      return {
+        actions: {
+          reset: {
+            property: 'user_password',
+            updateKey: 'password',
+            callToAction: 'Redefinir',
+            innerLabel: 'Nova senha de Usuário:',
+            outerLabel: 'Redefinir senha',
+            placeholder: 'ex: 123mud@r',
+            model: c.models.userDetail
+          }
+        }
+      };
     },
+
     view: function(ctrl, args){
-      var actions = args.actions,
+      var actions = ctrl.actions,
           item = args.item,
           details = args.details;
       return m('#admin-contribution-detail-box', [
         m('.divider.u-margintop-20.u-marginbottom-20'),
-        m('.w-row.u-marginbottom-30', []), /* actions go here */
+        m('.w-row.u-marginbottom-30', [
+            m.component(c.AdminInputAction, {data: actions.reset, item: item}),
+        ]),
       ]);
     }
   };
