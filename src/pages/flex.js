05155ebd45cd808ec79cdfe9f9992092ac9855e2
Vinícius Andrade <vchavesster@gmail.com>
2015-10-28 22:58:18 -0200

    Fixes copy and style issues

--- src/pages/flex.js
+++ src/pages/flex.js
@@ -28,9 +28,8 @@ window.c.pages.Flex = (function(m, c, h, models) {
                 m('.w-section.hero-full.hero-zelo', [
                     m('.w-container.u-text-center', [
                         m('img.logo-flex-home[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/5626e670595f80b338f5bf5c_logo-flex.png\'][width=\'359\']'),
-                        m('.fontsize-large.u-marginbottom-60', [
-                            'Vamos construir uma nova modalidade de crowdfunding para o Catarse!  ',
-                            m('br'), 'Junte-se a nós, inscreva seu email!'
+                        m('.w-row', [
+                            m('.w-col.fontsize-large.u-marginbottom-60.w-col-push-2.w-col-8', 'Vamos construir uma nova modalidade de crowdfunding para o Catarse! Junte-se a nós, inscreva seu email!')
                         ]),
                         m('.w-row', [
                             m('.w-col.w-col-2'),
@@ -63,21 +62,21 @@ window.c.pages.Flex = (function(m, c, h, models) {
                                 m('.w-col.w-col-6', [
                                     m('.u-text-center', [
                                         m('img[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/560e39c578b284493e2a428a_zelo-money.png\'][width=\'180\']')
-                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Fique com quanto arrecadar'), m('p.fontsize-base', 'O Flex é para impulsionar campanhas onde todo dinheiro é bem vindo! Você fica com tudo que arrecadar.')
+                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Fique com quanto arrecadar'), m('p.u-text-center.fontsize-base', 'O Flex é para impulsionar campanhas onde todo dinheiro é bem vindo! Você fica com tudo que arrecadar.')
                                 ]), m('.w-col.w-col-6', [
                                     m('.u-text-center', [
                                         m('img[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/560e39d37c013d4a3ee687d2_icon-reward.png\'][width=\'180\']')
-                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Não precisa de recompensas'), m('p.fontsize-base', 'Você não precisa oferecer recompensas aos seus apoiadores. Está na sua mão a escolha.')
+                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Não precisa de recompensas'), m('p.u-text-center.fontsize-base', 'Você não precisa oferecer recompensas aos seus apoiadores. Está na sua mão a escolha.')
                                 ])
                             ]), m('.w-row.u-marginbottom-40', [
                                 m('.w-col.w-col-6', [
                                     m('.u-text-center', [
                                         m('img[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/560e39fb01b66e250aca67e3_icon-curad.png\'][width=\'180\']')
-                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Você mesmo publica seu projeto'), m('p.fontsize-base', 'Todos os projetos inscritos no Flex entram no ar. Agilidade e facilidade para você captar recursos através da internet.')
+                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Você mesmo publica seu projeto'), m('p.u-text-center.fontsize-base', 'Todos os projetos inscritos no Flex entram no ar. Agilidade e facilidade para você captar recursos através da internet.')
                                 ]), m('.w-col.w-col-6', [
                                     m('.u-text-center', [
                                         m('img[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/560e39e77c013d4a3ee687d4_icon-time.png\'][width=\'180\']')
-                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Encerre a campanha quando quiser'), m('p.fontsize-base', 'Não há limite de tempo para sua campanha. Para receber o dinheiro arrecadado por você, basta encerrá-la.')
+                                    ]), m('.fontsize-large.u-marginbottom-10.u-text-center.fontweight-semibold', 'Encerre a campanha quando quiser'), m('p.u-text-center.fontsize-base', 'Não há limite de tempo para sua campanha. Para receber o dinheiro arrecadado por você, basta encerrá-la.')
                                 ])
                             ])
                         ])
@@ -113,23 +112,6 @@ window.c.pages.Flex = (function(m, c, h, models) {
                                 ])
                             ])
                         ])
-                    ]), m('.w-section.section-one-column.bg-catarse-zelo.section-large', [
-                        m('.w-container.u-text-center', [
-                            m('.u-marginbottom-40.fontsize-larger.lineheight-tight.fontcolor-negative', 'O Flex é um experimento e iniciativa do Catarse, maior plataforma de crowdfunding do Brasil.   '),
-                            m('.w-row.u-text-center',
-                                (ctrl.loader()) ? h.loader() : [
-                                    m('.w-col.w-col-4', [
-                                        m('.fontsize-jumbo.text-success.lineheight-loose', h.formatNumber(stats.total_contributors, 0, 3)), m('p.start-stats.fontsize-base.fontcolor-negative', 'Pessoas ja apoiaram pelo menos 01 projeto no Catarse')
-                                    ]),
-                                    m('.w-col.w-col-4', [
-                                        m('.fontsize-jumbo.text-success.lineheight-loose', h.formatNumber(stats.total_projects_success, 0, 3)), m('p.start-stats.fontsize-base.fontcolor-negative', 'Projetos ja foram financiados no Catarse')
-                                    ]),
-                                    m('.w-col.w-col-4', [
-                                        m('.fontsize-jumbo.text-success.lineheight-loose', stats.total_contributed.toString().slice(0, 2) + ' milhões'), m('p.start-stats.fontsize-base.fontcolor-negative', 'Foram investidos em ideias publicadas no Catarse')
-                                    ])
-                                ]
-                            )
-                        ])
                     ]), 
                     m('.w-section.section-large.u-text-center.bg-purple', [
                         m('.w-container.fontcolor-negative', [
@@ -164,14 +146,14 @@ window.c.pages.Flex = (function(m, c, h, models) {
                                 m('.w-col.w-col-2'),
                                 m('.w-col.w-col-8', [
                                     m('.w-row', [
-                                        m('.w-col.w-col-6', [
-                                            m('.div-share-mobile-fb', [
+                                        m('.w-col.w-col-6.w-col-small-6.w-col-tiny-6.w-sub-col-middle', [
+                                            m('div', [
                                                 m('img.icon-share-mobile[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/53a3f66e05eb6144171d8edb_facebook-xxl.png\']'),
                                                 m('a.w-button.btn.btn-large.btn-fb[href="http://www.facebook.com/sharer/sharer.php?u=https://www.catarse.me/flex?ref=facebook&title=' + encodeURIComponent('Conheça o novo Catarse Flex!') + '"][target="_blank"]', 'Compartilhar')
                                             ])
                                         ]),
-                                        m('.w-col.w-col-6', [
-                                            m('.div-share-mobile-fb', [
+                                        m('.w-col.w-col-6.w-col-small-6.w-col-tiny-6', [
+                                            m('div', [
                                                 m('img.icon-share-mobile[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/53a3f65105eb6144171d8eda_twitter-256.png\']'),
                                                 m('a.w-button.btn.btn-large.btn-tweet[href="http://twitter.com/?status=' + encodeURIComponent('Conheça o novo Catarse Flex! ') + 'https://www.catarse.me/flex?ref=twitter"][target="_blank"]', 'Tuitar')
                                             ])
@@ -186,7 +168,7 @@ window.c.pages.Flex = (function(m, c, h, models) {
                             m('.u-text-center', [
                                 m('.fontsize-largest.fontcolor-negative', 'Construa o flex conosco'), m('.fontsize-base.u-marginbottom-60.fontcolor-negative', 'Inicie uma conversa, pergunte, comente, critique e faça sugestões!')
                             ]),
-                            m('#disqus_thread', {
+                            m('#disqus_thread.card.u-radius', {
                                 config: ctrl.addDisqus
                             })
                         ])
