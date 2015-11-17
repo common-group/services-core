0b7603bcc8cd64593706f9532e56c8f342ed49f0
Diogo Biazus <diogob@gmail.com>
2015-11-17 18:15:38 -0500

    Fixes style hints

--- src/c/tooltip.js
+++ src/c/tooltip.js
@@ -22,43 +22,43 @@ window.c.Tooltip = ((m, c, h) => {
     return {
         controller: (args) => {
             let parentHeight = m.prop(0),
-            width = m.prop(args.width || 280),
-            top = m.prop(0),
-            left = m.prop(0),
-            opacity = m.prop(0),
-            parentOffset = m.prop({top: 0, left: 0}),
-            tooltip = h.toggleProp(0, 1),
-			toggle = () => {
-    tooltip.toggle();
-    m.redraw();
-			};
+                width = m.prop(args.width || 280),
+                top = m.prop(0),
+                left = m.prop(0),
+                opacity = m.prop(0),
+                parentOffset = m.prop({top: 0, left: 0}),
+                tooltip = h.toggleProp(0, 1),
+                toggle = () => {
+                    tooltip.toggle();
+                    m.redraw();
+                };
 
             const setParentPosition = (el, isInitialized) => {
                 if (!isInitialized){
                     parentOffset(h.cumulativeOffset(el));
                 }
             },
-			setPosition = (el, isInitialized) => {
-                if (!isInitialized){
-                    let elTop = el.offsetHeight + el.offsetParent.offsetHeight;
-                    let style = window.getComputedStyle(el);
-                    
-                    if(window.innerWidth < (el.offsetWidth + 2*parseFloat(style.paddingLeft) + 30)){ //30 here is a safe margin
-                        el.style.width = window.innerWidth - 30; //Adding the safe margin
-                        left(-parentOffset().left + 15); //positioning center of window, considering margin
-                    } else if ((parentOffset().left + (el.offsetWidth / 2)) <= window.innerWidth && (parentOffset().left - (el.offsetWidth / 2)) >= 0){
-                        console.log('Positioning center');
-                        left(-el.offsetWidth / 2); //Positioning to the center
-                    } else if ((parentOffset().left + (el.offsetWidth / 2)) > window.innerWidth) {
-                        console.log('Positioning right');
-                        left(-el.offsetWidth + el.offsetParent.offsetWidth); //Positioning to the left
-                    } else if ((parentOffset().left - (el.offsetWidth / 2)) < 0) {
-                        console.log("positioning left");
-                        left(-el.offsetParent.offsetWidth); //Positioning to the right
-                    }
-                    top(-elTop); //Setting top position
-                }
-			};
+                  setPosition = (el, isInitialized) => {
+                      if (!isInitialized){
+                          let elTop = el.offsetHeight + el.offsetParent.offsetHeight;
+                          let style = window.getComputedStyle(el);
+
+                          if (window.innerWidth < (el.offsetWidth + 2 * parseFloat(style.paddingLeft) + 30)){ //30 here is a safe margin
+                              el.style.width = window.innerWidth - 30; //Adding the safe margin
+                              left(-parentOffset().left + 15); //positioning center of window, considering margin
+                          } else if ((parentOffset().left + (el.offsetWidth / 2)) <= window.innerWidth && (parentOffset().left - (el.offsetWidth / 2)) >= 0){
+                              console.log('Positioning center');
+                              left(-el.offsetWidth / 2); //Positioning to the center
+                          } else if ((parentOffset().left + (el.offsetWidth / 2)) > window.innerWidth) {
+                              console.log('Positioning right');
+                              left(-el.offsetWidth + el.offsetParent.offsetWidth); //Positioning to the left
+                          } else if ((parentOffset().left - (el.offsetWidth / 2)) < 0) {
+                              console.log('positioning left');
+                              left(-el.offsetParent.offsetWidth); //Positioning to the right
+                          }
+                          top(-elTop); //Setting top position
+                      }
+                  };
 
             return {
                 width: width,
@@ -79,12 +79,12 @@ window.c.Tooltip = ((m, c, h) => {
                 config: ctrl.setParentPosition,
                 style: {cursor: 'pointer'}
             }, ctrl.tooltip() ? [
-				m(`.tooltip.dark[style="width: ${width}px; top: ${ctrl.top()}px; left: ${ctrl.left()}px;"]`, {
-    config: ctrl.setPosition
-				}, [
+                m(`.tooltip.dark[style="width: ${width}px; top: ${ctrl.top()}px; left: ${ctrl.left()}px;"]`, {
+                    config: ctrl.setPosition
+                }, [
                     m('.fontsize-smallest', args.text)
                 ])
-			] : '');
+            ] : '');
         }
     };
 }(window.m, window.c, window.c.h));
