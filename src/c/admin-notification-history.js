c16d54f0172b71e897c6ea18a3673a48d3830862
Igor P. Leroy <ip.leroy@gmail.com>
2015-11-29 13:31:00 -0200

    Convert component to es6

--- src/c/admin-notification-history.js
+++ src/c/admin-notification-history.js
@@ -1,36 +1,36 @@
-window.c.AdminNotificationHistory = (function(m, h, _, models) {
+window.c.AdminNotificationHistory = ((m, h, _, models) => {
     return {
-		controller: function(args) {
-			var notifications = m.prop([]),
-			getNotifications = function(user){
-				var notification = models.notification;
-				notification.getPageWithToken(m.postgrest.filtersVM({user_id: 'eq'}).user_id(user.id).parameters()).then(function(data){
-					notifications(data);
-				});
-				return notifications();
-			};
+        controller: (args) => {
+            const notifications = m.prop([]),
+                getNotifications = (user) => {
+                    let notification = models.notification;
+                    notification.getPageWithToken(m.postgrest.filtersVM({user_id: 'eq'}).user_id(user.id).parameters()).then(function(data){
+                        notifications(data);
+                    });
+                    return notifications();
+                };
 
-			getNotifications(args.user);
+            getNotifications(args.user);
 
-			return {
-				notifications: notifications
-			};
-		},
+            return {
+                notifications: notifications
+            };
+        },
 
-		view: function(ctrl) {
-			return m('.w-col.w-col-4', [
-				m('.fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20', 'Histórico de notificações'),
-				ctrl.notifications().map(function(cEvent) {
-					return m('.w-row.fontsize-smallest.lineheight-looser.date-event', [
-						m('.w-col.w-col-24', [
-							m('.fontcolor-secondary',
-							  'notificação: ', cEvent.template_name, ', ',
-							  'criada em: ', h.momentify(cEvent.created_at, 'DD/MM/YYYY, HH:mm'), ', ',
-							  'enviada em: ', h.momentify(cEvent.sent_at, 'DD/MM/YYYY, HH:mm'))
-						]),
-					]);
-				})
-		    ]);
-	    }
+        view: (ctrl) => {
+            return m('.w-col.w-col-4', [
+                m('.fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20', 'Histórico de notificações'),
+                ctrl.notifications().map(function(cEvent) {
+                    return m('.w-row.fontsize-smallest.lineheight-looser.date-event', [
+                        m('.w-col.w-col-24', [
+                            m('.fontcolor-secondary',
+                              'notificação: ', cEvent.template_name, ', ',
+                              'criada em: ', h.momentify(cEvent.created_at, 'DD/MM/YYYY, HH:mm'), ', ',
+                              'enviada em: ', h.momentify(cEvent.sent_at, 'DD/MM/YYYY, HH:mm'))
+                        ]),
+                    ]);
+                })
+            ]);
+        }
     };
 }(window.m, window.c.h, window._, window.c.models));
