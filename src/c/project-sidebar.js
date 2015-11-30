0ca95109f88a3cef6624b348d92aa9ae1a911fdc
Vinícius Andrade <vchavesster@gmail.com>
2015-11-30 16:10:27 -0200

    Implements fixed navbar actions

--- src/c/project-sidebar.js
+++ src/c/project-sidebar.js
@@ -107,7 +107,8 @@ window.c.ProjectSidebar = (function(m, h, c) {
                         ])
                     ])
                     , (project.open_for_contributions ? m('a#contribute_project_form.btn.btn-large.u-marginbottom-20[href="/projects/' + project.id + '/contributions/new"]', 'Apoiar este projeto') : ''), ((project.open_for_contributions) ? m.component(c.ProjectReminder, {
-                        project: project
+                        project: project,
+                        type: 'link'
                     }) : ''),
                     m('div[class="fontsize-smaller u-marginbottom-30 ' + (ctrl.displayCardClass()) + '"]', ctrl.displayStatusText())
                 ]),
