66ad8904de8c07933028fb07021e3cbc9cff1764
Igor P. Leroy <ip.leroy@gmail.com>
2015-12-01 11:57:16 -0200

    Fix indentation and es6 support

--- src/c/admin-notification-history.js
+++ src/c/admin-notification-history.js
@@ -4,7 +4,17 @@ window.c.AdminNotificationHistory = ((m, h, _, models) => {
             const notifications = m.prop([]),
                 getNotifications = (user) => {
                     let notification = models.notification;
-                    notification.getPageWithToken(m.postgrest.filtersVM({user_id: 'eq', sent_at: 'is.null'}).user_id(user.id).sent_at(!null).order({sent_at: 'desc'}).parameters()).then(function(data){
+                    notification.getPageWithToken(m.postgrest.filtersVM({
+                        user_id: 'eq',
+                        sent_at: 'is.null'
+                    })
+                    .user_id(user.id)
+                    .sent_at(!null)
+                    .order({
+                        sent_at: 'desc'
+                    })
+                    .parameters())
+                    .then((data) => {
                         notifications(data);
                     });
                     return notifications();
@@ -20,7 +30,7 @@ window.c.AdminNotificationHistory = ((m, h, _, models) => {
         view: (ctrl) => {
             return m('.w-col.w-col-4', [
                 m('.fontweight-semibold.fontsize-smaller.lineheight-tighter.u-marginbottom-20', 'Histórico de notificações'),
-                ctrl.notifications().map(function(cEvent) {
+                ctrl.notifications().map((cEvent) => {
                     return m('.w-row.fontsize-smallest.lineheight-looser.date-event', [
                         m('.w-col.w-col-24', [
                             m('.fontcolor-secondary', h.momentify(cEvent.sent_at, 'DD/MM/YYYY, HH:mm'),
