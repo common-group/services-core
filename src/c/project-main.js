465a1af02d178547e4c294095988193c99c74e8c
Diogo Biazus <diogob@gmail.com>
2015-12-23 15:28:01 -0500

    Creates redrawHashChange helper to hook a redraw on window hashchange with an optional callback before

--- src/c/project-main.js
+++ src/c/project-main.js
@@ -2,32 +2,32 @@ window.c.ProjectMain = ((m, c, _, h) => {
     return {
         controller: (args) => {
             const project = args.project,
-                displayTabContent = () => {
-                    const hash = window.location.hash,
-                        c_opts = {
-                            project: project
-                        },
-                        tabs = {
-                            '#rewards': m('.w-col.w-col-12', m.component(c.ProjectRewardList, _.extend({}, {
-                                rewardDetails: args.rewardDetails
-                            }, c_opts))),
-                            '#contribution_suggestions': m.component(c.ProjectSuggestedContributions, c_opts),
-                            '#contributions': m.component(c.ProjectContributions, c_opts),
-                            '#about': m.component(c.ProjectAbout, _.extend({}, {
-                                rewardDetails: args.rewardDetails
-                            }, c_opts)),
-                            '#comments': m.component(c.ProjectComments, c_opts),
-                            '#posts': m.component(c.ProjectPosts, c_opts)
-                        };
+                  displayTabContent = () => {
+                      const hash = window.location.hash,
+                            c_opts = {
+                                project: project
+                            },
+                            tabs = {
+                                '#rewards': m('.w-col.w-col-12', m.component(c.ProjectRewardList, _.extend({}, {
+                                    rewardDetails: args.rewardDetails
+                                }, c_opts))),
+                                '#contribution_suggestions': m.component(c.ProjectSuggestedContributions, c_opts),
+                                '#contributions': m.component(c.ProjectContributions, c_opts),
+                                '#about': m.component(c.ProjectAbout, _.extend({}, {
+                                    rewardDetails: args.rewardDetails
+                                }, c_opts)),
+                                '#comments': m.component(c.ProjectComments, c_opts),
+                                '#posts': m.component(c.ProjectPosts, c_opts)
+                            };
 
-                    if (_.isEmpty(hash) || hash === '#_=_' || hash === '#preview') {
-                        return tabs['#about'];
-                    }
+                      if (_.isEmpty(hash) || hash === '#_=_' || hash === '#preview') {
+                          return tabs['#about'];
+                      }
 
-                    return tabs[hash];
-                };
+                      return tabs[hash];
+                  };
 
-            window.addEventListener('hashchange', m.redraw, false);
+            h.redrawHashChange();
 
             return {
                 displayTabContent: displayTabContent
