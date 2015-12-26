73492bce79e8bf9a990764c9c3555c4289796cdc
Diogo Biazus <diogob@gmail.com>
2015-12-26 12:30:05 -0500

    Now if the loader is done and there is now projects in the project row we render an empty div

--- src/c/project-row.js
+++ src/c/project-row.js
@@ -4,24 +4,29 @@ window.c.ProjectRow = ((m, _, h) => {
             const collection = args.collection,
                 ref = args.ref,
                 wrapper = args.wrapper || '.w-section.section.u-marginbottom-40';
-            return m(wrapper, [
-                m('.w-container', [
-                    (!_.isUndefined(collection.title) || !_.isUndefined(collection.hash)) ? m('.w-row.u-marginbottom-30', [
-                        m('.w-col.w-col-10.w-col-small-6.w-col-tiny-6', [
-                            m('.fontsize-large.lineheight-looser', collection.title)
-                        ]),
-                        m('.w-col.w-col-2.w-col-small-6.w-col-tiny-6', [
-                            m(`a.btn.btn-small.btn-terciary[href="/pt/explore?ref=${ref}#${collection.hash}"]`, 'Ver todos')
-                        ])
-                    ]) : '',
-                    (collection.collection().length) ? m('.w-row', _.map(collection.collection(), (project) => {
-                        return m.component(c.ProjectCard, {
-                            project: project,
-                            ref: ref
-                        });
-                    })) : h.loader()
-                ])
-            ]);
+
+            if (collection.loader() || collection.collection().length > 0){
+                return m(wrapper, [
+                    m('.w-container', [
+                        (!_.isUndefined(collection.title) || !_.isUndefined(collection.hash)) ? m('.w-row.u-marginbottom-30', [
+                            m('.w-col.w-col-10.w-col-small-6.w-col-tiny-6', [
+                                m('.fontsize-large.lineheight-looser', collection.title)
+                            ]),
+                            m('.w-col.w-col-2.w-col-small-6.w-col-tiny-6', [
+                                m(`a.btn.btn-small.btn-terciary[href="/pt/explore?ref=${ref}#${collection.hash}"]`, 'Ver todos')
+                            ])
+                        ]) : '',
+                        collection.loader() ? h.loader() : m('.w-row', _.map(collection.collection(), (project) => {
+                            return m.component(c.ProjectCard, {
+                                project: project,
+                                ref: ref
+                            });
+                        }))
+                    ])
+                ]);
+            } else {
+                return m('div');
+            }
         }
     };
 }(window.m, window._, window.c.h));
