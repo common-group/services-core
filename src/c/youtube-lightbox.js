c600568cbb18730e076f9e11d5ab9f7ac2ca7ece
Vinícius Andrade <vchavesster@gmail.com>
2015-12-28 16:59:31 -0200

    Adds youtube iframe api control to lightbox component [fix #110649452]

--- src/c/youtube-lightbox.js
+++ src/c/youtube-lightbox.js
@@ -11,36 +11,72 @@
  */
 window.c.YoutubeLightbox = ((m, c, h, models) => {
     return {
-        controller: () => {
-            const showLightbox = h.toggleProp(false, true);
+        controller: (args) => {
+            let player;
+            const showLightbox = h.toggleProp(false, true),
+                setYoutube = (el, isInitialized) => {
+                    if (!isInitialized) {
+                        const tag = document.createElement('script'),
+                            firstScriptTag = document.getElementsByTagName('script')[0];
+                        tag.src = 'https://www.youtube.com/iframe_api';
+                        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+                        window.onYouTubeIframeAPIReady = createPlayer;
+                    }
+                },
+                closeVideo = () => {
+                    if (!_.isUndefined(player)) {
+                        player.pauseVideo();
+                    }
+
+                    showLightbox.toggle();
+
+                    return false;
+                },
+                createPlayer = () => {
+                    player = new YT.Player('ytvideo', {
+                        height: '528',
+                        width: '940',
+                        videoId: args.src,
+                        playerVars: {
+                            showInfo: 0,
+                            modestBranding: 0
+                        },
+                        events: {
+                            'onStateChange': (state) => (state.data === 0) ? closeVideo() : false
+                        }
+                    });
+                };
 
             return {
-                showLightbox: showLightbox
+                showLightbox: showLightbox,
+                setYoutube: setYoutube,
+                closeVideo: closeVideo
             };
         },
         view: (ctrl, args) => {
             return m('#youtube-lightbox', [
-                m('a#youtube-play.w-lightbox.w-inline-block.fa.fa-play-circle.fontcolor-negative.fa-5x[href=\'javascript:void(0);\']', {onclick: ctrl.showLightbox.toggle}),
-                ctrl.showLightbox() ?
-                m('#lightbox.w-lightbox-backdrop', [
+                m('a#youtube-play.w-lightbox.w-inline-block.fa.fa-play-circle.fontcolor-negative.fa-5x[href=\'javascript:void(0);\']', {
+                    onclick: ctrl.showLightbox.toggle
+                }),
+                m(`#lightbox.w-lightbox-backdrop[style="display:${ctrl.showLightbox() ? 'block' : 'none'}"]`, [
                     m('.w-lightbox-container', [
                         m('.w-lightbox-content', [
                             m('.w-lightbox-view', [
                                 m('.w-lightbox-frame', [
                                     m('figure.w-lightbox-figure', [
                                         m('img.w-lightbox-img.w-lightbox-image[src=\'data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%22940%22%20height=%22528%22/%3E\']'),
-                                        m(`iframe#video-lightbox.embedly-embed.w-lightbox-embed[allowfullscreen=\'\'][frameborder=\'0\'][height=\'528\'][scrolling=\'no\'][src=\'https://cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fwww.youtube.com%2Fembed%2FFlFTcDSKnLM%3Fautoplay%3D1%26feature%3Doembed&url=${encodeURI(args.src)}&image=https%3A%2F%2Fi.ytimg.com%2Fvi%2FFlFTcDSKnLM%2Fhqdefault.jpg&key=c4e54deccf4d4ec997a64902e9a30300&autoplay=1&type=text%2Fhtml&schema=youtube\'][width=\'940\']`)
+                                        m('#ytvideo.embedly-embed.w-lightbox-embed', {config: ctrl.setYoutube})
                                     ])
                                 ])
                             ]),
                             m('.w-lightbox-spinner.w-lightbox-hide'),
                             m('.w-lightbox-control.w-lightbox-left.w-lightbox-inactive'),
                             m('.w-lightbox-control.w-lightbox-right.w-lightbox-inactive'),
-                            m('#youtube-close.w-lightbox-control.w-lightbox-close', {onclick: ctrl.showLightbox.toggle})
+                            m('#youtube-close.w-lightbox-control.w-lightbox-close', {onclick: ctrl.closeVideo})
                         ]),
                         m('.w-lightbox-strip')
                     ])
-                ]) : ''
+                ])
             ]);
         }
     };
