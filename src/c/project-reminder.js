9c83959d3e28dd4eef8b1bf3e9a0d56cd95e9eeb
Vinícius Andrade <vchavesster@gmail.com>
2016-02-03 20:38:10 -0200

    Fixes more reference issues

--- src/c/project-reminder.js
+++ src/c/project-reminder.js
@@ -11,11 +11,11 @@
 window.c.ProjectReminder = ((m, models, h, c) => {
     return {
         controller: (args) => {
-            let project = args.project(),
+            let project = args.project,
                 filterVM = m.postgrest.filtersVM({
                     project_id: 'eq'
                 }),
-                storeReminderName = 'remind_' + project.id,
+                storeReminderName = 'remind_' + project().id,
                 l = m.prop(false),
                 popNotification = m.prop(false),
                 submitReminder = () => {
@@ -23,15 +23,15 @@ window.c.ProjectReminder = ((m, models, h, c) => {
                         h.storeAction(storeReminderName, submitReminder);
                         return h.navigateToDevise();
                     }
-                    let loaderOpts = project.in_reminder ? models.projectReminder.deleteOptions(filterVM.parameters()) : models.projectReminder.postOptions({
-                        project_id: project.id
+                    let loaderOpts = project().in_reminder ? models.projectReminder.deleteOptions(filterVM.parameters()) : models.projectReminder.postOptions({
+                        project_id: project().id
                     });
                     l = m.postgrest.loaderWithToken(loaderOpts);
 
                     l.load().then(() => {
-                        project.in_reminder = !project.in_reminder;
+                        project().in_reminder = !project().in_reminder;
 
-                        if (project.in_reminder) {
+                        if (project().in_reminder) {
                             popNotification(true);
                             setTimeout(() => {
                                 popNotification(false);
@@ -44,7 +44,7 @@ window.c.ProjectReminder = ((m, models, h, c) => {
                 };
 
             h.callStoredAction(storeReminderName, submitReminder);
-            filterVM.project_id(project.id);
+            filterVM.project_id(project().id);
 
             return {
                 l: l,
@@ -56,14 +56,14 @@ window.c.ProjectReminder = ((m, models, h, c) => {
             const mainClass = (args.type === 'button') ? '' : '.u-text-center.u-marginbottom-30',
                 buttonClass = (args.type === 'button') ? 'w-button btn btn-terciary btn-no-border' : 'btn-link link-hidden fontsize-small',
                 hideTextOnMobile = args.hideTextOnMobile || false,
-                project = args.project();
+                project = args.project;
 
             return m(`#project-reminder${mainClass}`, [
-                m(`button[class="${buttonClass} ${(project.in_reminder ? 'link-hidden-success' : 'fontcolor-secondary')} fontweight-semibold"]`, {
+                m(`button[class="${buttonClass} ${(project().in_reminder ? 'link-hidden-success' : 'fontcolor-secondary')} fontweight-semibold"]`, {
                     onclick: ctrl.submitReminder
                 }, [
                     (ctrl.l() ? 'aguarde ...' : m('span.fa.fa-clock-o', [
-                        m(`span${hideTextOnMobile ? '.w-hidden-medium' : ''}`, project.in_reminder ? ' Lembrete ativo' : ' Lembrar-me')
+                        m(`span${hideTextOnMobile ? '.w-hidden-medium' : ''}`, project().in_reminder ? ' Lembrete ativo' : ' Lembrar-me')
                     ]))
                 ]), (ctrl.popNotification() ? m.component(c.PopNotification, {
                     message: 'Ok! Vamos te mandar um lembrete por e-mail 48 horas antes do fim da campanha'
