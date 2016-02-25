d99afa1a08459fdc2aa3abb37a7367e7ef4ae25d
Antônio Roberto <forevertonny@gmail.com>
2016-02-25 11:21:56 -0300

    use new stats component instead details card

--- src/root/insights.js
+++ src/root/insights.js
@@ -115,9 +115,6 @@ window.c.root.Insights = ((m, c, h, models, _, I18n) => {
                         m('.w-col.w-col-2'),
                         m('.w-col.w-col-8.dashboard-header.u-text-center', [
                             m('.fontweight-semibold.fontsize-larger.lineheight-looser.u-marginbottom-10', I18n.t('campaign_title', I18nScope())),
-                            m.component(c.AdminProjectDetailsCard, {
-                                resource: project
-                            }),
                             m('p.' + project.state + '-project-text.fontsize-small.lineheight-loose', [
                                 project.mode === 'flex' && _.isNull(project.expires_at) && project.state !== 'draft' ? m('span', [I18n.t('finish_explanation', I18nScope()),
                                    m('a.alt-link[href="http://suporte.catarse.me/hc/pt-br/articles/206507863-Catarse-flex-Principais-perguntas-e-respostas-"][target="_blank"]', I18n.t('know_more', I18nScope()))]) : m.trust(I18n.t(`campaign.${project.mode}.${project.state}`, I18nScope({username: project.user.name, expires_at: h.momentify(project.zone_expires_at), sent_to_analysis_at: h.momentify(project.sent_to_analysis_at)})))
@@ -129,6 +126,7 @@ window.c.root.Insights = ((m, c, h, models, _, I18n) => {
                     m('.divider'),
                     m('.w-section.section-one-column.section.bg-gray.before-footer', [
                         m('.w-container', [
+                            m.component(c.ProjectDataStats, {project: project}),
                             m('.w-row', [
                                 m('.w-col.w-col-12.u-text-center', {
                                     style: {
