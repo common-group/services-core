c7e919b11001fa68bbc8c041683f8ac44ae9c461
Antônio Roberto <forevertonny@gmail.com>
2016-03-04 19:51:47 -0300

    use api data

--- src/c/project-successful-onboard-error-account.js
+++ src/c/project-successful-onboard-error-account.js
@@ -12,7 +12,8 @@
 window.c.ProjectSuccessfulOnboardErrorAccount = ((m, c, h) => {
     return {
         view: (ctrl, args) => {
-            const projectTransfer = args.projectTransfer;
+            const projectTransfer = args.projectTransfer,
+                  projectAccount = args.projectAccount;
 
             return m('.w-container', [
                 m('.w-row.u-marginbottom-40', [
@@ -22,9 +23,9 @@ window.c.ProjectSuccessfulOnboardErrorAccount = ((m, c, h) => {
                             m('img.u-marginbottom-20', {src: 'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/56c350fabfc50aef5095c67d_transfer-pending-icon.png', width: 94}),
                             m('.fontsize-large.fontweight-semibold.u-marginbottom-20', 'Transferência bancária pendente'),
                             m('.fontsize-base.u-marginbottom-30', [
-                                'Seus dados bancários estavam com alguma informação errada e já fomos notificados sobre esse problema! Fique de olho no&nbsp;',
-                                m('span.fontweight-semibold', `fulano@gmail.com`),
-                                '&nbsp;, pois vamos resolver esse problema via email!'
+                                'Seus dados bancários estavam com alguma informação errada e já fomos notificados sobre esse problema! Fique de olho no ',
+                                m('span.fontweight-semibold', projectAccount.user_email),
+                                ' , pois vamos resolver esse problema via email!'
                             ])
                         ])
                     ]),
--- src/c/project-successful-onboard-pending-transfer.js
+++ src/c/project-successful-onboard-pending-transfer.js
@@ -12,7 +12,8 @@
 window.c.ProjectSuccessfulOnboardPendingTransfer = ((m, c, h) => {
     return {
         view: (ctrl, args) => {
-            const projectTransfer = args.projectTransfer;
+            const projectTransfer = args.projectTransfer,
+                  projectAccount = args.projectAccount;
 
             return m('.w-container', [
                 m('.w-row.u-marginbottom-40', [
@@ -22,7 +23,7 @@ window.c.ProjectSuccessfulOnboardPendingTransfer = ((m, c, h) => {
                             m('img.u-marginbottom-20', {src: 'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/56c350c5e7d136a016bc34a4_transfer-ok-icon.png', width: 94}),
                             m('.fontsize-large.fontweight-semibold.u-marginbottom-20', 'Transferência bancária em andamento'),
                             m('.fontsize-base.u-marginbottom-30', [
-                                'Até o dia 28/02/2015, R$52.000 (taxas já descontadas) estarão em sua conta Banco Itaú, Agência:2810-x, Conta:23232-2. Fique atento no email fulano@gmail.com , pois nossa equipe pode entrar em contato com você se ocorrer algum problema com o banco!'
+                                `Até o dia ${projectAccount.transfer_limit_date}, R$${h.formatNumber(projectTransfer.total_amount, 2)} (taxas já descontadas) estarão em sua conta Banco ${projectAccount.bank_name}, Agência:${projectAccount.agency}${(_.isEmpty(projectAccount.agency_digit) ? '' : `-${projectAccount.agency_digit}`)}, Conta:${projectAccount.account}-${projectAccount.account_digit}. Fique atento no email ${projectAccount.user_email} , pois nossa equipe pode entrar em contato com você se ocorrer algum problema com o banco!`
                             ])
                         ])
                     ]),
