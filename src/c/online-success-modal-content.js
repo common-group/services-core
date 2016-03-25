e2824143c2073b88248e5cfaf503227f94447ae9
Adriano Benin <adriano.benin@gmail.com>
2016-03-24 21:29:45 -0300

    add online success modal

---
+++ src/c/online-success-modal-content.js
@@ -0,0 +1,14 @@
+/**
+ * window.c.OnlineSucessModalContent component
+ * Render online success message
+ *
+ */
+window.c.OnlineSucessModalContent = ((m, h, _, models, I18n) => {
+    return {
+        view: (ctrl, args) => {
+          return m('.modal-dialog-content.u-text-center',
+                   [m('.fa.fa-check-circle.fa-5x.text-success.u-marginbottom-40'),
+                     m('p.fontsize-larger.lineheight-tight', 'Sua campanha está no ar!!! Parabéns por esse primeiro grande passo. Boa sorte nessa jornada ;)')]);
+        }
+    };
+}(window.m, window.c.h, window._, window.c.models, window.I18n));
