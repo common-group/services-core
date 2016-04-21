ab4460e8a82c9f25a658284c1466eed325d9ede7
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-21 13:03:22 -0300

    added basic filters to contribution report

--- src/c/project-contribution-report-header.js
+++ src/c/project-contribution-report-header.js
@@ -1,6 +1,10 @@
 window.c.ProjectContributionReportHeader = ((m, c, _, h) => {
     return {
         view: (cltr, args) => {
+            const filterBuilder = args.filterBuilder,
+                  paymentStateFilter =  _.findWhere(filterBuilder, {label: 'payment_state'}),
+                  rewardFilter = _.findWhere(filterBuilder, {label: 'reward_filter'}),
+                  mainFilter = _.findWhere(filterBuilder, {component: 'FilterMain'});
             return m('.w-section.dashboard-header', [
                 m('.w-container', [
                     m('.w-row', [
@@ -10,39 +14,20 @@ window.c.ProjectContributionReportHeader = ((m, c, _, h) => {
                         m('.w-col.w-col-3')
                     ]),
                     m('.w-form', [
-                        m('form', [
+                        m('form', {onsubmit: args.submit}, [
                             m('.w-row', [
-                                m('.w-col.w-col-4', [
+                                m('.w-col.w-col-6', [
                                     m('.w-row', [
-                                        m('.w-col.w-col-6.w-col-small-6.w-col-tiny-6._w-sub-col-middle', [
-                                            m('select.w-select.text-field', [
-                                                m('option', 'Recompensas'),
-                                            ])
-                                        ]),
-                                        m('.w-col.w-col-6.w-col-small-6.w-col-tiny-6._w-sub-col', [
-                                            m('select.w-select.text-field', [
-                                                m('option', 'Status do apoio'),
-                                            ])
-                                        ])
+                                        m.component(c[paymentStateFilter.component], paymentStateFilter.data),
+                                            m.component(c[rewardFilter.component], rewardFilter.data)
                                     ])
                                 ]),
-                                m('.w-col.w-col-8', [
+                                m('.w-col.w-col-6.u-margintop-20', [
                                     m('.w-row', [
-                                        m('.w-col.w-col-4._w-sub-col', [
-                                            m('input.w-input.text-field[placeholder="Busque por nome ou email do apoiador"][required="required"][type="text"]')
-                                        ]),
-                                        m('.w-col.w-col-3', [
-                                            m('a.btn.btn-medium[href="#"]', 'Buscar')
+                                        m('.w-col.w-col-7._w-sub-col', [
+                                            m.component(c[mainFilter.component], mainFilter.data)
                                         ]),
                                         m('.w-col.w-col-5.w-clearfix.w-hidden-small.w-hidden-tiny', [
-                                            m('.dropdown-list.card.u-radius.dropdown-list-medium.zindex-10.u-margintop-40.absolute-right', {style: {'display': ' block'}}, [
-                                                m('.w-form', [
-                                                    m('select.w-select.text-field', [
-                                                        m('option', 'Recompensas'),
-                                                    ]),
-                                                    m('input.w-button.btn.btn-small[type="submit"][value="Baixar"]')
-                                                ])
-                                            ]),
                                             m('a.alt-link.u-right.fontsize-small.lineheight-looser[href="#"]', [
                                                 m('span.fa.fa-download', '.'),
                                                 ' Baixar relatórios'
@@ -51,12 +36,6 @@ window.c.ProjectContributionReportHeader = ((m, c, _, h) => {
                                     ])
                                 ])
                             ])
-                        ]),
-                        m(".w-form-done", [
-                            m("p", "Thank you! Your submission has been received!")
-                        ]),
-                        m(".w-form-fail", [
-                            m("p", "Oops! Something went wrong while submitting the form :(")
                         ])
                     ])
                 ])
--- src/root/projects-contribution-report.js
+++ src/root/projects-contribution-report.js
@@ -1,13 +1,64 @@
 window.c.root.ProjectsContributionReport = ((m, c, _, h, models) => {
     return {
         controller: (args) => {
-            const listVM = m.postgrest.paginationVM(models.projectContribution,
-                                                    'id.desc', {'Prefer': 'count=exact'}),
-                  filterVM = m.postgrest.filtersVM({
-                      project_id: 'eq',
-                      state: 'eq',
-                      reward_id: 'eq'
-                  });
+            const listVM = m.postgrest.paginationVM(models.projectContribution, 'id.desc', {'Prefer': 'count=exact'}),
+                  filterVM = c.root.ProjectsContributionReportVM,
+                  filterBuilder = [
+                      {
+                          component: 'FilterMain',
+                          data: {
+                              inputWrapperClass: '.w-input.text-field',
+                              btnClass: '.btn.btn-medium',
+                              vm: filterVM.full_text_index,
+                              placeholder: 'Busque por nome ou email do apoiador'
+                          }
+                      }, {
+                          label: 'reward_filter',
+                          component: 'FilterDropdown',
+                          data: {
+                              label: 'Recompensa',
+                              name: 'reward_id',
+                              vm: filterVM.reward_id,
+                              wrapper_class: '.w-col.w-col-6.w-col-small-6.w-col-tiny-6._w-sub-col-middle',
+                              options: []
+                          }
+                      }, {
+                          label: 'payment_state',
+                          component: 'FilterDropdown',
+                          data: {
+                              label: 'Status do apoio',
+                              name: 'state',
+                              vm: filterVM.state,
+                              wrapper_class: '.w-col.w-col-6.w-col-small-6.w-col-tiny-6._w-sub-col-middle',
+                              options: [{
+                                  value: '',
+                                  option: 'Status do apoio'
+                              }, {
+                                  value: 'paid',
+                                  option: 'Pago'
+                              }, {
+                                  value: 'refused',
+                                  option: 'Cancelado'
+                              }, {
+                                  value: 'pending',
+                                  option: 'Pendente'
+                              }, {
+                                  value: 'pending_refund',
+                                  option: 'Reembolso pendente'
+                              }, {
+                                  value: 'refunded',
+                                  option: 'Reembolsado'
+                              }, {
+                                  value: 'chargeback',
+                                  option: 'Contestado'
+                              }]
+                          }
+                      }
+                  ],
+                  submit = () => {
+                      listVM.firstPage(filterVM.parameters()).then(null);
+                      return false;
+                  };
 
             filterVM.project_id(args.root.getAttribute('data-id'));
 
@@ -17,14 +68,20 @@ window.c.root.ProjectsContributionReport = ((m, c, _, h, models) => {
 
             return {
                 listVM: listVM,
-                filterVM: filterVM
+                filterVM: filterVM,
+                filterBuilder: filterBuilder,
+                submit: submit
             };
         },
         view: (ctrl, args) => {
             const list = ctrl.listVM;
 
             return [
-                m.component(c.ProjectContributionReportHeader),
+                m.component(c.ProjectContributionReportHeader, {
+                    submit: ctrl.submit,
+                    filterBuilder: ctrl.filterBuilder,
+                    form: ctrl.filterVM.formDescriber
+                }),
                 m('.divider.u-margintop-30'),
                 m.component(c.ProjectContributionReportContent, {
                     list: list
