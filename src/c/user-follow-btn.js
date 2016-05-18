cdee0c8112659db8dee66dbf823201eb3e18d833
Antônio Roberto Silva <forevertonny@gmail.com>
2016-05-18 01:31:16 -0300

    fiz user follow js to es2015

--- src/c/user-follow-btn.js
+++ src/c/user-follow-btn.js
@@ -5,57 +5,65 @@
  * Example:
  * m.component(c.UserFollowBtn, {follow_id: 10, following: false})
  */
-window.c.UserFollowBtn = ((m, h, _, models) => {
-    return {
-        controller: (args) => {
-            const following = m.prop((args.following||false)),
-                  followVM = m.postgrest.filtersVM({follow_id: 'eq'}),
-                  loading = m.prop(false),
-                  userFollowInsert = models.userFollow.postOptions({
-                      follow_id: args.follow_id}),
-                  userFollowDelete = (() => {
-                      followVM.follow_id(args.follow_id);
 
-                      return models.userFollow.deleteOptions(
-                          followVM.parameters());
-                  }()),
-                  follow = () => {
-                      const l = m.postgrest.loaderWithToken(userFollowInsert);
-                      loading(true);
+import m from 'mithril';
+import postgrest from 'mithril-postgrest';
+import h from '../h';
+import models from '../models';
 
-                      l.load().then(() => {
-                          following(true);
-                          loading(false);
-                      });
-                  },
-                  unfollow = () => {
-                      const l = m.postgrest.loaderWithToken(userFollowDelete);
-                      loading(true);
 
-                      l.load().then(() => {
-                          following(false);
-                          loading(false);
-                      });
-                  };
+const UserFollowBtn = {
+    controller(args) {
+        const following = m.prop((args.following||false)),
+              followVM = postgrest.filtersVM({follow_id: 'eq'}),
+              loading = m.prop(false),
+              userFollowInsert = models.userFollow.postOptions({
+                  follow_id: args.follow_id}),
+              userFollowDelete = (() => {
+                  followVM.follow_id(args.follow_id);
 
-            return {
-                following: following,
-                follow: follow,
-                unfollow: unfollow,
-                loading: loading
-            };
-        },
-        view: (ctrl, args) => {
-            if (ctrl.loading()) { return h.loader(); }
-            if (ctrl.following()) {
-                return m('a.w-button.btn.btn-medium.u-margintop-20',
-                         {onclick: ctrl.unfollow},
-                         'Deixar de seguir');
-            } else {
-                return m('a.w-button.btn.btn-medium.btn-terciary.u-margintop-20',
-                         {onclick: ctrl.follow},
-                         'Seguir');
-            }
+                  return models.userFollow.deleteOptions(
+                      followVM.parameters());
+              })(),
+              follow = () => {
+                  const l = postgrest.loaderWithToken(userFollowInsert);
+                  loading(true);
+
+                  l.load().then(() => {
+                      following(true);
+                      loading(false);
+                  });
+              },
+              unfollow = () => {
+                  const l = postgrest.loaderWithToken(userFollowDelete);
+                  loading(true);
+
+                  l.load().then(() => {
+                      following(false);
+                      loading(false);
+                  });
+              };
+
+        return {
+            following: following,
+            follow: follow,
+            unfollow: unfollow,
+            loading: loading
+        };
+    },
+    view(ctrl, args) {
+        if (ctrl.loading()) { return h.loader(); }
+        if (ctrl.following()) {
+            return m('a.w-button.btn.btn-medium.u-margintop-20',
+                     {onclick: ctrl.unfollow},
+                     'Deixar de seguir');
+        } else {
+            return m('a.w-button.btn.btn-medium.btn-terciary.u-margintop-20',
+                     {onclick: ctrl.follow},
+                     'Seguir');
         }
-    };
-}(window.m, window.c.h, window._, window.c.models));
+    }
+};
+
+
+export default UserFollowBtn;
