d12ddbb0969d50cdfd2abcff46cea7fcdd046c27
Vinícius Andrade <vchavesster@gmail.com>
2016-05-31 17:30:02 -0300

    Removes header transparency on projects page

--- src/root/menu.js
+++ src/root/menu.js
@@ -13,11 +13,14 @@ const menu = {
         };
     },
     view(ctrl, args) {
-        return m("header.main-header.w-section.overlayer",
+        return m("header.main-header.w-section",{
+            class: args.menuTransparency ? 'overlayer' : ''
+        },
             [
                 m(".w-clearfix",
                     [
-                        m("a.header-logo.w-nav-brand[href='https://www.catarse.me/pt'][title='Catarse']",
+                        m("a.header-logo.w-nav-brand[href='javascript:void(0);'][title='Catarse']",
+                            {onclick: () => m.route('/')},
                             m("img[alt='Logo big'][src='/assets/catarse_bootstrap/logo_big-8726f3436ac23c97959d3f7bf10365b7.png']")
                         ),
                         m("a.w-hidden-small.w-hidden-tiny.header-link.w-nav-link[href='javascript:void(0);']",{onclick: () => m.route('/start')}, "Comece seu projeto"
--- src/root/projects-show.js
+++ src/root/projects-show.js
@@ -10,26 +10,29 @@ import projectDashboardMenu from '../c/project-dashboard-menu';
 const projectsShow = {
     controller(args) {
         h.analytics.windowScroll({cat: 'project_view',act: 'project_page_scroll'});
+
         return projectVM(args.project_id, args.project_user_id);
     },
     view(ctrl) {
-        const project = ctrl.projectDetails;
+        const project = ctrl.projectDetails || m.prop();
+
+        console.log('I am rendering with ', project() ? 'project' : 'h.getProject');
 
         return m('.project-show', [
                 m.component(projectHeader, {
-                    project: project,
+                    project: project() ? project : h.getProject(),
                     userDetails: ctrl.userDetails
                 }),
                 m.component(projectTabs, {
-                    project: project,
+                    project: project() ? project : h.getProject(),
                     rewardDetails: ctrl.rewardDetails
                 }),
                 m.component(projectMain, {
-                    project: project,
+                    project: project() ? project : h.getProject(),
                     rewardDetails: ctrl.rewardDetails
                 }),
                 (project() && project().is_owner_or_admin ? m.component(projectDashboardMenu, {
-                    project: project
+                    project: project() ? project : h.getProject()
                 }) : '')
             ]);
     }
