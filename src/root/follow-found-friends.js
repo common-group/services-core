2654fa1b3a076f423fa435fb4219887ba891804e
Adriano Benin <adriano.benin@gmail.com>
2016-07-20 18:40:12 -0300

    add follow card component

--- src/root/follow-found-friends.js
+++ src/root/follow-found-friends.js
@@ -3,7 +3,7 @@ import postgrest from 'mithril-postgrest';
 import _ from 'underscore';
 import h from '../h';
 import models from '../models';
-import UserFollowBtn from  '../c/user-follow-btn';
+import UserFollowCard from  '../c/user-follow-card';
 
 const FollowFoundFriends = {
     controller(args) {
@@ -66,32 +66,11 @@ const FollowFoundFriends = {
                              },`Siga todos os seus ${listVM.total() ? listVM.total() : ''} amigos`))
                         ])
                     ]),
-
+                    m(".w-row", [
                     _.map(listVM.collection(), (friend) => {
-                        const profile_img = _.isEmpty(friend.avatar) ? '/assets/catarse_bootstrap/user.jpg' : friend.avatar;
-
-                        return m('.card.card-clickable', [
-                            m('.w-row', [
-                                m('.w-col.w-col-9.w-col-small-9', [
-                                    m('.w-row', [
-                                        m('.w-col.w-col-2.w-col-small-2.w-col-tiny-6', [
-                                            m(`img.thumb.u-round.u-marginright-20[src='${profile_img}']`)
-                                        ]),
-                                        m('.w-col.w-col-10.w-col-small-10.w-col-tiny-6', [
-                                            m('.fontsize-base.fontweight-semibold', friend.name),
-                                            (_.isNull(friend.city) ? '' :
-                                             m('.fontsize-smaller.fontcolor-secondary.u-marginbottom-10', `${friend.city}, ${friend.state}`)),
-                                            m('.fontsize-small', [
-                                                m('span.fontweight-semibold',friend.total_contributed_projects),' apoiados'
-                                            ])
-                                        ])
-                                    ])
-                                ]),
-                                m('.w-col.w-col-3.w-col-small-3', m.component(UserFollowBtn, {following: friend.following, follow_id: friend.friend_id}))
-                            ])
-                        ]);
-                    }),
-
+                      return m.component(UserFollowCard, {friend: friend});
+                      }),
+                    ]),
                     m('.w-section.section.bg-gray', [
                         m('.w-container', [
                             m('.w-row.u-marginbottom-60', [
