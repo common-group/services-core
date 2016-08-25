2e17919915286996a79542ada218e89624060ccc
Antônio Roberto Silva <forevertonny@gmail.com>
2016-08-25 10:48:46 -0300

    added find friends link to row [#118758693]

--- src/c/project-row.js
+++ src/c/project-row.js
@@ -15,11 +15,17 @@ const projectRow = {
             return m(wrapper, [
                 m('.w-container', [
                     (!_.isUndefined(collection.title) || !_.isUndefined(collection.hash)) ? m('.w-row.u-marginbottom-30', [
-                        m('.w-col.w-col-10.w-col-small-6.w-col-tiny-6', [
+                        m((showFriends ? '.w-col.w-col-8.w-col-small-6.w-col-tiny-6' : '.w-col.w-col-10.w-col-small-6.w-col-tiny-6'), [
                             m('.fontsize-large.lineheight-looser', title)
                         ]),
-                        m('.w-col.w-col-2.w-col-small-6.w-col-tiny-6', [
-                            m(`a.btn.btn-small.btn-terciary[href="/pt/explore?ref=${ref}#${collection.hash}"]`, 'Ver todos')
+                        m((showFriends ? '.w-col.w-col-4.w-col-small-6.w-col-tiny-6' : '.w-col.w-col-2.w-col-small-6.w-col-tiny-6'), [
+                            m('.w-row', [
+                                (showFriends ? m('.w-col.w-col-6', [
+                                    m(`a.btn.btn-no-border.btn-small.btn-terciary[href="/connect-facebook?ref=${ref}"]`, "Encontrar amigos")
+                                ]) : ''),
+                                m((showFriends ? '.w-col.w-col-6' : '.w-col.w-col-12'),
+                                  m(`a.btn.btn-small.btn-terciary[href="/pt/explore?ref=${ref}#${collection.hash}"]`, 'Ver todos'))
+                            ])
                         ])
                     ]) : '',
                     collection.loader() ? h.loader() : m('.w-row', _.map(collection.collection(), (project) => {
