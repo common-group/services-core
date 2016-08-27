e726dfee37712c6303730b7547c37f670ec5687b
Antônio Roberto Silva <forevertonny@gmail.com>
2016-08-26 22:21:25 -0300

    added fallback contribution route

--- src/c/project-reward-list.js
+++ src/c/project-reward-list.js
@@ -26,7 +26,8 @@ const projectRewardList = {
                 //     return h.navigateToDevise('/' + projectVM.currentProject().permalink);
                 // } else {
                     vm.contributionValue(valueFloat);
-                    h.navigateTo(`/projects/${projectVM.currentProject().id}/contributions/new?reward_id=${vm.selectedReward().id}`);
+                    //h.navigateTo(`/projects/${projectVM.currentProject().id}/contributions/new?reward_id=${vm.selectedReward().id}`);
+                h.navigateTo(`/projects/${projectVM.currentProject().project_id}/contributions/fallback_create?contribution%5Breward_id%5D=${vm.selectedReward().id}&contribution%5Bvalue%5D=${valueFloat}`);
                     // m.route(`/projects/${projectVM.currentProject().id}/payment`, {
                     //    project_user_id: projectVM.currentProject().user_id
                     // });
