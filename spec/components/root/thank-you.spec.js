7c9ea895942b946b1bf3859dc5fea8162891ccbc
Vinicius Andrade <vini.andrade.dev@gmail.com>
2016-10-17 22:38:35 -0200

    Adjusts specs

--- spec/components/root/thank-you.spec.js
+++ spec/components/root/thank-you.spec.js
@@ -1,33 +1,43 @@
 import thankYou from '../../../src/root/thank-you'
 
 fdescribe('ThankYou', () => {
-    let $output;
-    let test = {
-
+    let $slip, $cc;
+    let test = (payment) => {
+        return {
+            contribution: ContributionAttrMockery(null, payment)
+        };
     };
 
     beforeAll(() => {
-        $output = mq(m(thankYou, test));
+        $slip = mq(m(thankYou, test('slip')));
+        $cc = mq(m(thankYou, test('creditcard')));
     });
 
     it('should render a thank you page', () => {
-        $output.should.have('#thank-you');
+        $slip.should.have('#thank-you');
+        $cc.should.have('#thank-you');
     });
 
-    it('should render a thank you message if not slip payment', () => {
-        $output.should.have('.thank-you');
+    it('should render a specific message according to payment type', () => {
+        $cc.should.have('#creditcard-thank-you');
+        $cc.should.not.have('#slip-thank-you');
+        $slip.should.have('#slip-thank-you');
+        $slip.should.not.have('#creditcard-thank-you');
     });
 
-    it('should render share buttons', () => {
+    it('should render share buttons if credit card', () => {
         // 3 desktop share buttons
-        expect($output.find('.btn-large').length).toEqual(3)
+        expect($cc.find('.btn-large').length).toEqual(4)
+        expect($slip.find('.btn-large').length).toEqual(0);
     });
 
     it('should render 3 recommended projects if not slip payment', () => {
-        expect($output.find('.project-card').length).toEqual(3);
+        expect($cc.find('.card-project').length).toEqual(3);
+        expect($slip.find('.card-project').length).toEqual(0);
     });
 
     it('should render the slip iframe if slip payment', () => {
-        expect($output.has('iframe.slip')).toBeTrue();
+        expect($slip.has('iframe.slip')).toBeTrue();
+        expect($cc.has('iframe.slip')).toBeFalse();
     });
 });
