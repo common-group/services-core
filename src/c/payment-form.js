dbdceb0e739da348badccc2c130a8756a300acc6
Vinicius Andrade <vini.andrade.dev@gmail.com>
2016-10-26 16:48:47 -0200

    Fixes payment option click behaviour

--- src/c/payment-form.js
+++ src/c/payment-form.js
@@ -5,8 +5,10 @@ import paymentCreditCard from './payment-credit-card';
 
 const paymentForm = {
     controller() {
+        const isSlip = m.prop(false);
+
         return {
-            toggleBoleto: h.toggleProp(false, true)
+            isSlip: isSlip
         };
     },
     view(ctrl, args) {
@@ -22,8 +24,8 @@ const paymentForm = {
             ]),
             m('.flex-row.u-marginbottom-40', [
                 m('a.w-inline-block.btn-select.flex-column.u-marginbottom-20.u-text-center[href=\'javascript:void(0);\']', {
-                    onclick: ctrl.toggleBoleto.toggle,
-                    class: !ctrl.toggleBoleto() ? 'selected' : ''
+                    onclick: () => ctrl.isSlip(false),
+                    class: !ctrl.isSlip() ? 'selected' : ''
                 }, [
                     m('.fontsize-base.fontweight-semibold',
                         'Cartão de crédito'
@@ -34,15 +36,15 @@ const paymentForm = {
                     m('img[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/57299bd8f326a24d4828a0fd_credit-cards.png\']')
                 ]),
                 !args.vm.isInternational() ? m('a.w-inline-block.btn-select.flex-column.u-marginbottom-20.u-text-center[href=\'javascript:void(0);\']', {
-                    onclick: ctrl.toggleBoleto.toggle,
-                    class: ctrl.toggleBoleto() ? 'selected' : ''
+                    onclick: () => ctrl.isSlip(true),
+                    class: ctrl.isSlip() ? 'selected' : ''
                 }, [
                     m('.fontsize-base.fontweight-semibold.u-marginbottom-20',
                         'Boleto bancário'
                     ),
                     m('img[src=\'https://daks2k3a4ib2z.cloudfront.net/54b440b85608e3f4389db387/57299c6ef96a6e44489a7a07_boleto.png\'][width=\'48\']')
                 ]) : m('.flex-column')
-            ]), !ctrl.toggleBoleto() ? m('#credit-card-section', [
+            ]), !ctrl.isSlip() ? m('#credit-card-section', [
                 m.component(paymentCreditCard, {vm: args.vm, contribution_id: args.contribution_id, project_id: args.project_id, user_id: args.user_id})
             ]) : !args.vm.isInternational() ? m('#boleto-section', [
                 m.component(paymentSlip, {vm: args.vm, contribution_id: args.contribution_id, project_id: args.project_id})
