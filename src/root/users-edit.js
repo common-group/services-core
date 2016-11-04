a1fd96401a8a16ce2b341b997678aa5bed36ff85
Adriano Benin <adriano.benin@gmail.com>
2016-11-04 10:16:26 -0200

    add users edit root

---
+++ src/root/users-edit.js
@@ -0,0 +1,40 @@
+import m from 'mithril';
+import _ from 'underscore';
+import h from '../h';
+import userVM from '../vms/user-vm';
+import userHeader from '../c/user-header';
+import userCreated from '../c/user-created';
+import menu from '../root/menu';
+
+const usersEdit = {
+    controller(args) {
+        const userDetails = m.prop({}),
+            user_id = args.user_id;
+
+        //@TODO remove this after migrating all tabs to API
+        const moveTabContent = () => {
+          $('#created-tab').appendTo('#dashboard_projects');
+        };
+
+        userVM.fetchUser(user_id, true, userDetails);
+        return {
+          userDetails: userDetails,
+          moveTabContent: moveTabContent
+        };
+    },
+
+    view(ctrl, args) {
+        const user = ctrl.userDetails();
+
+        return m('div', {config: ctrl.moveTabContent()}, [
+          m.component(menu, {menuTransparency: true}),
+          m.component(userHeader, {user: user, hideDetails: true}),
+          (!_.isEmpty(user) ? 
+              m.component(userCreated, {userId: user.id})
+           : '')
+      ]);
+
+    }
+};
+
+export default usersEdit;
