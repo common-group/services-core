8fa6d58cd2d267605fceae8176da2ed198a05676
Bruno Grossi <bruno@grossi.com.br>
2016-11-07 20:57:10 -0200

    Adicionado chat na página do mola2

--- src/root/projects-show.js
+++ src/root/projects-show.js
@@ -25,6 +25,27 @@ const projectsShow = {
     view(ctrl, args) {
         const project = ctrl.currentProject;
 
+        if(project && project.project_id && _.contains([29706], project.project_id)) {
+            try {
+                (window.$zopim && window.$zopim.livechat)||(function(d,s){var z=window.$zopim=function(c){z._.push(c)},$=z.s=d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set._.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');$.src='//v2.zopim.com/?2qPtIfZX0Exh5Szx5JUoUxWKqrTQI5Tm';z.t=+new Date;$.type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+                setTimeout(function t(){
+                    const c = window.$zopim && window.$zopim.livechat;
+                    if(c) {
+                        const u = h.getUser();
+                        if(u) {
+                            c.setEmail(u.email);
+                            c.setName(u.name);
+                        }
+                        window.zE && window.zE.hide();
+                    } else {
+                        setTimeout(t, 1000);
+                    }
+                }, 1000);
+            } catch(e) {
+                console.error(e);
+            }
+        }
+
         return m('.project-show',{
                 config: ctrl.setProjectPageTitle()
             },[
