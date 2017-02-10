49b754d33362e5eec24e0e5be1fac17038fcdb85
Antônio Roberto <forevertonny@gmail.com>
2017-02-10 13:15:43 -0200

    dont show check for recent users

--- src/root/check-email.js
+++ src/root/check-email.js
@@ -1,4 +1,5 @@
 import m from 'mithril';
+import moment from 'moment';
 import I18n from 'i18n-js';
 import _ from 'underscore';
 import h from '../h';
@@ -41,28 +42,34 @@ const CheckEmail = {
 
     view(ctrl, args) {
         let user = ctrl.user();
-        return ((user && !user.email_active && !ctrl.hideAlert()) ? m('.card-alert.section.u-text-center', { style: (args.menuTransparency ? {'padding-top': '100px'} : {}) },[
-            m('.w-container', (ctrl.confirmedEmail() ? [
-                m('.fontsize-large.fontweight-semibold', I18n.t('confirmed_title', I18nScope())),
-                m('.fontsize-large.fontweight-semibold.u-marginbottom-20', I18n.t('confirmed_sub', I18nScope())),
-            ] : [
-                m('.fontsize-large.fontweight-semibold', _.isNull(user.name)?"Olá":I18n.t('hello', I18nScope({name: user.name}))),
-                m('.fontsize-large.fontweight-semibold.u-marginbottom-20', I18n.t('hello_sub', I18nScope())),
-                m('.fontsize-base.u-marginbottom-10', I18n.t('hello_email', I18nScope({email: user.email}))),
-                m('.w-row', [
-                    m('.w-col.w-col-3'),
-                    m('.w-col.w-col-3', [
-                        m('a.btn.btn-medium.btn-terciary.w-button[href="javacript:void(0);"]', {
-                            onclick: ctrl.checkEmail
-                        }, 'Sim!')
-                    ]),
-                    m('.w-col.w-col-3', [
-                        m(`a.btn.btn-medium.w-button[href="/users/${user.id}/edit#about_me"]`,'Editar o email')
-                    ]),
-                    m('.w-col.w-col-3')
-                ])
-            ]))
-        ]) : m('div'));
+        if(user) {
+            let userCreatedRecently = moment().isBefore(moment(user.created_at).add(2, 'days'));
+
+            return ((user && !userCreatedRecently && !user.email_active && !ctrl.hideAlert()) ? m('.card-alert.section.u-text-center', { style: (args.menuTransparency ? {'padding-top': '100px'} : {}) },[
+                m('.w-container', (ctrl.confirmedEmail() ? [
+                    m('.fontsize-large.fontweight-semibold', I18n.t('confirmed_title', I18nScope())),
+                    m('.fontsize-large.fontweight-semibold.u-marginbottom-20', I18n.t('confirmed_sub', I18nScope())),
+                ] : [
+                    m('.fontsize-large.fontweight-semibold', _.isNull(user.name)?"Olá":I18n.t('hello', I18nScope({name: user.name}))),
+                    m('.fontsize-large.fontweight-semibold.u-marginbottom-20', I18n.t('hello_sub', I18nScope())),
+                    m('.fontsize-base.u-marginbottom-10', I18n.t('hello_email', I18nScope({email: user.email}))),
+                    m('.w-row', [
+                        m('.w-col.w-col-3'),
+                        m('.w-col.w-col-3', [
+                            m('a.btn.btn-medium.btn-terciary.w-button[href="javacript:void(0);"]', {
+                                onclick: ctrl.checkEmail
+                            }, 'Sim!')
+                        ]),
+                        m('.w-col.w-col-3', [
+                            m(`a.btn.btn-medium.w-button[href="/users/${user.id}/edit#about_me"]`,'Editar o email')
+                        ]),
+                        m('.w-col.w-col-3')
+                    ])
+                ]))
+            ]) : m('div'));
+        }
+
+        return m('div');
     }
 };
 
