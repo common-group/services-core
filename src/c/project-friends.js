cad95a9a217c88832a319eb51492fc67dd1c332a
Vinicius Andrade <vini.andrade.dev@gmail.com>
2017-02-14 21:35:37 +0100

    Fixes project friends card html structure

--- src/c/project-friends.js
+++ src/c/project-friends.js
@@ -7,17 +7,17 @@ import postgrest from 'mithril-postgrest';
 const projectFriends = {
     controller(args) {
         const project = args.project,
-              friendsSample = m.prop([]),
-              listVM = postgrest.paginationVM(models.contributor, 'user_id.desc', {
-                  'Prefer':  'count=exact'
-              }),
-              filterVM = postgrest.filtersVM({
-                  project_id: 'eq',
-                  is_follow: 'eq'
-              }).project_id(project.project_id).is_follow(true);
+            friendsSample = m.prop([]),
+            listVM = postgrest.paginationVM(models.contributor, 'user_id.desc', {
+                'Prefer': 'count=exact'
+            }),
+            filterVM = postgrest.filtersVM({
+                project_id: 'eq',
+                is_follow: 'eq'
+            }).project_id(project.project_id).is_follow(true);
 
         if (!listVM.collection().length) {
-            listVM.firstPage(filterVM.parameters()).then(()=>{
+            listVM.firstPage(filterVM.parameters()).then(() => {
                 friendsSample(_.sample(listVM.collection(), 2));
             });
         }
@@ -29,35 +29,30 @@ const projectFriends = {
     },
     view(ctrl, args) {
         const project = ctrl.project,
-              friendsCount = ctrl.listVM.collection().length;
-        return m('.friend-backed-card',
-                    [
-                      m('.friend-facepile',
-                        [
-                        _.map(ctrl.friendsSample(), (user) => {
-                            const profile_img = _.isEmpty(user.data.profile_img_thumbnail) ? '/assets/catarse_bootstrap/user.jpg' : user.data.profile_img_thumbnail;
-                            return m(`img.user-avatar[src='${profile_img}']`);
-                        })
-                      ]
-                    ),
-                    m('p.fontsize-smallest.friend-namepile.lineheight-tighter',
-                      [
-                        m('span.fontweight-semibold',
-                          _.map(ctrl.friendsSample(), (user) => {
-                              return user.data.name.split(' ')[0];
-                          }).join(friendsCount > 2 ? ', ' : ' e ')
-                         ),
-                        (friendsCount > 2 ? [
-                          ' e ',
-                          m('span.fontweight-semibold',
-                            `mais ${friendsCount - ctrl.friendsSample().length}`
-                        )] : ''),
-                        (friendsCount > 1 ?
-                        ' apoiaram' : ' apoiou')
-                      ]
+            friendsCount = ctrl.listVM.collection().length;
+        return [
+            m('.friend-facepile', [
+                _.map(ctrl.friendsSample(), (user) => {
+                    const profile_img = _.isEmpty(user.data.profile_img_thumbnail) ? '/assets/catarse_bootstrap/user.jpg' : user.data.profile_img_thumbnail;
+                    return m(`img.user-avatar[src='${profile_img}']`);
+                })
+            ]),
+            m('p.fontsize-smallest.friend-namepile.lineheight-tighter', [
+                m('span.fontweight-semibold',
+                    _.map(ctrl.friendsSample(), (user) => {
+                        return user.data.name.split(' ')[0];
+                    }).join(friendsCount > 2 ? ', ' : ' e ')
+                ),
+                (friendsCount > 2 ? [
+                    ' e ',
+                    m('span.fontweight-semibold',
+                        `mais ${friendsCount - ctrl.friendsSample().length}`
                     )
-                  ]
-                );
+                ] : ''),
+                (friendsCount > 1 ?
+                    ' apoiaram' : ' apoiou')
+            ])
+        ];
     }
 };
 
