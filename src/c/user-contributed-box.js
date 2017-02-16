753196f051bab6608ad642122f83ef8e7fa55e68
Adriano Benin <adriano.benin@gmail.com>
2017-02-16 20:10:23 -0200

    only show received button to paid contributions

--- src/c/user-contributed-box.js
+++ src/c/user-contributed-box.js
@@ -10,12 +10,14 @@ const I18nScope = _.partial(h.i18nScope, 'payment.state');
 const userContributedBox = {
     controller(args) {
         const toggleDelivery = (projectId, contribution) => {
-            userVM.toggleDelivery(projectId, contribution).then(() => {
-                const lastStatus = contribution.reward_sent_at ? 'delivered' : 'undelivered';
-                contribution.delivery_status = contribution.delivery_status == 'received' ? lastStatus : 'received'; //so we don't have to reload the page
-            });
-        };
+                userVM.toggleDelivery(projectId, contribution).then(() => {
+                    const lastStatus = contribution.reward_sent_at ? 'delivered' : 'undelivered';
+                    contribution.delivery_status = contribution.delivery_status == 'received' ? lastStatus : 'received'; //so we don't have to reload the page
+                });
+            },
+            canReceiveReward = (contribution) => (contribution.state == 'paid' && contribution.reward_id && contribution.project_state != 'failed');
         return {
+            canReceiveReward: canReceiveReward,
             toggleAnonymous: userVM.toggleAnonymous,
             toggleDelivery: toggleDelivery
         };
@@ -164,18 +166,19 @@ const userContributedBox = {
                             statusText(contribution)
                         ])
                     ),
-                    m('.u-text-center.w-col.w-col-1', {
-                        onclick: () => ctrl.toggleDelivery(contribution.project_id, contribution)
-                    }, [
-                        m('.fontsize-smallest',
-                            m(`a.checkbox-big${contribution.delivery_status == 'received' ? '.checkbox--selected.fa.fa-check.fa-lg' : ''}`,
-                                ''
+                    (ctrl.canReceiveReward(contribution) ?
+                        m('.u-text-center.w-col.w-col-1', {
+                            onclick: () => ctrl.toggleDelivery(contribution.project_id, contribution)
+                        }, [
+                            m('.fontsize-smallest',
+                                m(`a.checkbox-big${contribution.delivery_status == 'received' ? '.checkbox--selected.fa.fa-check.fa-lg' : ''}`,
+                                    ''
+                                )
+                            ),
+                            m('.fontcolor-secondary.fontsize-smallest.lineheight-looser',
+                                'Recebi!'
                             )
-                        ),
-                        m('.fontcolor-secondary.fontsize-smallest.lineheight-looser',
-                            'Recebi!'
-                        )
-                    ])
+                        ]) : '')
 
                 ]);
             }),
