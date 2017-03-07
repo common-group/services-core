375084c6465abd7842ee164569e1b99dcc4809bd
Antônio Roberto <forevertonny@gmail.com>
2017-03-06 21:27:42 -0300

    Wait user loads before render userSettings

--- src/root/project-edit-user-settings.js
+++ src/root/project-edit-user-settings.js
@@ -1,4 +1,5 @@
 import m from 'mithril';
+import h from '../h';
 import userVM from '../vms/user-vm';
 import userAboutEdit from '../c/user-about-edit';
 import userSettings from '../c/user-settings';
@@ -11,12 +12,12 @@ const projectEditUserSettings = {
     },
 
     view(ctrl, args) {
-        return m(userSettings, {
+        return (ctrl.user() ? m(userSettings, {
             user: ctrl.user(),
             userId: args.user_id,
             hideCreditCards: true,
             useFloatBtn: true
-        });
+        }) : m('div', h.loader()) );
     }
 };
 
