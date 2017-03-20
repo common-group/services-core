46cbc561dbbaad1a152696d015cf763d0f6445b6
Vinicius Andrade <vini.andrade.dev@gmail.com>
2017-03-20 22:56:46 +0100

    Adds project reward validation

--- src/vms/reward-vm.js
+++ src/vms/reward-vm.js
@@ -1,109 +1,115 @@
-import postgrest from 'mithril-postgrest';
-import _ from 'underscore';
-import m from 'mithril';
-import models from '../models';
-import h from '../h';
+import postgrest from 'mithril-postgrest'
+import _ from 'underscore'
+import m from 'mithril'
+import models from '../models'
+import h from '../h'
 
 const error = m.prop(''),
-    rewards = m.prop([]),
-    states = m.prop([]),
-    noReward = {
-        id: -1,
-        description: 'Obrigado. Eu só quero ajudar o projeto.',
-        minimum_value: 10
-    },
-    contributionValue = m.prop(`${noReward.minimum_value},00`),
-    selectedReward = m.prop(noReward),
-    vm = postgrest.filtersVM({
-        project_id: 'eq'
-    });
+  rewards = m.prop([]),
+  states = m.prop([]),
+  noReward = {
+    id: -1,
+    description: 'Obrigado. Eu só quero ajudar o projeto.',
+    minimum_value: 10
+  },
+  contributionValue = m.prop(`${noReward.minimum_value},00`),
+  selectedReward = m.prop(noReward),
+  vm = postgrest.filtersVM({
+    project_id: 'eq'
+  })
 
 const rewardsLoader = (projectId) => {
-    vm.project_id(projectId);
+  vm.project_id(projectId)
 
-    return postgrest.loaderWithToken(models.rewardDetail.getPageOptions(vm.parameters()));
-};
+  return postgrest.loaderWithToken(models.rewardDetail.getPageOptions(vm.parameters()))
+}
 
-const fetchRewards = projectId => rewardsLoader(projectId).load().then(rewards);
+const fetchRewards = projectId => rewardsLoader(projectId).load().then(rewards)
 
 const getSelectedReward = () => {
-    const root = document.getElementById('application'),
-        data = root && root.getAttribute('data-contribution');
+  const root = document.getElementById('application'),
+    data = root && root.getAttribute('data-contribution')
 
-    if (data) {
-        const contribution = JSON.parse(data);
+  if (data) {
+    const contribution = JSON.parse(data)
 
-        selectedReward(contribution.reward);
-        m.redraw(true);
+    selectedReward(contribution.reward)
+    m.redraw(true)
 
-        return selectedReward;
-    }
+    return selectedReward
+  }
 
-    return false;
-};
+  return false
+}
 
 const selectReward = reward => () => {
-    if (selectedReward() !== reward) {
-        selectedReward(reward);
+  if (selectedReward() !== reward) {
+    selectedReward(reward)
 
-        contributionValue(h.applyMonetaryMask(`${reward.minimum_value},00`));
-    }
-};
+    contributionValue(h.applyMonetaryMask(`${reward.minimum_value},00`))
+  }
+}
 
-const selectDestination = m.prop('');
+const selectDestination = m.prop('')
 
-const applyMask = _.compose(contributionValue, h.applyMonetaryMask);
+const applyMask = _.compose(contributionValue, h.applyMonetaryMask)
 
 const getFees = (reward) => {
-    const feesFilter = postgrest.filtersVM({
-        reward_id: 'eq'
-    });
+  const feesFilter = postgrest.filtersVM({
+    reward_id: 'eq'
+  })
 
-    feesFilter.reward_id(reward.id || 0);
-    const feesLoader = postgrest.loader(models.shippingFee.getPageOptions(feesFilter.parameters()));
-    return feesLoader;
-};
+  feesFilter.reward_id(reward.id || 0)
+  const feesLoader = postgrest.loader(models.shippingFee.getPageOptions(feesFilter.parameters()))
+  return feesLoader
+}
 
-const statesLoader = postgrest.loader(models.state.getPageOptions());
+const statesLoader = postgrest.loader(models.state.getPageOptions())
 const getStates = () => {
-    statesLoader.load().then(states);
-    return states;
-};
+  statesLoader.load().then(states)
+  return states
+}
 
 const locationOptions = (reward) => {
-    const options = m.prop([]),
-        mapStates = _.map(states(), state => ({ value: state.acronym, name: state.name }));
+  const options = m.prop([]),
+    mapStates = _.map(states(), state => ({ value: state.acronym, name: state.name }))
 
-    if (reward.shipping_options === 'national') {
-        options(mapStates);
-    } else if (reward.shipping_options === 'international') {
-        options(_.union([{ value: 'international', name: 'Outside Brazil' }], mapStates));
-    }
+  if (reward.shipping_options === 'national') {
+    options(mapStates)
+  } else if (reward.shipping_options === 'international') {
+    options(_.union([{ value: 'international', name: 'Outside Brazil' }], mapStates))
+  }
 
-    return options();
-};
+  return options()
+}
 
-const canEdit = (reward, project_state) => project_state === 'draft' || reward.paid_count <= 0;
+const shippingFeeForCurrentReward = (fees) => {
+    const currentFee = _.findWhere(fees(), {
+        destination: selectedDestination()
+    });
+
+    return currentFee.value
+}
 
 const rewardVM = {
-    error,
-    canEdit,
-    getStates,
-    getFees,
-    rewards,
-    applyMask,
-    noReward,
-    fetchRewards,
-    selectDestination,
-    selectReward,
-    getSelectedReward,
-    selectedReward,
-    contributionValue,
-    rewardsLoader,
-    locationOptions,
-    getValue: contributionValue,
-    setValue: contributionValue,
-    selectedDestination: selectDestination
-};
+        error,
+        getStates,
+        getFees,
+        rewards,
+        applyMask,
+        noReward,
+        fetchRewards,
+        selectDestination,
+        selectReward,
+        getSelectedReward,
+        selectedReward,
+        contributionValue,
+        rewardsLoader,
+        locationOptions,
+        shippingFeeForCurrentReward,
+        getValue: contributionValue,
+        setValue: contributionValue,
+        selectedDestination: selectDestination
+}
 
-export default rewardVM;
+export default rewardVM
