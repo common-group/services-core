f17dde9c1427e1d539fd85b9be12e65922f442a7
Antônio Roberto <forevertonny@gmail.com>
2017-03-21 14:04:32 -0300

    Added project basics edit form

---
+++ src/c/project-basics-edit.js
@@ -0,0 +1,169 @@
+import m from 'mithril';
+import _ from 'underscore';
+import h from '../h';
+import I18n from 'i18n-js';
+import userVM from '../vms/user-vm';
+import projectBasicsVM from '../vms/project-basics-vm';
+import popNotification from './pop-notification';
+import inlineError from './inline-error';
+import inputCard from './input-card';
+
+const I18nScope = _.partial(h.i18nScope, 'projects.dashboard_basics');
+
+const projectBasicsEdit = {
+    controller(args) {
+        const vm = projectBasicsVM,
+              loading = m.prop(false),
+              categories = m.prop([]),
+              onSubmit = (event) => {
+                  event.preventDefault();
+                  loading(true);
+                  vm.updateProject(args.projectId).then((data) => {
+                      console.log(data);
+                      loading(false);
+                  }).catch((err) => {
+                      console.log(err);
+                      loading(false);
+                  });
+              };
+        vm.fillFields(args.project);
+        vm.loadCategoriesOptionsTo(categories, vm.fields.category_id());
+
+        return {
+            vm,
+            onSubmit,
+            loading,
+            categories
+        };
+    },
+    view(ctrl, args) {
+        const vm = ctrl.vm;
+        return m('#basics-tab', [
+            //add pop notifications here
+            m('form.w-form', { onsubmit: ctrl.onSubmit }, [
+                m('.w-container', [
+                    // admin fields
+                    (args.user.is_admin ?
+                      m('.w-row', [
+                          m('.w-col.w-col-10.w-col-push-1', [
+                              m(inputCard, {
+                                  label: I18n.t('tracker_snippet_html', I18nScope()),
+                                  children: [
+                                      m('textarea.text.optional.w-input.text-field.positive.medium', {
+                                          value: vm.fields.tracker_snippet_html(),
+                                          onchange: m.withAttr('value', vm.fields.tracker_snippet_html)
+                                      })
+                                  ]
+                              }),
+                              m(inputCard, {
+                                  label: I18n.t('user_id', I18nScope()),
+                                  children: [
+                                      m('input.string.optional.w-input.text-field.positive.medium[type="text"]', {
+                                          value: vm.fields.user_id(),
+                                          onchange: m.withAttr('value', vm.fields.user_id)
+                                      })
+                                  ]
+                              }),
+                              m(inputCard, {
+                                  label: 'Admin Tags',
+                                  label: I18n.t('admin_tags', I18nScope()),
+                                  label_hint: I18n.t('admin_tags_hint', I18nScope()),
+                                  children: [
+                                      m('input.string.optional.w-input.text-field.positive.medium[type="text"]', {
+                                          value: vm.fields.admin_tags(),
+                                          onchange: m.withAttr('value', vm.fields.admin_tags)
+                                      })
+                                  ]
+                              }),
+                              m(inputCard, {
+                                  label: I18n.t('service_fee', I18nScope()),
+                                  children: [
+                                      m('input.string.optional.w-input.text-field.positive.medium[type="number"]', {
+                                          value: vm.fields.service_fee(),
+                                          onchange: m.withAttr('value', vm.fields.service_fee)
+                                      })
+                                  ]
+                              })
+                          ])
+                      ])
+                     : ''),
+                    m('.w-row', [
+                        m('.w-col.w-col-10.w-col-push-1', [
+                            m(inputCard, {
+                                label: I18n.t('name', I18nScope()),
+                                label_hint: I18n.t('name_hint', I18nScope()),
+                                children: [
+                                    m('input.string.required.w-input.text-field.positive.medium[type="text"][maxlength="50"]', {
+                                        value: vm.fields.name(),
+                                        onchange: m.withAttr('value', vm.fields.name)
+                                    })
+                                ]
+                            }),
+                            m(inputCard, {
+                                label: I18n.t('tags', I18nScope()),
+                                label_hint: I18n.t('tags_hint', I18nScope()),
+                                children: [
+                                    m('input.string.optional.w-input.text-field.positive.medium[type="text"]', {
+                                        value: vm.fields.public_tags(),
+                                        onchange: m.withAttr('value', vm.fields.public_tags)
+                                    })
+                                ]
+                            }),
+                            m(inputCard, {
+                                label: I18n.t('permalink', I18nScope()),
+                                label_hint: I18n.t('permalink_hint', I18nScope()),
+                                children: [
+                                    m('.w-row',[
+                                        m('.w-col.w-col-4.w-col-small-6.w-col-tiny6.text-field.prefix.no-hover.medium.prefix-permalink',
+                                          m('.fontcolor-secondary.u-text-center.fontcolor-secondary.u-text-center.fontsize-smallest', 'www.catarse.me/')),
+                                        m('.w-col.w-col-8.w-col-small-6.w-col-tiny-6', [
+                                            m('input.string.required.w-input.text-field.postfix.positive.medium[type="text"]', {
+                                                value: vm.fields.permalink(),
+                                                onchange: m.withAttr('value', vm.fields.permalink)
+                                            })
+                                        ])
+                                    ])
+                                ]
+                            }),
+                            m(inputCard, {
+                                label: I18n.t('category', I18nScope()),
+                                label_hint: I18n.t('category_hint', I18nScope()),
+                                children: [
+                                    m('select.required.w-input.text-field.w-select.positive.medium', {
+                                        value: vm.fields.category_id(),
+                                        onchange: m.withAttr('value', vm.fields.category_id)
+                                    }, ctrl.categories())
+                                ]
+                            }),
+                            m(inputCard, {
+                                label: I18n.t('city', I18nScope()),
+                                label_hint: I18n.t('city_hint', I18nScope()),
+                                children: [
+                                    m('input.string.required.w-input.text-field.positive.medium[type="text"]', {
+                                        value: vm.fields.city_id(),
+                                        onchange: m.withAttr('value', vm.fields.city_id)
+                                    })
+                                ]
+                            })
+                        ])
+                    ])
+                ]),
+                m('.w-container.w-section.save-draft-btn-section', [
+                    m('.w-row', [
+                        m('.w-col.w-col-4.w-col-push-4',
+                          (ctrl.loading() ? h.loader() : [
+                              m('input[id="anchor"][name="anchor"][type="hidden"][value="about_me"]'),
+                              m('input.btn.btn.btn-large[name="commit"][type="submit"][value="Salvar"]', {
+                                  onclick: ctrl.onSubmit
+                              })
+                          ])
+                         ),
+                        m('.w-col.w-col-4')
+                    ])
+                ])
+            ])
+        ]);
+    }
+};
+
+export default projectBasicsEdit;
