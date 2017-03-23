21ff09b6f2be4e7631256cd1d36c22746449b8ad
Antônio Roberto <forevertonny@gmail.com>
2017-03-23 13:32:42 -0300

    When click on opened online_days should reset online_days field

--- src/c/project-goal-edit.js
+++ src/c/project-goal-edit.js
@@ -189,7 +189,8 @@ const projectGoalEdit = {
                                 ] : [
                                     m('.flex-row', [
                                         m('a.choose-time.choose-unlimited.w-inline-block.btn-select.flex-column.u-text-center', {
-                                            class: _.isEmpty(vm.fields.online_days().toString()) ? 'selected' : ''
+                                            class: _.isEmpty(vm.fields.online_days().toString()) ? 'selected' : '',
+                                            onclick: () => { vm.fields.online_days(''); }
                                         }, [
                                             m('.fontsize-base.fontweight-semibold.u-marginbottom-20', I18n.t('online_days_open', I18nScope())),
                                             m('.w-hidden-tiny', I18n.t('online_days_open_hint', I18nScope()))
