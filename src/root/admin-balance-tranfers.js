e5accb971e3c0415ee4eb363abaa312418b384a7
Antônio Roberto <forevertonny@gmail.com>
2017-05-04 14:44:10 -0300

    Added approve/reject behavior

--- src/root/admin-balance-tranfers.js
+++ src/root/admin-balance-tranfers.js
@@ -5,6 +5,8 @@ import balanceTransferFilterVM from '../vms/balance-transfer-filter-vm';
 import adminList from '../c/admin-list';
 import adminFilter from '../c/admin-filter';
 import filterMain from '../c/filter-main';
+import filterDropdown from '../c/filter-dropdown';
+import modalBox from '../c/modal-box';
 import adminBalanceTransferItem from '../c/admin-balance-transfer-item';
 import adminBalanceTransferItemDetail from '../c/admin-balance-transfer-item-detail';
 
@@ -13,6 +15,7 @@ const adminBalanceTranfers = {
         const listVM = balanceTransferListVM,
               filterVM = balanceTransferFilterVM,
               error = m.prop(''),
+              selectedAny = m.prop(false),
               filterBuilder = [
                   {
                       component: filterMain,
@@ -20,16 +23,73 @@ const adminBalanceTranfers = {
                           vm: filterVM.full_text_index,
                           placeholder: 'Busque pelo email, ids do usuario, ids de transferencia e eventos de saldo'
                       }
+                  },
+                  {
+                      component: filterDropdown,
+                      data: {
+                          label: 'state',
+                          name: 'state',
+                          vm: filterVM.state,
+                          options: [{
+                              value: '',
+                              option: 'Qualquer um'
+                          }, {
+                              value: 'pending',
+                              option: 'Pendente'
+                          }, {
+                              value: 'authorized',
+                              option: 'Autorizado'
+                          }, {
+                              value: 'processing',
+                              option: 'Processando'
+                          }, {
+                              value: 'transferred',
+                              option: 'Concluido'
+                          }, {
+                              value: 'error',
+                              option: 'Erro'
+                          }, {
+                              value: 'rejected',
+                              option: 'Rejeitado'
+                          }, {
+                              value: 'gateway_error',
+                              option: 'Erro no gateway'
+                          }]
+                      }
                   }
               ],
-              seletectedItemsIDs = m.prop([]),
-              generateSeletecItem = (item_id) => {
-                  seletectedItemsIDs().push(item_id);
-                  console.log('select', seletectedItemsIDs());
+              selectedItemsIDs = m.prop([]),
+              displayApprovalModal = h.toggleProp(false, true),
+              displayRejectModal = h.toggleProp(false, true),
+              isSelected = (item_id) => {
+                  return _.find(selectedItemsIDs(), (i) => {
+                      return i.id == item_id;
+                  });
               },
-              generateUnselectItem = (item_id) => {
-                  seletectedItemsIDs(_.without(seletectedItemsIDs(), item_id));
-                  console.log('unselect', seletectedItemsIDs());
+              selectItem = (item) => {
+                  if (!_.find(selectedItemsIDs(), (i) => { return i.id == item.id; })) {
+                      selectedItemsIDs().push(item);
+                  }
+                  selectedAny(true);
+              },
+              unSelectItem = (item) => {
+                  let newIDs = _.reject(selectedItemsIDs(), (i) => {
+                      return i.id == item.id;
+                  });
+                  selectedItemsIDs(newIDs);
+                  if(_.isEmpty(newIDs)) {
+                      selectedAny(false);
+                  }
+              },
+              selectedInputActions = () => {
+                  return m('', [
+                      m('button.btn.btn-inline.btn-small.btn-terciary.u-marginright-20.w-button', {
+                          onclick: displayApprovalModal.toggle
+                      }, 'Aprovar'),
+                      m('button.btn.btn-inline.btn-small.btn-terciary.u-marginright-20.w-button', {
+                          onclick: displayRejectModal.toggle
+                      }, 'Rejeitar')
+                  ]);
               },
               submit = () => {
                   error(false);
@@ -38,16 +98,102 @@ const adminBalanceTranfers = {
                   });
 
                   return false;
+              },
+              generateWrapperModal = (customAttrs) => {
+                  const wrapper = {
+                      view(ctrl, args) {
+                          return m('', [
+                              m('.modal-dialog-header', [
+                                  m('.fontsize-large.u-text-center', args.modalTitle)
+                              ]),
+                              m('.modal-dialog-content', [
+                                  _.map(selectedItemsIDs(), (item, index) => {
+                                      return m('.divider.fontsize-smallest.lineheight-looser', [
+                                          m('.w-row.fontweight-semibold', [
+                                              m('.w-col.w-col-4', [
+                                                  m('', item.user.name),
+                                              ]),
+                                              m('.w-col.w-col-4', [
+                                                  m('span', `R$ ${h.formatNumber(item.amount, 2, 3)}`)
+                                              ]),
+                                              m('.w-col.w-col-4', [
+                                                  m('.fontsize-smallest', [
+                                                      'Solicitado em: ',
+                                                      m('span.fontsize-small.lineheight-tightest', h.momentify(item.created_at)),
+                                                  ])
+                                              ]),
+                                          ])
+                                      ]);
+                                  })
+                              ]),
+                              m('.modal-dialog-nav-bottom', [
+                                  m('.w-row', [
+                                      m('.w-col.w-col-4.w-col-push-4',
+                                        m('button.btn.btn-large.btn-terciary', {
+                                            onclick: args.onClickCallback
+                                        },
+                                          args.ctaText
+                                         )
+                                       ),
+                                  ])
+                              ]),
+                          ]);
+                      }
+                  };
+
+                  return [wrapper, customAttrs];
+              },
+              approveSelectedIDs = () => {
+                  m.request({
+                      method: 'POST',
+                      url: '/admin/balance_transfers/batch_approve',
+                      data: {
+                          transfer_ids: _.uniq(_.map(selectedItemsIDs(), (s) => {
+                              return s.id;
+                          }))
+                      },
+                      config: h.setCsrfToken
+                  }).then((data) => {
+                      selectedItemsIDs([]);
+                      listVM.firstPage();
+                      displayApprovalModal(false);
+                      m.redraw();
+                  });
+              },
+              rejectSelectedIDs = () => {
+                  m.request({
+                      method: 'POST',
+                      url: '/admin/balance_transfers/batch_reject',
+                      data: {
+                          transfer_ids: _.uniq(_.map(selectedItemsIDs(), (s) => {
+                              return s.id;
+                          }))
+                      },
+                      config: h.setCsrfToken
+                  }).then((data) => {
+                      selectedItemsIDs([]);
+                      displayRejectModal(false);
+                      listVM.firstPage();
+                      m.redraw();
+                  });
               };
 
         return {
+            displayApprovalModal,
+            displayRejectModal,
+            generateWrapperModal,
+            approveSelectedIDs,
+            rejectSelectedIDs,
             filterVM,
             filterBuilder,
             listVM: {
                 list: listVM,
-                seletectedItemsIDs,
-                generateSeletecItem,
-                generateUnselectItem,
+                selectedItemsIDs,
+                selectItem,
+                unSelectItem,
+                selectedAny,
+                selectedInputActions,
+                isSelected,
                 error
             },
             data: {
@@ -62,6 +208,22 @@ const adminBalanceTranfers = {
                 filterBuilder: ctrl.filterBuilder,
                 submit: ctrl.submit
             }),
+            (ctrl.displayApprovalModal() ? m(modalBox, {
+                displayModal: ctrl.displayApprovalModal,
+                content: ctrl.generateWrapperModal({
+                    modalTitle: 'Aprovar saques',
+                    ctaText: 'Aprovar',
+                    onClickCallback: ctrl.approveSelectedIDs
+                })
+            }) : '' ),
+            (ctrl.displayRejectModal() ? m(modalBox, {
+                displayModal: ctrl.displayRejectModal,
+                content: ctrl.generateWrapperModal({
+                    modalTitle: 'Rejeitar saques',
+                    ctaText: 'Rejeitar',
+                    onClickCallback: ctrl.rejectSelectedIDs
+                })
+            }) : '' ),
             m(adminList, {
                 vm: ctrl.listVM,
                 listItem: adminBalanceTransferItem,
