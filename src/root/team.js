431b7c40bcbac5f776792ee2fbeb4c5107006440
Antônio Roberto <forevertonny@gmail.com>
2017-05-29 09:54:33 -0300

    Missing part for Team root component

--- src/root/team.js
+++ src/root/team.js
@@ -5,6 +5,13 @@ import teamMembers from '../c/team-members';
 const team = {
     view() {
         return m('#static-team-app', [
+            m('.w-section.hero-who.hero-full', [
+                m('.w-container.u-text-center', [
+                    m('img.icon-hero[src="https://catarse.me/assets/logo-yellow.png"]'),
+                    m('.u-text-center.u-marginbottom-20.fontsize-largest',
+                      'Conheça nosso time')
+                ])
+            ]),
             m.component(teamTotal),
             m.component(teamMembers)
         ]);
