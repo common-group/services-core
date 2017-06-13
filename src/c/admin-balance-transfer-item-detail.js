e2cea05bef24625e2900fce52b4d76774dab9300
Antônio Roberto <forevertonny@gmail.com>
2017-06-13 13:29:45 -0300

    Added feedback when admin fill a note

--- src/c/admin-balance-transfer-item-detail.js
+++ src/c/admin-balance-transfer-item-detail.js
@@ -5,34 +5,53 @@ import h from '../h';
 import userVM from '../vms/user-vm';
 import models from '../models';
 import adminUserBalanceTransactionsList from './admin-user-balance-transactions-list';
+import popNotification from './pop-notification';
+import I18n from 'i18n-js';
+
+const I18nScope = _.partial(h.i18nScope, 'admin.balance_transfers');
 
 const adminBalanceTransferItemDetail = {
     controller(args) {
         const userBankAccount = m.prop(null),
-              metadata = args.item.last_transition_metadata || {transfer_data: {}},
-              transferData = metadata.transfer_data || {},
-              metaBank = transferData.bank_account,
-              userBalance = m.prop({}),
-              transitionBankAccount = m.prop({}),
-              fields = {
-                  admin_notes: m.prop(args.item.admin_notes)
-              },
-              loadingNotes = m.prop(false),
-              submitNotes = () => {
-                  loadingNotes(true);
-                  m.request({
-                      method: 'PUT',
-                      url: `/admin/balance_transfers/${args.item.id}`,
-                      data: {
-                          balance_transfer: {
-                              admin_notes: fields.admin_notes()
-                          }
-                      },
-                      config: h.setCsrfToken
-                  }).then((data) => {
-                      loadingNotes(false);
-                  });
-              };
+            showPopNotification = m.prop(false),
+            popNotificationAttributes = m.prop({}),
+            metadata = args.item.last_transition_metadata || {transfer_data: {}},
+            transferData = metadata.transfer_data || {},
+            metaBank = transferData.bank_account,
+            userBalance = m.prop({}),
+            transitionBankAccount = m.prop({}),
+            fields = {
+                admin_notes: m.prop(args.item.admin_notes)
+            },
+            loadingNotes = m.prop(false),
+            submitNotes = () => {
+                loadingNotes(true);
+                m.request({
+                    method: 'PUT',
+                    url: `/admin/balance_transfers/${args.item.id}`,
+                    data: {
+                        balance_transfer: {
+                            admin_notes: fields.admin_notes()
+                        }
+                    },
+                    config: h.setCsrfToken
+                }).then((data) => {
+                    showPopNotification(true);
+                    popNotificationAttributes({
+                        message: I18n.t('admin_notes.success_update', I18nScope()),
+                        toggleOpt: showPopNotification
+                    });
+                    loadingNotes(false);
+                }, (err) => {
+                    showPopNotification(true);
+                    popNotificationAttributes({
+                        message: I18n.t('admin_notes.failed_update', I18nScope()),
+                        error: true,
+                        toggleOpt: showPopNotification
+                    });
+                    loadingNotes(false);
+                });
+            };
 
         if(!_.isUndefined(metaBank)) {
             if(metaBank.conta) {
@@ -61,7 +80,9 @@ const adminBalanceTransferItemDetail = {
             userBalance,
             fields,
             submitNotes,
-            loadingNotes
+            loadingNotes,
+            showPopNotification,
+            popNotificationAttributes
         };
     },
 
@@ -73,30 +94,32 @@ const adminBalanceTransferItemDetail = {
             m('.w-row.card.card-terciary.u-radius',[
                 m('.w-col.w-col-4', [
                     (bankAccount ? [
-                    m('.fontsize-smaller.fontweight-semibold.lineheight-tighter.u-marginbottom-20', 'Dados bancários'),
-                    m('.fontsize-smallest.lineheight-looser', [
-                        m('span.fontweight-semibold', 'Banco:'),
-                        `${bankAccount.bank_code} - ${(bankAccount.bank_name ? bankAccount.bank_name : '' )}`,m('br'),
-                        m('span.fontweight-semibold', 'Agencia:'),
-                        ` ${bankAccount.agency} - ${bankAccount.agency_digit ? bankAccount.agency_digit : ''}`,m('br'),
-                        m('span.fontweight-semibold', "Conta:"),
-                        ` ${bankAccount.account} - ${bankAccount.account_digit ? bankAccount.account_digit : ''}`,m('br'),
-                        m('span.fontweight-semibold', 'Nome:'),
-                        bankAccount.owner_name, m('br'),
-                        m('span.fontweight-semibold', 'CPF:'),
-                        bankAccount.owner_document
-                    ])
+                        m('.fontsize-smaller.fontweight-semibold.lineheight-tighter.u-marginbottom-20', 'Dados bancários'),
+                        m('.fontsize-smallest.lineheight-looser', [
+                            m('span.fontweight-semibold', 'Banco:'),
+                            `${bankAccount.bank_code} - ${(bankAccount.bank_name ? bankAccount.bank_name : '' )}`,m('br'),
+                            m('span.fontweight-semibold', 'Agencia:'),
+                            ` ${bankAccount.agency} - ${bankAccount.agency_digit ? bankAccount.agency_digit : ''}`,m('br'),
+                            m('span.fontweight-semibold', "Conta:"),
+                            ` ${bankAccount.account} - ${bankAccount.account_digit ? bankAccount.account_digit : ''}`,m('br'),
+                            m('span.fontweight-semibold', 'Nome:'),
+                            bankAccount.owner_name, m('br'),
+                            m('span.fontweight-semibold', 'CPF:'),
+                            bankAccount.owner_document
+                        ])
                     ] : h.loader()),
                     (ctrl.loadingNotes() ? h.loader() : m('', [
                         m('textarea.text-field.height-mini.w-input', {
                             value: ctrl.fields.admin_notes(),
-                            onkeydown: m.withAttr('value', ctrl.fields.admin_notes)
+                            onkeyup: m.withAttr('value', ctrl.fields.admin_notes)
                         }),
                         m('.u-text-center',
-                          m('button.btn.btn-terciary', {
-                              onclick: ctrl.submitNotes
-                          }, I18n.t('shared.save_text'))
-                         )
+                            m('button.btn.btn-terciary', {
+                                onclick: ctrl.submitNotes
+                            }, I18n.t('shared.save_text'))
+                        ),
+                        (ctrl.showPopNotification() ? m(popNotification, ctrl.popNotificationAttributes()) : '')
+
                     ]))
                 ]),
                 m(adminUserBalanceTransactionsList, {user_id: args.item.user_id})
