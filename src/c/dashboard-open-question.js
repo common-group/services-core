e94067251173098ba8796e41e3585f8c5a60af1c
Vinicius Andrade <vini.andrade.dev@gmail.com>
2017-07-05 21:45:09 +0200

    fixes empty field after edit

--- src/c/dashboard-open-question.js
+++ src/c/dashboard-open-question.js
@@ -14,7 +14,8 @@ const dashboardOpenQuestion = {
                     m('.w-col.w-col-8',
                         m('input.positive.text-field.w-input[name="question"][type="text"]', {
                             name: `reward[surveys_attributes][questions][${index}][question]`,
-                            onchange: m.withAttr('value', newValue => question.question = newValue)
+                            onchange: m.withAttr('value', newValue => question.question = newValue),
+                            value: question.question
                         })
                     )
                 ]),
@@ -27,7 +28,8 @@ const dashboardOpenQuestion = {
                     m('.w-col.w-col-8',
                         m('input.positive.text-field.w-input[type="text"]', {
                             name: `reward[surveys_attributes][questions][${index}][description]`,
-                            onchange: m.withAttr('value', newValue => question.description = newValue)
+                            onchange: m.withAttr('value', newValue => question.description = newValue),
+                            value: question.description
                         })
                     )
                 ])
