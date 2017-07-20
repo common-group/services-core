9b794a5d0e59f1f041be7b6976c1879345cb2e0b
Antônio Roberto <forevertonny@gmail.com>
2017-07-20 11:39:41 -0300

    change default order on balance transfers vm

--- src/vms/balance-transfer-list-vm.js
+++ src/vms/balance-transfer-list-vm.js
@@ -1,4 +1,4 @@
 import m from 'mithril';
 import models from '../models';
 
-export default postgrest.paginationVM(models.balanceTransfer, 'id.desc', { Prefer: 'count=exact' });
+export default postgrest.paginationVM(models.balanceTransfer, 'created_at.asc', { Prefer: 'count=exact' });
