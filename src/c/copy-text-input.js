9d4933738afacc71ea69feef9e4e1f588eb2969e
Vinicius Andrade <vini.andrade.dev@gmail.com>
2017-08-01 21:13:32 +0200

    fix copy input button

--- src/c/copy-text-input.js
+++ src/c/copy-text-input.js
@@ -10,36 +10,18 @@
  * }
  */
 import m from 'mithril';
+import select from 'select';
 import popNotification from './pop-notification';
 
 const copyTextInput = {
     controller(args) {
         const showSuccess = m.prop(false);
-
-        const select = (el) => {
-            let range,
-                selection;
-
-            if (document.body.createTextRange) {
-                range = document.body.createTextRange();
-                range.moveToElementText(el);
-                range.select();
-            } else if (window.getSelection) {
-                selection = window.getSelection();
-                range = document.createRange();
-                range.selectNodeContents(el);
-                selection.removeAllRanges();
-                selection.addRange(range);
-            }
-        };
-
         const setClickHandler = (el, isInitialized) => {
             let copy;
             if (!isInitialized) {
                 const textarea = el.parentNode.previousSibling.firstChild;
 
                 textarea.innerText = args.value; //This fixes an issue when instantiating multiple copy clipboard components
-
                 el.onclick = () => {
                     select(textarea);
                     copy = document.execCommand('copy');
