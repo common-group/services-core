4fd176a73e85b372e641884a94fad3395266622c
Adriano Benin <adriano.benin@gmail.com>
2017-08-11 10:09:31 -0300

    move divider to contributed list component [#149139883]

--- src/c/user-contributed-list.js
+++ src/c/user-contributed-list.js
@@ -22,43 +22,47 @@ const userContributedList = {
             hideSurveys = ctrl.hideSurveys,
             title = ctrl.title;
 
-        return (!_.isEmpty(collection) ? m('.section-one-column.u-marginbottom-30',
-            m('.w-container', [
-                m('.fontsize-larger.fontweight-semibold.u-marginbottom-30.u-text-center',
-                    title
-                ),
-                m('.card.card-secondary.w-hidden-small.w-hidden-tiny.w-row', [
-                    m('.w-col.w-col-3',
-                        m('.fontsize-small.fontweight-semibold',
-                            I18n.t('project_col', I18nScope())
-                        )
+        return (!_.isEmpty(collection) ? m('div', [m('.section-one-column.u-marginbottom-30',
+                m('.w-container', [
+                    m('.fontsize-larger.fontweight-semibold.u-marginbottom-30.u-text-center',
+                        title
                     ),
-                    m('.w-col.w-col-3',
-                        m('.fontsize-small.fontweight-semibold',
-                            I18n.t('contribution_col', I18nScope())
-                        )
-                    ),
-                    m('.w-col.w-col-3',
-                        m('.fontsize-small.fontweight-semibold',
-                            I18n.t('reward_col', I18nScope())
-                        )
-                    ),
-                    m('.w-col.w-col-1'),
-                    (!hideSurveys ?
-                    m('.w-col.w-col-2',
-                        m('.fontsize-small.fontweight-semibold',
-                            I18n.t('survey_col', I18nScope())
-                        )
-                    ) : '')
-                ]),
-                _.map(collection, contribution => m(userContributedBox, { contribution })),
-                m('.w-row.u-marginbottom-40.u-margintop-30', [
-                    m(loadMoreBtn, {
-                        collection: pagination,
-                        cssClass: '.w-col-push-4'
-                    })
-                ])
-            ])) : m('div', ''));
+                    m('.card.card-secondary.w-hidden-small.w-hidden-tiny.w-row', [
+                        m('.w-col.w-col-3',
+                            m('.fontsize-small.fontweight-semibold',
+                                I18n.t('project_col', I18nScope())
+                            )
+                        ),
+                        m('.w-col.w-col-3',
+                            m('.fontsize-small.fontweight-semibold',
+                                I18n.t('contribution_col', I18nScope())
+                            )
+                        ),
+                        m('.w-col.w-col-3',
+                            m('.fontsize-small.fontweight-semibold',
+                                I18n.t('reward_col', I18nScope())
+                            )
+                        ),
+                        m('.w-col.w-col-1'),
+                        (!hideSurveys ?
+                            m('.w-col.w-col-2',
+                                m('.fontsize-small.fontweight-semibold',
+                                    I18n.t('survey_col', I18nScope())
+                                )
+                            ) : '')
+                    ]),
+                    _.map(collection, contribution => m(userContributedBox, {
+                        contribution
+                    })),
+                    m('.w-row.u-marginbottom-40.u-margintop-30', [
+                        m(loadMoreBtn, {
+                            collection: pagination,
+                            cssClass: '.w-col-push-4'
+                        })
+                    ])
+                ])),
+            m('.divider.u-marginbottom-80.u-margintop-80')
+        ]) : m('div', ''));
     }
 };
 
