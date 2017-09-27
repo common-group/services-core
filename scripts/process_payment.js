e5b3b6f1e7bc78e7be8662c6c12359fb036dcbfe
Antônio Roberto <forevertonny@gmail.com>
2017-09-27 09:22:05 -0300

    Added basic connection to pagar-me and catalog payments

--- scripts/process_payment.js
+++ scripts/process_payment.js
@@ -1,27 +1,68 @@
 const {Client} = require('pg');
+const pagarme = require('pagarme');
+
+const exit = (code, message) => {
+    console.log(message);
+    process.exit(code);
+};
+
+const init = (stdin_data) => {
+    if (stdin_data == null) { exit(1, 'invalid stdin'); }
+    const pg_client = new Client(process.env.PROCESS_PAYMENT_DATABASE_URL);
+    pg_client.connect();
+
+    // fetch payment and user data to build context
+    pg_client.query(
+        `select
+            row_to_json(cp.*) as payment_data,
+            row_to_json(u.*) as user_data
+        from payment_service.catalog_payments cp
+            join community_service.users u on u.id = cp.user_id
+            where cp.id = $1::bigint`
+        , [stdin_data.id]
+        , (err, res) => {
+            if (err) {
+                exit(1, err);
+            } else {
+                const payment = res.rows[0].payment_data;
+                const user = res.rows[0].user_data;
+                //pg_client.end();
+
+                try {
+                    pagarme.client.connect({
+                        api_key: process.env.GATEWAY_API_KEY
+                    }).then((pagarme_client) => {
+                        let transaction_data = {
+                        };
+                        pagarme_client.transactions.create(transaction_data)
+                            .then((transaction) => {
+                                console.log(transaction);
+                                process.exit(0);
+                            }).catch((err) => {
+                                pg_client.query(
+                                    `update payment_service.catalog_payments
+                                        set gateway_cached_data = $2::json
+                                    where id = $1::bigint`
+                                    , [payment.id, JSON.stringify(err.response.errors)]
+                                    , (errq, res) => {
+                                        if(errq) { exit(1, errq); }
+                                        pg_client.end();
+                                        exit(1, err.response.errors);
+                                    });
+                            });
+                    });
+                } catch(e) {
+                    exit(1, e);
+                }
+            }
+        }
+    );
+
+};
 
 process.stdin.setEncoding('utf8');
-
 process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-        const client = new Client(process.env.PROCESS_PAYMENT_DATABASE_URL);
-        client.connect();
-        client.query('select count(1) from payment_service.catalog_payments;', (err, res) => {
-            console.log(err ? err.stack : res.rows[0].count)
-            client.end();
-            console.log(chunk);
-        });
-    }
+    const data = process.stdin.read();
+    if(data!==null) { init(JSON.parse(data)); }
 });
 
-//require 'json'
-//require 'pg'
-//
-//message = JSON.parse(ARGF.read)
-//pg_con = PG.connect(ENV['PROCESS_PAYMENT_DATABASE_URL'])
-//
-//puts message["id"]
-//count = pg_con.exec("select count(1) from payment_service.catalog_payments")
-//puts count[0]["count"]
-//
