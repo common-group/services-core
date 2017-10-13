fcc8c0b21b736dce0fedd901aa19ebd23030ee8c
Antônio Roberto <forevertonny@gmail.com>
2017-10-13 18:10:56 -0300

    Adjust create user to create user version too

--- specs/database/schema.sql
+++ specs/database/schema.sql
@@ -240,9 +240,9 @@ CREATE FUNCTION create_scoped_user_session(id bigint) RETURNS json
             _jwt core.jwt_token;
             _result json;
         begin
-            if current_role != 'platform_user' then
-                RAISE insufficient_privilege;
-            end if;
+            -- ensure that roles come from any permitted
+            perform core.force_any_of_roles('{platform_user}');
+
 
             select * from community_service.users cu
                 where cu.platform_id = core.current_platform_id()
@@ -289,26 +289,41 @@ CREATE FUNCTION create_user(data json) RETURNS json
             _platform platform_service.platforms;
             _refined json;
             _result json;
+            _passwd text;
+            _version community_service.user_versions;
         begin
+            -- ensure that roles come from any permitted
+            perform core.force_any_of_roles('{platform_user}');
+
             -- generate user basic data structure with received json
             select community_service._serialize_user_basic_data($1)
                 into _refined;
+
+            -- check if password already encrypted
+            select (case when ($1->>'password_encrypted'::text) = 'true' then ($1->>'password')::text  else crypt(($1->>'password')::text, gen_salt('bf')) end)
+                into _passwd;
 
             -- insert user in current platform
             insert into community_service.users (platform_id, email, password, data, created_at, updated_at)
                 values (core.current_platform_id(),
                         lower(($1)->>'email'::text),
-                        crypt(($1->>'password')::text,
-                        gen_salt('bf')),
+                        _passwd,
                         _refined::jsonb,
                         coalesce(($1->>'created_at')::timestamp, now()),
                         coalesce(($1->>'updated_at')::timestamp, now())
                     )
                     returning * into _user;
 
+            -- insert user version
+            insert into community_service.user_versions(user_id, data)
+                values (_user.id, _refined)
+            returning * into _version;
+
             -- build result with user id
             select json_build_object(
-                'id', _user.id
+                'id', _user.id,
+                'old_version_id', _version.id,
+                'data', _refined
             ) into _result;
 
             return _result;
