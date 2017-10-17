dcfdf30f5d016c8b5dda73046a9f9448d6518306
Antônio Roberto <forevertonny@gmail.com>
2017-10-17 20:33:49 -0200

    change to use set timeout to avoid duplicate run

--- scripts/subscription_scheduler.js
+++ scripts/subscription_scheduler.js
@@ -5,18 +5,11 @@ const {Client} = require('pg');
 const pagarme = require('pagarme');
 const _ = require('lodash');
 
-setInterval(() => {
-    console.log('checking for subscriptions');
-    init()
-        .then(void(0))
-        .catch(void(0));
-}, 5000);
-
 async function init() {
     try {
         const pg_client = new Client({
             connectionString: process.env.DATABASE_URL,
-            statement_timeout: 5000
+            statement_timeout: (process.env.STATEMENT_TIMEOUT || 5000)
         });
         await pg_client.connect();
 
@@ -25,8 +18,21 @@ async function init() {
             `select payment_service.subscriptions_charge($1::interval)`
             , [process.env.SUBSCRIPTION_INTERVAL]);
 
+        await pg_client.end();
         console.log(res.rows[0]);
     } catch (e) {
         console.log(e);
     };
 };
+
+const recursive_init = () => {
+    setTimeout(() => {
+        console.log('checking for subscriptions');
+        init()
+            .then(void(0))
+            .catch(void(0));
+        recursive_init();
+    }, (process.env.SET_INTERVAL || 60000))
+};
+
+recursive_init();
