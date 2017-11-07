ae05340c0b3883d292ab2fa77487a75f702b2c33
Antônio Roberto <forevertonny@gmail.com>
2017-11-07 10:31:44 -0200

    select token from gen_jwt instea pass record

---
+++ migrations/2017-11-07-121207_should_use_token_instead_jwt_type/up.sql
@@ -0,0 +1,125 @@
+-- Your SQL goes here
+CREATE OR REPLACE FUNCTION community_service_api.create_scoped_user_session(id uuid)
+ RETURNS json
+ LANGUAGE plpgsql
+ STABLE
+AS $function$
+        declare
+            _platform platform_service.platforms;
+            _user community_service.users;
+            _jwt core.jwt_token;
+            _result json;
+        begin
+            -- ensure that roles come from any permitted
+            perform core.force_any_of_roles('{platform_user}');
+
+            select * from community_service.users cu
+                where cu.platform_id = core.current_platform_id()
+                    and cu.id = $1
+                into _user;
+
+            if _user is null then
+                raise exception 'invalid user id';
+            end if;
+
+
+            select token from core.gen_jwt_token(json_build_object(
+                'role', 'scoped_user',
+                'user_id', _user.id,
+                'platform_token', core.current_platform_token(),
+                'exp', extract(epoch from now())::integer + (60*60)*2
+            )) into _jwt;
+
+            select json_build_object(
+                'token', _jwt
+            ) into _result;
+
+            return _result;
+        end;
+    $function$;
+
+CREATE OR REPLACE FUNCTION platform_service_api.generate_api_key(platform_id uuid)
+ RETURNS platform_service_api.api_keys
+ LANGUAGE plpgsql
+AS $function$
+        declare
+            _platform_token uuid;
+            _jwt_token text;
+            _result platform_service.platform_api_keys;
+        begin
+            if not platform_service.user_in_platform(core.current_user_id(), $1) then
+                raise exception 'insufficient permissions to do this action';
+            end if;
+
+            select token from platform_service.platforms p where p.id = $1
+                into _platform_token;
+
+            select token from core.gen_jwt_token(json_build_object(
+                'role', 'platform_user',
+                'platform_token', _platform_token,
+                'gen_at', extract(epoch from now())::integer
+            )) into _jwt_token;
+            
+            insert into platform_service.platform_api_keys(platform_id, token)
+                values ($1, _jwt_token)
+            returning * into _result;
+
+            return _result;
+        end;
+    $function$
+;
+
+CREATE OR REPLACE FUNCTION platform_service_api.login(email text, password text)
+ RETURNS core.jwt_token
+ LANGUAGE plpgsql
+AS $function$
+declare
+    _user platform_service.users;
+    result core.jwt_token;
+begin
+    select
+        u.*
+    from platform_service.users u
+        where lower(u.email) = lower($1)
+            and u.password = crypt($2, u.password)
+        into _user;
+
+    if _user is null then
+        raise invalid_password using message = 'invalid user or password';
+    end if;
+
+    select token from core.gen_jwt_token(
+        json_build_object(
+            'role', 'platform_user',
+            'user_id', _user.id,
+            'exp', extract(epoch from now())::integer + (60*60)*2
+        )
+    ) into result;
+
+    return result;
+end;
+$function$
+;
+CREATE OR REPLACE FUNCTION platform_service_api.sign_up(name text, email text, password text)
+ RETURNS core.jwt_token
+ LANGUAGE plpgsql
+AS $function$
+    declare
+        _user platform_service.users;
+        result core.jwt_token;
+    begin
+        insert into platform_service.users(name, email, password)
+            values (name, email, crypt(password, gen_salt('bf')))
+            returning * into _user;
+
+        select token from core.gen_jwt_token(json_build_object(
+            'role', 'platform_user',
+            'user_id', _user.id,
+            'exp', extract(epoch from now())::integer + (60*60)*2
+        )) 
+        into result;
+
+        return result;
+    end;
+$function$
+;
