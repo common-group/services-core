d541bf4e2339f4889da57eb6dffcd9c630ddadf5
Adriano Benin <adriano.benin@gmail.com>
2017-11-14 18:12:02 -0200

    update specs

--- spec/lib/mocks/postgrest-requests.mock.js
+++ spec/lib/mocks/postgrest-requests.mock.js
@@ -1,8 +1,8 @@
 // import Postgrest from 'mithril-postgrest';
-const testToken = "test_token";
+const testToken = 'test_token';
 const nContributions = 20;
 const apiMeta = document.createElement('meta');
-const apiPrefix = 'https://api.catarse.me'
+const apiPrefix = 'https://api.catarse.me';
 apiMeta.content = apiPrefix;
 apiMeta.name = 'api-host';
 document.getElementsByTagName('head')[0].appendChild(apiMeta);
@@ -12,32 +12,34 @@ commonPaymentApiMeta.content = 'https://payment.common.io';
 commonPaymentApiMeta.name = 'common-payment-api-host';
 document.getElementsByTagName('head')[0].appendChild(commonPaymentApiMeta);
 
+const commonAnalyticsApiMeta = document.createElement('meta');
+commonAnalyticsApiMeta.content = 'https://analytics.common.io';
+commonAnalyticsApiMeta.name = 'common-analytics-api-host';
+document.getElementsByTagName('head')[0].appendChild(commonAnalyticsApiMeta);
 // const apiPrefix = "https://api.catarse.me";
 // catarse.init(apiPrefix, {method: "GET", url: "/api_token"});
 
-const mockEndpoint = function(endpoint, result){
-  return jasmine.Ajax.stubRequest(
-    new RegExp('(' + apiPrefix + '\/' + endpoint + ')(.*)')
+const mockEndpoint = function (endpoint, result) {
+    return jasmine.Ajax.stubRequest(
+    new RegExp(`(${apiPrefix}\/${endpoint})(.*)`)
   ).andReturn({
-    'responseText' : JSON.stringify(result)
+      responseText: JSON.stringify(result)
   });
 };
 
-beforeAll(function() {
-  jasmine.Ajax.install();
+beforeAll(() => {
+    jasmine.Ajax.install();
 
-  //API token stub
-  jasmine.Ajax.stubRequest('/api_token').andReturn({
-    'responseText' : '{"token": "' + testToken + '"}'
-  });
+  // API token stub
+    jasmine.Ajax.stubRequest('/api_token').andReturn({
+        responseText: `{"token": "${testToken}"}`
+    });
 
-  jasmine.Ajax.stubRequest('/test').andReturn({
-    'responseText' : '{"object": "responseOK"}'
-  });
-
-
+    jasmine.Ajax.stubRequest('/test').andReturn({
+        responseText: '{"object": "responseOK"}'
+    });
 });
 
-afterAll(function() {
-  jasmine.Ajax.uninstall();
+afterAll(() => {
+    jasmine.Ajax.uninstall();
 });
