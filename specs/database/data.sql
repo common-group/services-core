e6df7e75f571f73fe8fd8cbc2c1458f876699f3f
Antônio Roberto <forevertonny@gmail.com>
2017-11-15 13:22:26 -0200

    Added basic specs for test project/rewards endpoint

--- specs/database/data.sql
+++ specs/database/data.sql
@@ -1,6 +1,7 @@
 -- test_login_user_01
 insert into core.core_settings(name, value)
-    values ('jwt_secret', 'gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C');
+    values ('jwt_secret', 'gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C'),
+        ('subscription_interval', '1 month');
 
 insert into platform_service.users(id, name, email, password)
     values ('6d870ed7-65c7-4419-b78b-3946e6f6e695', 'test login user 01', 'test_login_user_01@test.com', crypt('123456', gen_salt('bf')));
@@ -21,5 +22,11 @@ insert into project_service.projects(id, platform_id, user_id, name, mode, perma
 
 insert into payment_service.credit_cards(id, platform_id, user_id, gateway, gateway_data) values ('a41e44d9-5834-4371-afdd-b36bffa8ef9b', '8187a11e-6fa5-4561-a5e5-83329236fbd6', 'd44378a2-3637-447c-9f57-dc20fff574db', 'pagarme', json_build_object(
 'id', 123));
+
+insert into payment_service.subscriptions(id, platform_id, project_id, user_id, credit_card_id, status, created_at, checkout_data)
+    values ('da484d9e-1df2-491f-868b-8aa7ee52680a', '8187a11e-6fa5-4561-a5e5-83329236fbd6', '52273d0a-1610-4f48-9239-e96e5861c3d3', 'd44378a2-3637-447c-9f57-dc20fff574db', 'a41e44d9-5834-4371-afdd-b36bffa8ef9b', 'active', now(), '{"amount": "1500", "customer": {"phone": {"ddd": "21", "ddi": "55", "number": "933448877"}, "address": {"city": "lorem", "state": "MG", "street": "Rua lorem ipsum", "country": "Brasil", "zipcode": "33600000", "neighborhood": "bairro", "complementary": "comple", "street_number": "200"}}, "current_ip": "127.0.0.1", "payment_method": "credit_card", "is_international": false}'::jsonb),
+    ('18124332-701d-4d29-9ab6-6471680edfb5', '8187a11e-6fa5-4561-a5e5-83329236fbd6', '52273d0a-1610-4f48-9239-e96e5861c3d3', 'bb8f4478-df41-411c-8ed7-12c034044c0e', 'a41e44d9-5834-4371-afdd-b36bffa8ef9b', 'active', now(), '{"amount": "1500", "customer": {"phone": {"ddd": "21", "ddi": "55", "number": "933448877"}, "address": {"city": "lorem", "state": "MG", "street": "Rua lorem ipsum", "country": "Brasil", "zipcode": "33600000", "neighborhood": "bairro", "complementary": "comple", "street_number": "200"}}, "current_ip": "127.0.0.1", "payment_method": "credit_card", "is_international": false}'::jsonb);
+insert into payment_service.catalog_payments(id, subscription_id, project_id, platform_id, user_id, created_at, data, status, gateway)
+    values ('cf5dc490-b5bc-4211-8e08-b49219977938', 'da484d9e-1df2-491f-868b-8aa7ee52680a', '52273d0a-1610-4f48-9239-e96e5861c3d3', '8187a11e-6fa5-4561-a5e5-83329236fbd6',  'd44378a2-3637-447c-9f57-dc20fff574db', now(), '{}'::jsonb, 'paid', 'pagarme');
 
 
