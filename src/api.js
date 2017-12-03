2352ee0c2b2c883ed262530823468b1661e29419
Antônio Roberto <forevertonny@gmail.com>
2017-12-03 21:20:46 -0200

    apiInit should receive a apiMeta for every service

--- src/api.js
+++ src/api.js
@@ -1,26 +1,27 @@
 import Postgrest from 'mithril-postgrest';
 
 const platformTokenMeta = document.querySelector('[name="common-platform-token"]');
-const platformToken = platformTokenMeta.getAttribute('content');
+const platformToken = platformTokenMeta ? platformTokenMeta.getAttribute('content') : null;
 const commonRequestHeader = { 'Platform-Code': platformToken };
-const apiInit = (api, authUrl, globalHeader) => {
-    api.init(catarseApiMeta.getAttribute('content'), { method: 'GET', url: authUrl }, globalHeader);
+
+const apiInit = (api, apiMeta, authUrl, globalHeader) => {
+    api.init(apiMeta.getAttribute('content'), { method: 'GET', url: authUrl }, globalHeader);
 }
 
 const catarse = new Postgrest();
 const catarseApiMeta = document.querySelector('[name="api-host"]');
-apiInit(catarse, '/api_token');
+apiInit(catarse, catarseApiMeta, '/api_token');
 
 const commonPayment = new Postgrest();
 const commonPaymentApiMeta = document.querySelector('[name="common-payment-api-host"]');
-apiInit(commonPayment, '/api_token/common', commonRequestHeader);
+apiInit(commonPayment, commonPaymentApiMeta,'/api_token/common', commonRequestHeader);
 
 const commonProject = new Postgrest();
 const commonProjectApiMeta = document.querySelector('[name="common-project-api-host"]');
-apiInit(commonProject, '/api_token/common', commonRequestHeader);
+apiInit(commonProject, commonProjectApiMeta,'/api_token/common', commonRequestHeader);
 
 const commonAnalytics = new Postgrest();
 const commonAnalyticsApiMeta = document.querySelector('[name="common-analytics-api-host"]');
-apiInit(commonAnalytics, '/api_token/common', commonRequestHeader);
+apiInit(commonAnalytics, commonAnalyticsApiMeta, '/api_token/common', commonRequestHeader);
 
 export { catarse, commonPayment, commonProject, commonAnalytics };
