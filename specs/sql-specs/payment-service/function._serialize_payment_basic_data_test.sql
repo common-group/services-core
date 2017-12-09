0bd4d2f228261f214914576882be572a9095cfd2
Antônio Roberto <forevertonny@gmail.com>
2017-12-09 19:14:01 -0200

    added specs for check generate structure on _serialize_payment_basic_data

--- specs/sql-specs/payment-service/function._serialize_payment_basic_data_test.sql
+++ specs/sql-specs/payment-service/function._serialize_payment_basic_data_test.sql
@@ -2,7 +2,7 @@ BEGIN;
     -- import __json_data_payment from helpers
     \i /specs/sql-support/payment_json_build_helpers.sql
 
-    select plan(19);
+    select plan(35);
     -- check function signature
     SELECT function_returns(
         'payment_service', '_serialize_payment_basic_data', ARRAY['json'], 'json' 
@@ -13,9 +13,32 @@ BEGIN;
     create or replace function test_serialize_with_valid_data()
     returns setof text language plpgsql as $$
     declare
+        _result json;
+        _expected json;
     begin
         -- test with valid params
         return next lives_ok('EXECUTE generate_data(''{}'')');
+        _expected := __json_data_payment('{}'::json);
+
+        -- check serialized structure
+        _result := payment_service._serialize_payment_basic_data(_expected);
+
+        return next is(_result ->> 'amount', _expected ->> 'amount');
+        return next is(_result ->> 'payment_method', _expected ->> 'payment_method');
+        return next is(_result -> 'customer' ->> 'name', _expected -> 'customer' ->> 'name');
+        return next is(_result -> 'customer' ->> 'email', _expected -> 'customer' ->> 'email');
+        return next is(_result -> 'customer' ->> 'document_number', _expected -> 'customer' ->> 'document_number');
+        return next is(_result -> 'customer' -> 'address' ->> 'street', _expected -> 'customer' -> 'address' ->> 'street');
+        return next is(_result -> 'customer' -> 'address' ->> 'street_number', _expected -> 'customer' -> 'address' ->> 'street_number');
+        return next is(_result -> 'customer' -> 'address' ->> 'neighborhood', _expected -> 'customer' -> 'address' ->> 'neighborhood');
+        return next is(_result -> 'customer' -> 'address' ->> 'zipcode', _expected -> 'customer' -> 'address' ->> 'zipcode');
+        return next is(_result -> 'customer' -> 'address' ->> 'country', _expected -> 'customer' -> 'address' ->> 'country');
+        return next is(_result -> 'customer' -> 'address' ->> 'state', _expected -> 'customer' -> 'address' ->> 'state');
+        return next is(_result -> 'customer' -> 'address' ->> 'city', _expected -> 'customer' -> 'address' ->> 'city');
+        return next is(_result -> 'customer' -> 'address' ->> 'complementary', _expected -> 'customer' -> 'address' ->> 'complementary');
+        return next is(_result -> 'customer' -> 'phone' ->> 'ddi', _expected -> 'customer' -> 'phone' ->> 'ddi');
+        return next is(_result -> 'customer' -> 'phone' ->> 'ddd', _expected -> 'customer' -> 'phone' ->> 'ddd');
+        return next is(_result -> 'customer' -> 'phone' ->> 'number', _expected -> 'customer' -> 'phone' ->> 'number');
     end;
     $$;
     select test_serialize_with_valid_data();
