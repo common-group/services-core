88003f2bb2b7ee2cb7e357c40e2a6956fd90f241
Antônio Roberto <forevertonny@gmail.com>
2017-12-09 01:52:20 -0200

    Move json_data_payment to helper module

--- specs/sql-specs/payment-service/function._serialize_payment_basic_data_test.sql
+++ specs/sql-specs/payment-service/function._serialize_payment_basic_data_test.sql
@@ -1,95 +1,14 @@
 BEGIN;
+    -- import __json_data_payment from helpers
+    \i /specs/sql-support/payment_json_build_helpers.sql
+
     select plan(19);
-
-    create or replace function _json_data_payment(json) returns json
-    language plpgsql as $$
-    declare
-    data json;
-    begin
-        data := json_build_object(
-            'current_ip', '127.0.0.1',
-            'anonymous', false,
-            'subscription', false,
-            'user_id', 'd44378a2-3637-447c-9f57-dc20fff574db',
-            'project_id', '52273d0a-1610-4f48-9239-e96e5861c3d3',
-            'amount', 2400,
-            'payment_method', 'boleto',
-            'customer', json_build_object(
-                'name', (case 
-                when $1::jsonb ?| '{customer_name}' then
-                    $1->>'customer_name'::text
-                else 'Teste da silva' end) ,
-                'email', (case 
-                when $1::jsonb ?| '{customer_email}' then
-                    $1->>'customer_email'::text
-                else 'test@tesemail.com' end) ,
-                'document_number', (case 
-                when $1::jsonb ?| '{customer_document_number}' then
-                    $1->>'customer_document_number'::text
-                else '889.851.228-78' end) ,
-                'address', json_build_object(
-                    'street', (case 
-                    when $1::jsonb ?| '{customer_address_street}' then
-                        $1->>'customer_address_street'::text
-                    else 'Rua lorem ipsum' end) ,
-                    'street_number', (case 
-                    when $1::jsonb ?| '{customer_address_street_number}' then
-                        $1->>'customer_address_street_number'::text
-                    else '200' end) ,
-                    'neighborhood', (case 
-                    when $1::jsonb ?| '{customer_address_neighborhood}' then
-                        $1->>'customer_address_neighborhood'::text
-                    else 'bairro' end) ,
-                    'zipcode', (case 
-                    when $1::jsonb ?| '{customer_address_zipcode}' then
-                        $1->>'customer_address_zipcode'::text
-                    else '33600000' end) ,
-                    'city', (case 
-                    when $1::jsonb ?| '{customer_address_city}' then
-                        $1->>'customer_address_city'::text
-                    else 'loem city' end) ,
-                    'state', (case 
-                    when $1::jsonb ?| '{customer_address_state}' then
-                        $1->>'customer_address_state'::text
-                    else 'Minas gerais' end) ,
-                    'country', (case 
-                    when $1::jsonb ?| '{customer_address_country}' then
-                        $1->>'customer_address_country'::text
-                    else 'Brasil' end) ,
-                    'complementary', (case 
-                    when $1::jsonb ?| '{customer_address_complementary}' then
-                        $1->>'customer_address_complementary'::text
-                    else 'casa' end)
-                ),
-                'phone', json_build_object(
-                    'ddi', (case 
-                    when $1::jsonb ?| '{customer_phone_ddi}' then
-                        $1->>'customer_phone_ddi'::text
-                    else '55' end) ,
-                    'ddd', (case 
-                    when $1::jsonb ?| '{customer_phone_ddd}' then
-                        $1->>'customer_phone_ddd'::text
-                    else '21' end) ,
-                    'number', (case 
-                    when $1::jsonb ?| '{customer_phone_number}' then
-                        $1->>'customer_phone_number'::text
-                    else '982402833' end)
-                )
-            )
-        );
-
-        data := (data::jsonb || $1::jsonb)::json;
-
-        return data;
-    end;
-    $$;
-
     -- check function signature
     SELECT function_returns(
         'payment_service', '_serialize_payment_basic_data', ARRAY['json'], 'json' 
     );
 
-    prepare generate_data as select * from payment_service._serialize_payment_basic_data(_json_data_payment($1::json));
+    prepare generate_data as select * from payment_service._serialize_payment_basic_data(__json_data_payment($1::json));
 
     create or replace function test_serialize_with_valid_data()
     returns setof text language plpgsql as $$
