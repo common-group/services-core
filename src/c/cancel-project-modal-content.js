f46e1141c965e2156bfc944e9049d57a77417806
Adriano Benin <adriano.benin@gmail.com>
2018-01-19 10:56:46 -0200

    fix project cancel button

--- src/c/cancel-project-modal-content.js
+++ src/c/cancel-project-modal-content.js
@@ -4,15 +4,17 @@
  *
  */
 import m from 'mithril';
+import h from '../h';
 
 const cancelProjectModalContent = {
     controller(args) {
         const checkError = m.prop(false),
+            showRedactor = m.prop(false),
             check = m.prop(''),
+            commentHtml = m.prop(''),
             showNextModal = () => {
                 if (check() === 'cancelar-projeto') {
-                    args.displayModal.toggle();
-                    document.getElementById('send-message').style.display = 'block';
+                    showRedactor(true);
                 } else {
                     checkError(true);
                 }
@@ -21,55 +23,88 @@ const cancelProjectModalContent = {
 
         return {
             showNextModal,
+            commentHtml,
+            showRedactor,
             checkError,
             check
         };
     },
 
     view(ctrl, args) {
-        return m('form.cancel-project-modal.modal-dialog-content', { onsubmit: ctrl.showNextModal },
-            [
-                m('.fontsize-small.u-marginbottom-20',
-                    [
-                        'Após o cancelamento, sua campanha será expirada e os seus apoiadores serão reembolsados dentro das próximas 24h horas.',
-                        m('span.fontweight-semibold',
-                                'Essa ação não poderá ser desfeita!'
-                            ),
-                        m('br'),
-                        m('span.fontweight-semibold')
-                    ]
-                    ),
-                m('.fontsize-small.u-marginbottom-10',
-                    [
-                        'Se você tem certeza que deseja cancelar seu projeto, confirme escrevendo ',
-                        m('span.fontweight-semibold.text-error',
-                                'cancelar-projeto '
-                            ),
-                        'no campo abaixo. Em seguida lhe pediremos para escrever uma mensagem aos apoiadores e seu projeto será então cancelado.',
-                        m('span.fontweight-semibold.text-error')
-                    ]
+        return m(`form.cancel-project-modal.modal-dialog-content[accept-charset='UTF-8'][action='/pt/projects/${args.project.id}'][id='edit_project_${args.project.id}'][method='post'][novalidate='novalidate']`,
+            ctrl.showRedactor() ? [
+                m("input[name='utf8'][type='hidden'][value='✓']"),
+                m("input[name='_method'][type='hidden'][value='patch']"),
+                m(`input[name='authenticity_token'][type='hidden'][value='${h.authenticityToken()}']`),
+                m("input[id='anchor'][name='anchor'][type='hidden'][value='posts']"),
+                m("input[id='cancel_project'][name='cancel_project'][type='hidden'][value='true']"),
+                m('.fontsize-smaller.u-marginbottom-20',
+                    'Conte porque você está cancelando sua campanha. Essa mensagem será enviada por email para os seus apoiadores e estará pública na aba "Novidades" do seu projeto no Catarse.'
                 ),
-                m('.w-form',
-                    [
-                        m('input.positive.text-field.u-marginbottom-40.w-input[maxlength=\'256\'][type=\'text\']', { class: !ctrl.checkError() ? false : 'error', placeholder: 'cancelar-projeto', onchange: m.withAttr('value', ctrl.check) })
-                    ]
+                m('.w-form', [
+                    m("label.string.required.field-label.field-label.fontweight-semibold[for='project_posts_attributes_0_title']",
+                        'Título'
                     ),
+                    m("input.string.required.w-input.text-field.w-input.text-field.positive[id='project_posts_attributes_0_title'][name='project[posts_attributes][0][title]'][type='text']"),
+                    m("label.string.optional.field-label.field-label.fontweight-semibold[for='project_posts_attributes_0_comment']",
+                        'Texto'
+                    ),
+                    h.redactor('project[posts_attributes][0][comment_html]', ctrl.commentHtml)
+                ]),
                 m('div',
-                        m('.w-row',
-                            [
-                                m('.w-col.w-col-3'),
-                                m('.u-text-center.w-col.w-col-6',
-                                    [
-                                        m('input.btn.btn-inactive.btn-large.u-marginbottom-20[type=\'submit\'][value=\'Próximo passo >\']'),
-                                        m('a.fontsize-small.link-hidden-light[href=\'#\']', { onclick: args.displayModal.toggle },
-                                            'Cancelar'
-                                        )
-                                    ]
-                                ),
-                                m('.w-col.w-col-3')
-                            ]
-                        )
-                    )
+                    m('.w-row', [
+                        m('.w-col.w-col-3'),
+                        m('.u-text-center.w-col.w-col-6', [
+                            m("input.btn.btn-inactive.btn-large.u-marginbottom-20[name='commit'][type='submit'][value='Cancelar campanha']"),
+                            m(".fontsize-small.link-hidden-light[id='modal-close']", {
+                                onclick: args.displayModal.toggle
+                            },
+                                'Cancelar'
+                            )
+                        ]),
+                        m('.w-col.w-col-3')
+                    ])
+                )
+            ] : [
+                m('.fontsize-small.u-marginbottom-20', [
+                    'Após o cancelamento, sua campanha será expirada e os seus apoiadores serão reembolsados dentro das próximas 24h horas.',
+                    m('span.fontweight-semibold',
+                        'Essa ação não poderá ser desfeita!'
+                    ),
+                    m('br'),
+                    m('span.fontweight-semibold')
+                ]),
+                m('.fontsize-small.u-marginbottom-10', [
+                    'Se você tem certeza que deseja cancelar seu projeto, confirme escrevendo ',
+                    m('span.fontweight-semibold.text-error',
+                        'cancelar-projeto '
+                    ),
+                    'no campo abaixo. Em seguida lhe pediremos para escrever uma mensagem aos apoiadores e seu projeto será então cancelado.',
+                    m('span.fontweight-semibold.text-error')
+                ]),
+                m('.w-form', [
+                    m('input.positive.text-field.u-marginbottom-40.w-input[maxlength=\'256\'][type=\'text\']', {
+                        class: !ctrl.checkError() ? false : 'error',
+                        placeholder: 'cancelar-projeto',
+                        onchange: m.withAttr('value', ctrl.check)
+                    })
+                ]),
+                m('div',
+                    m('.w-row', [
+                        m('.w-col.w-col-3'),
+                        m('.u-text-center.w-col.w-col-6', [
+                            m('button.btn.btn-inactive.btn-large.u-marginbottom-20', {
+                                onclick: ctrl.showNextModal
+                            }, 'Próximo passo >'),
+                            m('a.fontsize-small.link-hidden-light[href=\'#\']', {
+                                onclick: args.displayModal.toggle
+                            },
+                                'Cancelar'
+                            )
+                        ]),
+                        m('.w-col.w-col-3')
+                    ])
+                )
             ]);
     }
 };
