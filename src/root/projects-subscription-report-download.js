9192f9dc343b2133587250d380fa1c635bbd1785
Thiago Maia <thiago@catarse.me>
2018-01-31 18:32:26 +1100

    Warning on report download about sensible data

--- src/root/projects-subscription-report-download.js
+++ src/root/projects-subscription-report-download.js
@@ -43,6 +43,9 @@ const projectSubscriptionReportDownload = {
                                 ),
                                 'Baixar relatórios'
                             ]),
+                            m('.card.u-radius.u-marginbottom-20', [
+                                'Atenção: Ao realizar o download desses dados, você se compromete a armazená-los em local seguro e respeitar o direitos dos usuários conforme o que está previsto nos Termos de Uso e na política de privacidade do Catarse.'
+                            ]),                                  
                             m('ul.w-list-unstyled', [
                                 m('li.fontsize-smaller.u-marginbottom-10',
                                     m('div', [
