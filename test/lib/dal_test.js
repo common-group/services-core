b5fe9da06905faa293fb6d89eb18f031f7f89026
Antônio Roberto <forevertonny@gmail.com>
2018-02-22 15:26:33 -0300

    Added dal tests and adjusts methods to receive the pg client

--- test/lib/dal_test.js
+++ test/lib/dal_test.js
@@ -1,24 +1,19 @@
 const { test } = require('ava');
-const {Pool} = require('pg');
-const { _ } = require('lodash');
+const { Pool } = require('pg');
+const R = require('ramda');
 const helpers = require('../support/helpers');
 const {
     pool,
-    loadPaymentContext
+    loadPaymentContext,
+    createCardFromPayment,
+    updateGatewayDataOnPayment
 } = require('../../lib/dal')
 
-let basicData;
-let client;
-
-// generate basic database data
-test.before( async t => {
-    client = await pool.connect();
-});
-
 test('test loadPaymentContext', async t => {
+    const client = await pool.connect();
     await client.query('begin;');
     // insert fixtures (platform, project, users)
-    basicData = await helpers.insertBasicData(client);
+    const basicData = await helpers.insertBasicData(client);
 
     // insert payment into database
     const payment = (await client.query(`
@@ -44,11 +39,106 @@ test('test loadPaymentContext', async t => {
     t.deepEqual(ctx.project.data, basicData.project.data);
 
     await client.query('rollback;')
+    await client.release();
 });
 
-// remove generated data
-test.after.always( async t => {
-    if(client) {
-        await client.release();
-    }
+test('test createCardFromPayment', async t => {
+    const client = await pool.connect();
+    await client.query('begin;');
+    // insert fixtures (platform, project, users)
+    const basicData = await helpers.insertBasicData(client);
+
+    // insert payment into database
+    const payment = (await client.query(`
+        insert into payment_service.catalog_payments
+        (status, created_at, gateway, platform_id, user_id, project_id, data, gateway_cached_data) 
+        values ('paid', '01-31-2018 13:00', 'pagarme', $1::uuid, $2::uuid, $3::uuid, $4::json, $5::json)
+        returning *`, [
+        basicData.platform.id,
+        basicData.community_first_user.id,
+        basicData.project.id,
+        JSON.stringify(helpers.paymentBasicData({})),
+        JSON.stringify(helpers.paymentBasicGatewayCachedData({}))
+    ])).rows[0];
+    
+    const transaction = payment.gateway_cached_data.transaction;
+    const created_card = await createCardFromPayment(client, payment.id);
+
+    t.deepEqual(created_card.gateway_data, transaction.card);
+    t.is(created_card.gateway_data.id, transaction.card.id);
+    t.is(created_card.platform_id, basicData.platform.id);
+    t.is(created_card.user_id, basicData.community_first_user.id);
+
+    await client.query('rollback;');
+    await client.release();
 });
+
+test('test updateGatewayDataOnPayment', async t => {
+    const client = await pool.connect();
+    await client.query('begin;');
+
+    // insert fixtures (platform, project, users)
+    const basicData = await helpers.insertBasicData(client);
+
+    // insert payment into database
+    const payment = (await client.query(`
+        insert into payment_service.catalog_payments
+        (status, created_at, gateway, platform_id, user_id, project_id, data)
+        values ('paid', '01-31-2018 13:00', 'pagarme', $1::uuid, $2::uuid, $3::uuid, $4::json)
+        returning *`, [
+        basicData.platform.id,
+        basicData.community_first_user.id,
+        basicData.project.id,
+        JSON.stringify(helpers.paymentBasicData({}))
+    ])).rows[0];
+
+    t.is(R.isNil(payment.gateway_data), true);
+    const { payables, transaction } = helpers.paymentBasicGatewayCachedData({});
+    const updated_payment = await updateGatewayDataOnPayment(client, payment.id, transaction, payables);
+    const expected_general_data = {
+        boleto_barcode: null,
+        boleto_expiration_date: null,
+        boleto_url: null,
+        card_brand: 'visa',
+        card_country: 'UNITED STATES',
+        card_fingerprint: 'cj5bw4cio00000j23jx5l60cq',
+        card_first_digits: '411111',
+        card_holder_name: 'LOREM  NAME',
+        card_id: 'card_id_foo_bar',
+        card_last_digits: '1111',
+        customer_document_number: '11111111111',
+        customer_document_type: 'cpf',
+        customer_email: 'lorem@email.com',
+        customer_name: 'Lorem name',
+        gateway_acquirer_response_code: '00',
+        gateway_cost: 25,
+        gateway_id: '2933402',
+        gateway_ip: '127.0.0.1',
+        gateway_payment_method: 'credit_card',
+        gateway_refuse_reason: null,
+        gateway_status: 'paid',
+        gateway_status_reason: 'acquirer',
+        installments: '1',
+        payable_first_compensation_date: null,
+        payable_last_compensation_date: null,
+        payable_total_fee: null,
+        payables: null,
+    };
+
+    t.is(payment.id, updated_payment.id);
+    t.is(R.isNil(updated_payment.gateway_cached_data), false);
+    t.is(R.isNil(updated_payment.gateway_general_data), false);
+    t.deepEqual(updated_payment.gateway_cached_data.transaction, transaction);
+    t.deepEqual(updated_payment.gateway_cached_data.payables, payables);
+
+    t.deepEqual(updated_payment.gateway_general_data, expected_general_data);
+
+    //t.deepEqual(created_card.gateway_data, transaction.card);
+    //t.is(created_card.gateway_data.id, transaction.card.id);
+    //t.is(created_card.platform_id, basicData.platform.id);
+    //t.is(created_card.user_id, basicData.community_first_user.id);
+
+    await client.query('rollback;');
+    await client.release();
+});
+
