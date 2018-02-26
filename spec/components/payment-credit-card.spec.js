979e2802bf20839057966941c09c6ae9d7dc15aa
Vinicius Andrade <vini.andrade.dev@gmail.com>
2018-02-26 22:26:47 +0100

    Fix test, intialize component properly

--- spec/components/payment-credit-card.spec.js
+++ spec/components/payment-credit-card.spec.js
@@ -14,7 +14,8 @@ describe('paymentCreditCard', () => {
                 vm: vm,
                 contribution_id: 1,
                 project_id: 1,
-                user_id: 1
+                user_id: 1,
+                isSubscriptionEdit: () => false                
             };
             $output = mq(
                 m(paymentCreditCard, test)
