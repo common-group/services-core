176e4d7412f3d3fdb3e99f618ec596e78fc5f48c
Vinicius Andrade <vini.andrade.dev@gmail.com>
2018-03-06 21:00:19 +0100

    momentify next_charge_at

--- src/root/projects-subscription-checkout.js
+++ src/root/projects-subscription-checkout.js
@@ -391,7 +391,7 @@ const projectsSubscriptionCheckout = {
                                     ),
                                     ctrl.isEdit()
                                         ? ctrl.oldSubscription().next_charge_at
-                                            ? ctrl.oldSubscription().next_charge_at
+                                            ? h.momentify(ctrl.oldSubscription().next_charge_at)
                                             : h.momentify(Date.now())
                                         : h.lastDayOfNextMonth()
                                 ]
