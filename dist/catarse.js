2d496d0495fe09301195f99c780119989cf96fa7
Antônio Roberto <forevertonny@gmail.com>
2018-03-22 19:09:16 -0300

    redist

--- dist/catarse.js
+++ dist/catarse.js
@@ -17361,7 +17361,7 @@ var I18nScope$70 = _.partial(h.i18nScope, 'projects.dashboard_start');
 var projectEditStart = {
     controller: function controller(args) {},
     view: function view(ctrl, args) {
-        return m('.dashboard-header.min-height-70.u-text-center', m('.w-container', m('.u-marginbottom-40.w-row', [m('.w-col.w-col-8.w-col-push-2', [m('.fontsize-larger.fontweight-semibold.lineheight-looser.u-marginbottom-10', I18n.t('title', I18nScope$70())), m('.fontsize-small.lineheight-loose.u-marginbottom-40', I18n.t('description', I18nScope$70({ name: args.project().user.name || '' }))), m('.card.card-terciary.u-radius', m('iframe[allowfullscreen="true"][frameborder="0"][scrolling="no"][mozallowfullscreen="true"][webkitallowfullscreen="true"][src=' + I18n.t('video_src', I18nScope$70()) + ']'))])])));
+        return m('.dashboard-header.min-height-70.u-text-center', m('.w-container', m('.u-marginbottom-40.w-row', [m('.w-col.w-col-8.w-col-push-2', [m('.fontsize-larger.fontweight-semibold.lineheight-looser.u-marginbottom-10', I18n.t('title', I18nScope$70())), m('.fontsize-small.lineheight-loose.u-marginbottom-40', I18n.t('description', I18nScope$70({ name: args.project().user.name || '' }))), m('.card.card-terciary.u-radius', m('iframe[allowfullscreen="true"][width="630"][height="383"][frameborder="0"][scrolling="no"][mozallowfullscreen="true"][webkitallowfullscreen="true"][src=' + I18n.t('video_src', I18nScope$70()) + ']'))])])));
     }
 };
 
