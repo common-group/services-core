6a31ac534b60929fd2528902da805ecb164ef0b6
Vinicius Andrade <vini.andrade.dev@gmail.com>
2018-07-03 21:02:29 +0200

    Improves webpack configuration, re-enable babel env preset
    Signed-off-by: Vinicius Andrade <vini.andrade.dev@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    },
     output: {
         filename: 'catarse.js',
         path: path.resolve(__dirname, 'dist'),
