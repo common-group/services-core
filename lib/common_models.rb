82e783f5c1ed6f17cceead84d1f5f6c3bdc5d188
Adriano Benin <adriano.benin@gmail.com>
2018-07-11 17:42:26 -0300

    require new models

--- lib/common_models.rb
+++ lib/common_models.rb
@@ -5,6 +5,10 @@ require 'common_models/config'
 require 'common_models/models/user'
 require 'common_models/models/project'
 require 'common_models/models/goal'
+require 'common_models/models/reward'
+require 'common_models/models/subscription'
+require 'common_models/models/notification'
+require 'common_models/models/catalog_payment'
 
 
 module CommonModels
