8d854a97ad5f12c1e0289dfdfff26169356b89ee
Vinicius Andrade <vini.andrade.dev@gmail.com>
2018-07-11 09:26:23 +0200

    Compile test using webpack
    Signed-off-by: Vinicius Andrade <vini.andrade.dev@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -1,83 +1,47 @@
-// Karma configuration
-// Generated on Sat Sep 05 2015 18:32:11 GMT-0300 (BRT)
-
-module.exports = function(config) {
-  config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      'spec/lib/mithril-query/mithril-query.js',
-      'spec/lib/jasmine-species/jasmine-grammar.js',
-      'spec/lib/jasmine-matchers.js',
-      'spec/lib/jasmine-ajax/mock-ajax.js',
-      'spec/lib/i18n/i18n.js',
-      'spec/lib/matchers.js',
-      'spec/lib/analytics.js',
-      'node_modules/mithril/mithril.js',
-      'node_modules/underscore/underscore.js',
-      'node_modules/liquidjs/dist/liquid.js',
-      'node_modules/mithril-postgrest/dist/mithril-postgrest.js',
-      'node_modules/chart.js/Chart.js',
-      'node_modules/moment/moment.js',
-      'node_modules/select/dist/select.js',
-      'node_modules/jquery/dist/jquery.min.js',
-      'vendor/replaceDiacritics.js',
-      'spec/lib/mocks/*mock.js',
-      'spec/bundle.spec.js',
-    ],
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['NodeWebkitHidden'],
-    customLaunchers: {
-        'NodeWebkitHidden': {
-          base: 'NodeWebkit',
-          options: {
-              window: {
-                show: false,
-              }
-          }
-        }
-    },
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  })
-}
+module.exports = (config) => {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'spec/lib/mithril-query/mithril-query.js',
+            'spec/lib/jasmine-species/jasmine-grammar.js',
+            'spec/lib/jasmine-matchers.js',
+            'spec/lib/jasmine-ajax/mock-ajax.js',
+            'spec/lib/i18n/i18n.js',
+            'spec/lib/matchers.js',
+            'spec/lib/analytics.js',
+            'node_modules/mithril/mithril.js',
+            'node_modules/underscore/underscore.js',
+            'node_modules/liquidjs/dist/liquid.js',
+            'node_modules/mithril-postgrest/dist/mithril-postgrest.js',
+            'node_modules/chart.js/Chart.js',
+            'node_modules/moment/moment.js',
+            'node_modules/select/dist/select.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'vendor/replaceDiacritics.js',
+            'spec/lib/mocks/*mock.js',
+            'spec/index.spec.js',
+        ],
+        preprocessors: {
+            'spec/**/*.spec.js': ['webpack'],
+            'spec/index.spec.js': ['webpack']
+        },
+        exclude: [],
+        reporters: ['spec'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        browsers: ['NodeWebkitHidden'],
+        customLaunchers: {
+            NodeWebkitHidden: {
+                base: 'NodeWebkit',
+                options: {
+                    window: {
+                        show: false,
+                    }
+                }
+            }
+        },
+        singleRun: true
+    });
+};
