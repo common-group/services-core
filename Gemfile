717b33314bc642eab099e6beb0853d8fe6efb163
Adriano Benin <adriano.benin@gmail.com>
2018-07-11 17:40:23 -0300

    initialize db connection

--- Gemfile
+++ Gemfile
@@ -44,4 +44,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'pundit'
 
 gem 'common_models', path: '../common_models/'
-gem 'postgrest', path: '../../postgrest-ruby'
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 
 module CommonApi
   class Application < Rails::Application
+    extend CommonModels
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
