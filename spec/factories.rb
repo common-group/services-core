21b84cee8628506fee42501e47bb912042ec321a
Antônio Roberto <forevertonny@gmail.com>
2018-07-13 13:45:28 -0300

    add basic factories file

---
+++ spec/factories.rb
@@ -0,0 +1,30 @@
+FactoryBot.define do
+  factory :platform, class: CommonModels::Platform do
+    name 'Platform name'
+  end
+
+  factory :project, class: CommonModels::Project do
+    goal 100
+    headline 'Foo headline'
+    about_html 'foo html description'
+    state 'draft'
+    mode 'sub'
+    name 'Foo project name'
+    service_fee 0.13
+    sequence(:permalink) do |n|
+      "permalink_#{n}"
+    end
+    user
+    platform
+  end
+
+  factory :user, class: CommonModels::User do
+    platform
+    name 'foo user'
+    sequence(:email) do |n|
+      "email#{n}@email.com"
+    end
+    account_type 'pf'
+    password '123456'
+  end
+end
