9d9564a44fa6748195652d660db0ed32b90a00cd
Antônio Roberto <forevertonny@gmail.com>
2018-07-23 13:40:29 -0300

    rollback to hs256 for support generating internal secrets

---
+++ migrations/2018-07-23-163655_rollbackhs256/up.sql
@@ -0,0 +1,9 @@
+-- Your SQL goes here
+CREATE OR REPLACE FUNCTION core.gen_jwt_token(json)
+ RETURNS core.jwt_token
+ LANGUAGE sql
+ STABLE
+AS $function$
+        select core.sign($1::json, core.get_setting('jwt_secret'));
+    $function$
+;
