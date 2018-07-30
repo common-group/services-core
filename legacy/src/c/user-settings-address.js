96847a2ef9e4a0f2a5c328581af051b4eaeb00de
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-07-30 15:30:36 -0300

    split user settings forms #158139162

---
+++ legacy/src/c/user-settings-address.js
@@ -0,0 +1,31 @@
+import m from 'mithril';
+import _ from 'underscore';
+import bigCard from './big-card';
+import h from '../h';
+import addressForm from './address-form';
+
+const I18nScope = _.partial(h.i18nScope, 'users.edit.settings_tab');
+
+const userSettingsAddress = {
+    controller: function(args)
+    {
+        return {};
+    },
+    view: function(ctrl, args)
+    {
+        const
+            fields = args.fields,
+            parsedErrors = args.parsedErrors;
+
+        return m(bigCard, {
+            label: window.I18n.t('address_title', I18nScope()),
+            label_hint: window.I18n.t('address_subtitle', I18nScope()),
+            children: [
+                m('.divider.u-marginbottom-20'),
+                m(addressForm, { fields, parsedErrors })
+            ]
+        });
+    }
+};
+
+export default userSettingsAddress;
--- legacy/src/root/project-edit-user-settings.js
+++ legacy/src/root/project-edit-user-settings.js
@@ -17,7 +17,8 @@ const projectEditUserSettings = {
             userId: args.user_id,
             hideCreditCards: true,
             useFloatBtn: true,
-            publishingUserSettings: true
+            publishingUserSettings: true,
+            isProjectUserEdit: true
         }) : m('div', h.loader()));
     }
 };
