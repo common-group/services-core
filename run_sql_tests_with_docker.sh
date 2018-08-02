fafb36c74378411e3f703a5f7590fac445e9ce24
Antônio Roberto <forevertonny@gmail.com>
2018-08-02 14:23:57 -0300

    check last command status

--- run_sql_tests_with_docker.sh
+++ run_sql_tests_with_docker.sh
@@ -2,7 +2,9 @@
 function __clean_containers() {
     docker stop common_service_db_inline_test
     #docker rm sql_test_files_inline_common 
-    if [ $(docker rm sql_test_files_inline_common) -eq 1]; then
+    docker rm sql_test_files_inline_common
+    if [ $? -eq 1 ]
+    then
         echo 'removed sql inline container'
     else
         echo 'sql inline container already removed'
