7a2d2d319326e01126bad5fb0ac99d436e60d523
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-08-08 20:56:30 -0300

    total paid and paid count filters added #158587322 #158551900

---
+++ legacy/src/c/filter-text.js
@@ -0,0 +1,27 @@
+import m from 'mithril';
+
+const filterText = {
+    view: function (ctrl, args) {
+
+        return m(args.wrapper_class, [
+            m('.fontsize-smaller.u-text-center',
+                args.label
+            ),
+            m('.w-row', [
+                m('.text-field.positive.prefix.no-hover.w-col.w-col-2.w-col-small-2.w-col-tiny-2',
+                    m('a.w-inline-block[href=\'#\']',
+                        m('img.header-lupa[src=\'https://uploads-ssl.webflow.com/5991cfb722e8860001b12d51/5991cfb722e8860001b12df0_lupa.png\']')
+                    )
+                ),
+                m('.w-col.w-col-10.w-col-small-10.w-col-tiny-10',
+                    m(`input.text-field.postfix.positive.w-input[maxlength='256'][placeholder='${args.placeholder}'][type='text']`, {
+                        onchange: m.withAttr('value', args.vm),
+                        value: args.vm()
+                    })
+                )
+            ])
+        ]);       
+    }
+};
+
+export default filterText;
