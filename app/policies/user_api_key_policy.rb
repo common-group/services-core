94d3289c00b6967b34b36430c412784c2d57e19c
Antônio Roberto <forevertonny@gmail.com>
2018-08-07 21:28:10 -0300

    remove unused methods

--- app/policies/user_api_key_policy.rb
+++ app/policies/user_api_key_policy.rb
@@ -20,14 +20,4 @@ class UserApiKeyPolicy < ApplicationPolicy
   def user_is_from_platform?
     record.user.try(:platform_id) == record.platform_id
   end
-
-  private
-
-  def is_project_owner?
-    record.project.user_id == user.id
-  end
-
-  def is_admin?
-    #user.user_roles.any? {|r| r.name == 'admin'}
-  end
 end
