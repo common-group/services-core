69bc378918a8fa903b238662dc1ba4de83403701
Adriano Benin <adriano.benin@gmail.com>
2018-08-13 15:05:46 -0300

    fix migration

--- migrations/2018-08-13-180019_drop_reward_platform/up.sql
+++ migrations/2018-08-13-180019_drop_reward_platform/up.sql
@@ -1,2 +1,20 @@
 -- Your SQL goes here
+CREATE OR REPLACE VIEW "project_service_api"."rewards" AS 
+ SELECT r.id,
+    r.external_id,
+    r.project_id,
+    r.data,
+    ((r.data ->> 'metadata'::text))::jsonb AS metadata,
+    r.created_at,
+    r.updated_at,
+    ( SELECT count(*) AS count
+           FROM payment_service.subscriptions s
+          WHERE ((s.reward_id = r.id) AND (s.status = 'active'::payment_service.subscription_status))) AS subscribed_count
+   FROM project_service.rewards r
+  WHERE (((select platform_id from project_service.projects p where p.id = r.project_id limit 1) = core.current_platform_id()) AND core.has_any_of_roles('{platform_user,scoped_user,anonymous}'::text[]))
+  ORDER BY r.id DESC;
+
+---
+
 ALTER TABLE project_service.rewards DROP COLUMN IF EXISTS platform_id;
+
