90bbce3ddea509589dfebed57e6626fe13586021
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-08-13 15:07:30 -0300

    fix NaN, enter key close filter dropdown and search image button asset to local #159679966

--- legacy/src/c/filter-text.js
+++ legacy/src/c/filter-text.js
@@ -15,7 +15,7 @@ const filterText = {
             m('.w-row', [
                 m('.text-field.positive.prefix.no-hover.w-col.w-col-2.w-col-small-2.w-col-tiny-2',
                     m('a.w-inline-block[href=\'#\']', buttonOptions,
-                        m('img.header-lupa[src=\'https://uploads-ssl.webflow.com/5991cfb722e8860001b12d51/5991cfb722e8860001b12df0_lupa.png\']')
+                        m('img.header-lupa[src=\'/assets/catarse_bootstrap/lupa_grey.png\']')
                     )
                 ),
                 m('.w-col.w-col-10.w-col-small-10.w-col-tiny-10',
