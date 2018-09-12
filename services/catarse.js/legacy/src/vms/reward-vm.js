5b9d67a40f976428049f7aa03caa01bd311a2b50
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2018-09-12 17:01:09 -0300

    fixed rewrite value on contribution card #160392497
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- services/catarse.js/legacy/src/vms/reward-vm.js
+++ services/catarse.js/legacy/src/vms/reward-vm.js
@@ -79,7 +79,12 @@ const selectReward = reward => () => {
         if (reward.id) {
             contributionValue(h.applyMonetaryMask(`${reward.minimum_value},00`));
         } else { // no reward
-            contributionValue(h.applyMonetaryMask('$10,00'));
+            if (contributionValue() === '10,00' || 
+                contributionValue() === '' || 
+                contributionValue() === 0 || 
+                contributionValue() === null || 
+                contributionValue() === undefined)
+                contributionValue(h.applyMonetaryMask('$10,00'));
         }
 
         if (reward.id) {
