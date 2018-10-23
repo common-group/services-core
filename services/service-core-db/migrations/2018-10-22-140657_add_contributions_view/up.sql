8c973d70d36f75f19bc8346fa9cdd4d2e5e2fdb1
Adriano Benin <adriano.benin@gmail.com>
2018-10-23 11:59:10 -0300

    remove anonymous access

--- services/service-core-db/migrations/2018-10-22-140657_add_contributions_view/up.sql
+++ services/service-core-db/migrations/2018-10-22-140657_add_contributions_view/up.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE VIEW "payment_service_api"."contributions" AS
     c.created_at,
     c.updated_at
    FROM payment_service.contributions c
-  WHERE core.has_any_of_roles('{platform_user,scoped_user,anonymous}'::text[])
+  WHERE core.has_any_of_roles('{platform_user,scoped_user}'::text[])
   ORDER BY c.id DESC;
 
 GRANT SELECT ON payment_service_api.contributions TO platform_user;
