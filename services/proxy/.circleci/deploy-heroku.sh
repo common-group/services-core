04d0fe5669efbe4eadc5a1c5a3a05b7f1007fb2e
Antônio Roberto <forevertonny@gmail.com>
2019-02-07 02:02:44 -0200

    fix wrong redirection

--- services/proxy/.circleci/deploy-heroku.sh
+++ services/proxy/.circleci/deploy-heroku.sh
@@ -4,5 +4,5 @@ set -e
 echo 'pushing to heroku'
 npx heroku container:login
 npx heroku container:push web --recursive -a $PRODUCTION_PROXY_APP 1> /dev/null
-npx heroku container:release web -a $PRODUCTION_PROXY_APP /dev/null
+npx heroku container:release web -a $PRODUCTION_PROXY_APP 1> /dev/null
 
