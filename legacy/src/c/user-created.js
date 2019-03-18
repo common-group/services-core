bd26c915ae4f9f900958d59d3959647aaade8f9f
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-03-18 16:23:12 -0300

    user balance drawing transactions properly and transaction history using vm
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/src/c/user-created.js
+++ legacy/src/c/user-created.js
@@ -45,46 +45,61 @@ const userCreated = {
     },
     view: function({state, attrs}) {
         const projects_collection = state.projects.collection();
-        console.log(projects_collection);
 
         return m('.content[id=\'created-tab\']',
-            (state.error() ? m(inlineError, {
-                message: 'Erro ao carregar os projetos.'
-            }) : !state.loader() ? [
-                (!_.isEmpty(projects_collection) ? _.map(projects_collection, project => m(projectCard, {
-                    project,
-                    ref: 'user_contributed',
-                    showFriends: false
-                })) :
-                    m('.w-container',
-                        m('.u-margintop-30.u-text-center.w-row', [
-                            m('.w-col.w-col-3'),
-                            m('.w-col.w-col-6', [
-                                m('.fontsize-large.u-marginbottom-30',
-                                    'O que você está esperando para tirar seu projeto do papel aqui no Catarse?'),
-                                m('.w-row', [
-                                    m('.w-col.w-col-3'),
-                                    m('.w-col.w-col-6',
-                                        m('a.btn.btn-large[href=\'/start\']',
-                                            'Comece agora!'
+            (
+                state.error() ? 
+                    m(inlineError, { message: 'Erro ao carregar os projetos.' }) 
+                : 
+                
+                    (
+                        !state.loader() ? 
+                            [
+                                (
+                                    !_.isEmpty(projects_collection) ? 
+                                        _.map(projects_collection, project => m(projectCard, {
+                                            project,
+                                            ref: 'user_contributed',
+                                            showFriends: false
+                                        })) 
+                                    :
+                                        m('.w-container',
+                                            m('.u-margintop-30.u-text-center.w-row', [
+                                                m('.w-col.w-col-3'),
+                                                m('.w-col.w-col-6', [
+                                                    m('.fontsize-large.u-marginbottom-30',
+                                                        'O que você está esperando para tirar seu projeto do papel aqui no Catarse?'),
+                                                    m('.w-row', [
+                                                        m('.w-col.w-col-3'),
+                                                        m('.w-col.w-col-6',
+                                                            m('a.btn.btn-large[href=\'/start\']',
+                                                                'Comece agora!'
+                                                            )
+                                                        ),
+                                                        m('.w-col.w-col-3')
+                                                    ])
+                                                ]),
+                                                m('.w-col.w-col-3')
+                                            ])
                                         )
-                                    ),
-                                    m('.w-col.w-col-3')
-                                ])
-                            ]),
-                            m('.w-col.w-col-3')
-                        ])
+                                ),
+                                // Load more button        
+                                (
+                                    !_.isEmpty(projects_collection) ?
+                                        m('.w-row.u-marginbottom-40.u-margintop-30', [
+                                            m(loadMoreBtn, {
+                                                collection: state.projects,
+                                                cssClass: '.w-col-push-5'
+                                            })
+                                        ]) 
+                                    : 
+                                        ''
+                                )
+                            ] 
+                        : 
+                            h.loader()
                     )
-                ),
-
-                (!_.isEmpty(projects_collection) ?
-                    m('.w-row.u-marginbottom-40.u-margintop-30', [
-                        m(loadMoreBtn, {
-                            collection: state.projects,
-                            cssClass: '.w-col-push-5'
-                        })
-                    ]) : '')
-            ] : h.loader())
+            )
         );
     }
 };
