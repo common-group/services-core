ff8a0f6dc7304e56ab2fd5cacef465024c6c3172
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-04-01 09:27:24 -0300

    updating payment flow
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/src/c/credit-card-input.js
+++ legacy/src/c/credit-card-input.js
@@ -5,12 +5,11 @@ import creditCardVM from '../vms/credit-card-vm';
 const creditCardInput = {
     oninit: function(vnode) {
         const cardType = vnode.attrs.type || prop('unknown');
-      // TODO: move all input logic to vdom paradigm
-      // CreditCard Input still handle events on a dom-based model.
-        const setCreditCardHandlers = (el, isInitialized) => {
-            if (!isInitialized) {
-                creditCardVM.setEvents(el, cardType, vnode.attrs.value);
-            }
+        // TODO: move all input logic to vdom paradigm
+        // CreditCard Input still handle events on a dom-based model.
+        const cardNumberProp = vnode.attrs.value;
+        const setCreditCardHandlers = (vnode) => {
+            creditCardVM.setEvents(vnode.dom, cardType, cardNumberProp);
         };
 
         vnode.state = {
@@ -22,7 +21,7 @@ const creditCardInput = {
         return m(`input.w-input.text-field[name="${attrs.name}"][required="required"][type="tel"]`, {
             onfocus: attrs.onfocus,
             class: attrs.class,
-            config: state.setCreditCardHandlers,
+            oncreate: state.setCreditCardHandlers,
             onblur: attrs.onblur
         });
     }
