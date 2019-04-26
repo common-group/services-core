423676ad21415ec4b0c45eb1790c95df9901ab25
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-04-26 19:17:22 -0300

    fixing ui buttons data representation
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- services/catarse.js/legacy/src/c/project-reward-card.js
+++ services/catarse.js/legacy/src/c/project-reward-card.js
@@ -115,7 +115,7 @@ const projectRewardCard = {
                     reward_value: reward.minimum_value
                 }
             }, state.selectReward(reward)),
-            config: state.isRewardOpened(reward) ? h.scrollTo() : Function.prototype
+            oncreate: state.isRewardOpened(reward) ? h.scrollTo() : Function.prototype
         }, [
             m('.u-marginbottom-20', [
                 m('.fontsize-base.fontweight-semibold', `Para R$ ${h.formatNumber(reward.minimum_value)} ou mais${isSub ? ' por mês' : ''}`),
@@ -167,13 +167,11 @@ const projectRewardCard = {
                     ])
                 ]))
             ] : '',
-            (reward.run_out ? '' :
-             m('.fontcolor-secondary.fontsize-smallest.fontweight-semibold',
-               h.pluralize.apply(
-                   null,
-                   isSub ? [reward.paid_count, ' assinante', ' assinantes'] : [reward.paid_count, ' apoio', ' apoios'])
-              )
-            ),
+            m('.fontcolor-secondary.fontsize-smallest.fontweight-semibold',
+              h.pluralize.apply(
+                  null,
+                  isSub ? [reward.paid_count, ' assinante', ' assinantes'] : [reward.paid_count, ' apoio', ' apoios'])
+             ),
             reward.waiting_payment_count > 0 ? m('.maximum_contributions.in_time_to_confirm.clearfix', [
                 m('.pending.fontsize-smallest.fontcolor-secondary', h.pluralize(reward.waiting_payment_count, ' apoio em prazo de confirmação', ' apoios em prazo de confirmação.'))
             ]) : '',
