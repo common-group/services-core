b10406f90fc1d970f66647804e2c1371f626aae8
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-05-16 22:37:06 -0300

    https://www.pivotaltracker.com/story/show/165959174 - [QA SANDBOX] Modal de enviar mensagem na página de projeto não está carregando corretamente
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/src/c/owner-message-content.js
+++ legacy/src/c/owner-message-content.js
@@ -20,7 +20,7 @@ const ownerMessageContent = {
             // sets default values when user is not logged in
             user = h.getUser() || {
                 name: '',
-                email: ''
+                email: '',
             },
             from_name = prop(userVM.displayName(user)),
             from_email = prop(user.email),
@@ -31,19 +31,23 @@ const ownerMessageContent = {
                 return false;
             }
             submitDisabled(true);
-            content(content().split('\n').join('<br />'));
+            content(
+                content()
+                    .split('\n')
+                    .join('<br />')
+            );
 
             const loaderOpts = models.directMessage.postOptions({
                 from_name: from_name(),
                 from_email: from_email(),
                 user_id: h.getUser().user_id,
                 content: content(),
-                project_id: vnode.attrs().project_id,
-                to_user_id: userDetails().id,
+                project_id: vnode.attrs.project_id,
+                to_user_id: userDetails.id,
                 data: {
                     page_title: document.title,
-                    page_url: window.location.href
-                }
+                    page_url: window.location.href,
+                },
             });
 
             l = catarse.loaderWithToken(loaderOpts);
@@ -62,75 +66,96 @@ const ownerMessageContent = {
             from_name,
             from_email,
             content,
-            l
+            l,
         };
     },
-    view: function({state, attrs}) {
+    view: function({ state, attrs }) {
         const successMessage = m('.modal-dialog-content.u-text-center', [
                 m('.fa.fa-check-circle.fa-5x.text-success.u-marginbottom-40'),
-                m('p.fontsize-large', `Sua mensagem foi enviada com sucesso para ${state.userDetails().name}. Você vai receber uma cópia no seu email e pode seguir a conversa por lá!`)
+                m(
+                    'p.fontsize-large',
+                    `Sua mensagem foi enviada com sucesso para ${
+                        state.userDetails.name
+                    }. Você vai receber uma cópia no seu email e pode seguir a conversa por lá!`
+                ),
             ]),
             contactForm = [
                 m('.modal-dialog-content', [
                     m('.w-form', [
-                        m('form', {
-                            onsubmit: h.validate().submit([{
-                                prop: state.from_name,
-                                rule: 'text'
-                            }, {
-                                prop: state.from_email,
-                                rule: 'email'
-                            }, {
-                                prop: state.content,
-                                rule: 'text'
-                            }], state.sendMessage)
-                        }, [
-                            m('.w-row', [
-                                m('.w-col.w-col-6.w-sub-col', [
-                                    m('label.fontsize-smaller', 'Seu nome'),
-                                    m(`input.w-input.text-field[value='${state.from_name()}'][type='text'][required='required']`, {
-                                        onchange: m.withAttr('value', state.from_name),
-                                        class: h.validate().hasError(state.from_name) ? 'error' : ''
-                                    })
+                        m(
+                            'form',
+                            {
+                                onsubmit: h.validate().submit(
+                                    [
+                                        {
+                                            prop: state.from_name,
+                                            rule: 'text',
+                                        },
+                                        {
+                                            prop: state.from_email,
+                                            rule: 'email',
+                                        },
+                                        {
+                                            prop: state.content,
+                                            rule: 'text',
+                                        },
+                                    ],
+                                    state.sendMessage
+                                ),
+                            },
+                            [
+                                m('.w-row', [
+                                    m('.w-col.w-col-6.w-sub-col', [
+                                        m('label.fontsize-smaller', 'Seu nome'),
+                                        m(`input.w-input.text-field[value='${state.from_name()}'][type='text'][required='required']`, {
+                                            onchange: m.withAttr('value', state.from_name),
+                                            class: h.validate().hasError(state.from_name) ? 'error' : '',
+                                        }),
+                                    ]),
+                                    m('.w-col.w-col-6', [
+                                        m('label.fontsize-smaller', 'Seu email'),
+                                        m(`input.w-input.text-field[value='${state.from_email()}'][type='text'][required='required']`, {
+                                            onchange: m.withAttr('value', state.from_email),
+                                            class: h.validate().hasError(state.from_email) ? 'error' : '',
+                                        }),
+                                    ]),
                                 ]),
-                                m('.w-col.w-col-6', [
-                                    m('label.fontsize-smaller', 'Seu email'),
-                                    m(`input.w-input.text-field[value='${state.from_email()}'][type='text'][required='required']`, {
-                                        onchange: m.withAttr('value', state.from_email),
-                                        class: h.validate().hasError(state.from_email) ? 'error' : ''
-                                    })
-                                ])
-                            ]),
-                            m('label', 'Mensagem'),
-                            m('textarea.w-input.text-field.height-small[required=\'required\']', {
-                                onchange: m.withAttr('value', state.content),
-                                class: h.validate().hasError(state.content) ? 'error' : ''
-                            }),
-                            m('.u-marginbottom-10.fontsize-smallest.fontcolor-terciary', 'Você receberá uma cópia desta mensagem em seu email.'),
-                            m('.w-row', h.validationErrors().length ? _.map(h.validationErrors(), errors => m('span.fontsize-smallest.text-error', [
-                                m('span.fa.fa-exclamation-triangle'),
-                                ` ${errors.message}`,
-                                m('br')
-                            ])) : ''),
-                            m('.modal-dialog-nav-bottom',
-                                m('.w-row',
-                                    m('.w-col.w-col-6.w-col-push-3', !state.l() ? m('input.w-button.btn.btn-large[type="submit"][value="Enviar mensagem"]', {
-                                        disabled: state.submitDisabled()
-                                    }) : h.loader())
-                                )
-                            )
-                        ]),
+                                m('label', 'Mensagem'),
+                                m("textarea.w-input.text-field.height-small[required='required']", {
+                                    onchange: m.withAttr('value', state.content),
+                                    class: h.validate().hasError(state.content) ? 'error' : '',
+                                }),
+                                m('.u-marginbottom-10.fontsize-smallest.fontcolor-terciary', 'Você receberá uma cópia desta mensagem em seu email.'),
+                                m(
+                                    '.w-row',
+                                    h.validationErrors().length
+                                        ? _.map(h.validationErrors(), errors =>
+                                              m('span.fontsize-smallest.text-error', [m('span.fa.fa-exclamation-triangle'), ` ${errors.message}`, m('br')])
+                                          )
+                                        : ''
+                                ),
+                                m(
+                                    '.modal-dialog-nav-bottom',
+                                    m(
+                                        '.w-row',
+                                        m(
+                                            '.w-col.w-col-6.w-col-push-3',
+                                            !state.l()
+                                                ? m('input.w-button.btn.btn-large[type="submit"][value="Enviar mensagem"]', {
+                                                      disabled: state.submitDisabled(),
+                                                  })
+                                                : h.loader()
+                                        )
+                                    )
+                                ),
+                            ]
+                        ),
                     ]),
-                ])
+                ]),
             ];
 
-        return m('div', [
-            m('.modal-dialog-header',
-                m('.fontsize-large.u-text-center', 'Enviar mensagem')
-            ),
-            state.sendSuccess() ? successMessage : contactForm
-        ]);
-    }
+        return m('div', [m('.modal-dialog-header', m('.fontsize-large.u-text-center', 'Enviar mensagem')), state.sendSuccess() ? successMessage : contactForm]);
+    },
 };
 
 export default ownerMessageContent;
