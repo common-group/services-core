10a21f9588dbad329f3aa3369f71a21eb76b4e02
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-05-17 22:51:49 -0300

    https://www.pivotaltracker.com/story/show/165959166 - [QA SANDBOX] Comentários do FB não exibidos na aba comentarios
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/src/c/project-comments.js
+++ legacy/src/c/project-comments.js
@@ -3,24 +3,21 @@ import h from '../h';
 import projectReport from './project-report';
 
 const projectComments = {
-    oninit: function(vnode) {
-        const loadComments = (el, isInitialized) => (el, isInitialized) => {
-            if (isInitialized) { return; }
+    oninit: function (vnode) {
+        const loadComments = vnode => {
             h.fbParse();
         };
 
         vnode.state = { loadComments };
     },
-    view: function({state, attrs}) {
+    view: function ({ state, attrs }) {
         const project = attrs.project();
-        return m('.w-row',
-            [
-                m('.w-col.w-col-7',
-                m(`.fb-comments[data-href="http://www.catarse.me/${project.permalink}"][data-num-posts=50][data-width="610"]`, { config: state.loadComments() })
-              ),
-                m('.w-col.w-col-5', m(projectReport))
-            ]
-          );
+        return m('.w-row', [
+            m('.w-col.w-col-7',
+                m(`.fb-comments[data-href="http://www.catarse.me/${project.permalink}"][data-num-posts=50][data-width="610"]`, { oncreate: state.loadComments })
+            ),
+            m('.w-col.w-col-5', m(projectReport))
+        ]);
     }
 };
 
