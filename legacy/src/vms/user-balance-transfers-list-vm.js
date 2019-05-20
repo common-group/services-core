633697e453b0e9e482bad15479b4294a82277970
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-05-20 13:26:58 -0300

    https://www.pivotaltracker.com/story/show/165991836 - [QA SANDBOX] Botão de carregar mais na aba SALDO não carrega mais eventos no saldo
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/src/vms/user-balance-transfers-list-vm.js
+++ legacy/src/vms/user-balance-transfers-list-vm.js
@@ -5,27 +5,27 @@ import { catarse } from '../api';
 
 
 // Defined for UI (balance transfers history is shown in pages of 3 in a row)
-models.userBalanceTransfers.pageSize(3);
 
-const userBalanceTransfersPager = (() => {
+
+const userBalanceTransfersListVM = userIdParameters => {
+    
+    models.userBalanceTransfers.pageSize(3);
 
     const loader = catarse.paginationVM(models.userBalanceTransfers, 'requested_in.desc', { Prefer: 'count=exact' });
+    
+    loader
+        .firstPage(userIdParameters)
+        .then(() => {
+            m.redraw();
+        });
 
     return {
-        firstPage : loader.firstPage,
-        nextPage : () => {
-            loader.nextPage().then(_ => m.redraw());
-        },
+        nextPage : () => loader.nextPage().then(_ => m.redraw()),
         isLoading : loader.isLoading,
         isLastPage : loader.isLastPage,
         collection : loader.collection
     }
 
-})();
-
-const userBalanceTransfersVM = {
-    getWithPagination : userBalanceTransfersPager
 };
 
-
-export default userBalanceTransfersVM;
+export default userBalanceTransfersListVM;
