0c4e3c496e312b640baa1343bff6549d013b3540
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-05-29 13:56:27 -0300

    https://www.pivotaltracker.com/story/show/165960189 - [QA SANDBOX] Erros no fluxo de pagamento de projeto de assinatura
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/src/c/subscription-next-charge-date-method-info.js
+++ legacy/src/c/subscription-next-charge-date-method-info.js
@@ -1,5 +1,6 @@
 import m from 'mithril';
 import moment from 'moment';
+import h from '../h';
 
 const subscriptionNextChargeDateMethodInfo = {
     view: function({attrs}) {
@@ -9,16 +10,19 @@ const subscriptionNextChargeDateMethodInfo = {
             next_charge_at,
         } = attrs;
 
+        const hasPaymentMethodDetails = payment_method_details && payment_method_details.last_digits && payment_method_details.brand;
 
         if (payment_method === 'boleto') {
             return `${moment(next_charge_at).format('DD/MM/YYYY')} - Boleto`;
-        } else {
+        } else if (hasPaymentMethodDetails) {
             const {
                 last_digits,
                 brand
             } = payment_method_details;
 
             return `${moment(next_charge_at).format('DD/MM/YYYY')} - Cartão ${brand} final ${last_digits}`;
+        } else {
+            return h.loader();
         }
     }
 };
