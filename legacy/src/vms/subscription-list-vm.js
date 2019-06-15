a2b3b2164106b54fc6e7002e7e22440a59c91938
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-06-15 08:29:17 -0300

    auto reload fix
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/src/vms/subscription-list-vm.js
+++ legacy/src/vms/subscription-list-vm.js
@@ -1,5 +1,5 @@
-import m from 'mithril';
-import { commonPayment,  } from '../api';
+import h from '../h';
+import { commonPayment } from '../api';
 import models from '../models';
 
 export default commonPayment.paginationVM(models.userSubscription, 'id.desc', { Prefer: 'count=exact' });
@@ -8,22 +8,26 @@ export const getUserPrivateSubscriptionsListVM = userCommonId => {
     models.userSubscription.pageSize(9);
     const contextSubVM = commonPayment.filtersVM({
         user_id: 'eq',
-        status: 'in'
+        status: 'in',
     });
-    contextSubVM.user_id(userCommonId).status(['started', 'active', 'inactive', 'canceled', 'canceling', 'error']).order({
-        created_at: 'desc'
-    });
+    contextSubVM
+        .user_id(userCommonId)
+        .status(['started', 'active', 'inactive', 'canceled', 'canceling', 'error'])
+        .order({
+            created_at: 'desc',
+        });
 
     const subscriptions = commonPayment.paginationVM(models.userSubscription, 'created_at.desc', { Prefer: 'count=exact' });
-    
-    subscriptions
-        .firstPage(contextSubVM.parameters())
-        .then(() => m.redraw());
+
+    (async function() {
+        await subscriptions.firstPage(contextSubVM.parameters());
+        h.redraw();
+    })();
 
     return {
         isLoading: subscriptions.isLoading,
         collection: subscriptions.collection,
         isLastPage: subscriptions.isLastPage,
-        nextPage: () => subscriptions.nextPage().then(() => m.redraw())
-    }
-}
+        nextPage: () => subscriptions.nextPage().then(() => h.redraw()),
+    };
+};
