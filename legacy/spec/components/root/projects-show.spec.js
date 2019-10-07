6f599c84b3974a573936ae00085062576a533a0f
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-10-07 16:59:21 -0300

    set project.about to use original mithril trust [finishes #168986703]
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- legacy/spec/components/root/projects-show.spec.js
+++ legacy/spec/components/root/projects-show.spec.js
@@ -5,6 +5,7 @@ describe('ProjectsShow', () => {
   let $output, projectDetail;
 
   beforeAll(() => {
+    m.originalTrust = m.trust;
     window.location.hash = '';
     projectDetail = ProjectDetailsMockery()[0];
     let component = m(projectsShow, {project_id: 123, project_user_id: 1231});
