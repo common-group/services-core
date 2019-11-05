aabc92f67442d39faedc39755947eaa2f3a17ee5
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-11-05 09:22:36 -0300

    babel to use async/await and project reports enqueued
    Signed-off-by: Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>

--- services/catarse.js/legacy/src/root/projects-subscription-report-download.js
+++ services/catarse.js/legacy/src/root/projects-subscription-report-download.js
@@ -8,10 +8,14 @@ import {
 import projectsContributionReportVM from '../vms/projects-contribution-report-vm';
 import h from '../h';
 import models from '../models';
+import { projectSubscriptionReportDownloadEntry } from '../c/project-subscription-report-download-entry';
+import { listProjectReportExports, Report } from '../vms/project-report-exports-vm';
 
 const projectSubscriptionReportDownload = {
-    oninit: function(vnode) {
+    oninit: function (vnode) {
         const catarseVM = projectsContributionReportVM;
+        const reports = prop([]);
+        const loading = prop(true);
         const project = prop([{}]);
         catarseVM.project_id(vnode.attrs.project_id);
         const lProject = catarse.loaderWithToken(models.projectDetail.getPageOptions({
@@ -21,72 +25,75 @@ const projectSubscriptionReportDownload = {
         lProject.load().then((data) => {
             project(data);
         });
+
+        listProjectReportExports(vnode.attrs.project_id)
+            .then((r) => {
+                reports(r);
+                loading(false);
+                h.redraw();
+            })
+            .catch((e) => {
+                loading(false);
+                h.redraw();
+            });
+
         vnode.state = {
-            project
+            project,
+            loading,
+            reports
         };
     },
-    view: function({state, attrs}) {
-        return m('.section.project-metrics',
-            m('.w-container',
-                m('.w-row', [
-                    m(projectDashboardMenu, {
-                        project: prop(_.first(state.project()))
-                    }),
-                    m('.w-col.w-col-2'),
-                    m('.w-col.w-col-8',
-                        m('.card.u-radius.u-marginbottom-20.card-terciary', [
-                            m('.fontsize-small.fontweight-semibold.u-marginbottom-20', [
-                                m('span.fa.fa-download',
-                                    m.trust('&nbsp;')
-                                ),
-                                'Baixar relatórios'
+    view: function ({ state, attrs }) {
+
+        /** @type {Report[]} */
+        const reports = state.reports();
+        /** @type {boolean} */
+        const loading = state.loading();
+
+
+        return m('div', [
+            m('div.dashboard-header.u-text-center',
+                m('div.w-container',
+                    m('div.w-row', [
+                        m('div.w-col.w-col-2'),
+                        m('div.w-col.w-col-8',
+                            m('div.fontweight-semibold.fontsize-larger.lineheight-looser', 'Relatórios exportados')
+                        ),
+                        m('div.w-col.w-col-2')
+                    ])
+                )
+            ),
+            m('div.section.min-height-70',
+                m('div.w-container',
+                    m('div.w-row', [
+                        m('div.w-col.w-col-2'),
+                        m('div.w-col.w-col-8', [
+                            m('.card.u-radius.u-marginbottom-20.card-terciary', [
+                                m('div.fontsize-small.fontweight-semibold.u-marginbottom-20', [
+                                    m('span.fa.fa-download'),
+                                    ' Baixar relatórios'
+                                ]),
+                                m('div.card.u-radius', [
+                                    m('strong', 'Atenção: '),
+                                    'Ao realizar o download desses dados, você se compromete a armazená-los em local seguro e respeitar o direitos dos usuários conforme o que está previsto nos Termos de Uso e na política de privacidade do Catarse.'
+                                ])
                             ]),
-                            m('.card.u-radius.u-marginbottom-20', [
-                                m('span.fontweight-semibold',
-                                    m.trust('Atenção:')
-                                ),
-                                'Ao realizar o download desses dados, você se compromete a armazená-los em local seguro e respeitar o direitos dos usuários conforme o que está previsto nos Termos de Uso e na política de privacidade do Catarse.'
-                            ]),
-                            m('ul.w-list-unstyled', [
-                                m('li.fontsize-smaller.u-marginbottom-10',
-                                    m('div', [
-                                        'Base de assinantes ',
-                                        m.trust('&nbsp;'),
-                                        m(`a.alt-link[href='/projects/${attrs.project_id}/subscriptions_report_for_project_owners.csv']`,
-                                            'CSV'
-                                        ),
-                                        m.trust('&nbsp;'),
-                                        '\\',
-                                        m.trust('&nbsp;'),
-                                        m(`a.alt-link[href='/projects/${attrs.project_id}/subscriptions_report_for_project_owners.xls']`,
-                                            'XLS'
-                                        )
-                                    ])
-                                ),
-                                m('li.divider.u-marginbottom-10'),
-                                m('li.fontsize-smaller.u-marginbottom-10',
-                                    m('div', [
-                                        'Relatório de pagamentos confirmados',
-                                        m.trust('&nbsp;'),
-                                        m.trust('&nbsp;'),
-                                        m(`a.alt-link[href='/projects/${attrs.project_id}/subscriptions_monthly_report_for_project_owners.csv']`,
-                                            'CSV'
-                                        ),
-                                        m.trust('&nbsp;'),
-                                        '\\',
-                                        m.trust('&nbsp;'),
-                                        m(`a.alt-link[href='/projects/${attrs.project_id}/subscriptions_monthly_report_for_project_owners.xls']`,
-                                            'XLS'
-                                        )
-                                    ])
-                                )
-                            ])
-                        ])
-                    ),
-                    m('.w-col.w-col-2')
-                ])
+
+                            (
+                                loading ?
+                                    h.loader()
+                                :
+                                    reports.map(report => 
+                                        m(projectSubscriptionReportDownloadEntry, report)
+                                    )
+                            )
+                        ]), 
+                        m("div.w-col.w-col-2")
+                    ])
+                )
             )
-        );
+        ]);
+
     }
 };
 
