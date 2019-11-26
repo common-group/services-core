32e94e9ed3142391f2598cdcf804680e55bba02c
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2019-11-26 20:04:03 -0300

    ui display done

--- services/catarse.js/legacy/src/c/input-card.js
+++ services/catarse.js/legacy/src/c/input-card.js
@@ -2,15 +2,19 @@ import m from 'mithril';
 
 const inputCard = {
     view: function({attrs}) {
-        const cardClass = attrs.cardClass || '.w-row.u-marginbottom-30.card.card-terciary',
+        const cardClass = attrs.cardClass || '.u-marginbottom-30.card.card-terciary',
             onclick = attrs.onclick || Function.prototype;
 
         return m(cardClass, { onclick }, [
-            m('.w-col.w-col-5.w-sub-col', [
-                m('label.field-label.fontweight-semibold', attrs.label),
-                (attrs.label_hint ? m('label.hint.fontsize-smallest.fontcolor-secondary', attrs.label_hint) : '')
+            m('.w-row', [
+                m('.w-col.w-col-5.w-sub-col', [
+                    m('label.field-label.fontweight-semibold', attrs.label),
+                    (attrs.label_hint ? m('label.hint.fontsize-smallest.fontcolor-secondary', attrs.label_hint) : '')
+                ]),
+                m('.w-col.w-col-7.w-sub-col', attrs.children)
             ]),
-            m('.w-col.w-col-7.w-sub-col', attrs.children)
+
+            attrs.belowChildren
         ]);
     }
 };
