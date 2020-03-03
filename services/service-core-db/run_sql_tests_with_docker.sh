062fb90173f99314894609ab51e469f46012b0c3
Antonio Roberto Silva <forevertonny@gmail.com>
2020-03-03 20:27:46 +0000

    adjusts on run sql tests with docker to use pgpassword env

--- services/service-core-db/run_sql_tests_with_docker.sh
+++ services/service-core-db/run_sql_tests_with_docker.sh
@@ -18,20 +18,20 @@ echo 'building dockerfile.test'
 docker build -f Dockerfile.test -t inline_services_core_test .
 
 echo 'creating test database...'
-docker run -e POSTGRES_DB=test_db --name common_service_db_inline_test --rm -d postgres:9.6
+docker run -e POSTGRES_PASSWORD=example -e POSTGRES_DB=test_db --name common_service_db_inline_test --rm -d postgres:9.6
 
 echo 'waiting test_db up...'
-until docker run --rm --link common_service_db_inline_test:pg postgres:9.6 pg_isready -U postgres -h pg; do sleep 1; done
+until docker run -e PGPASSWORD=example --rm --link common_service_db_inline_test:pg postgres:9.6 pg_isready -U postgres -h pg; do sleep 1; done
 
 echo 'loading basic init.sql'
-docker run -i --rm --link common_service_db_inline_test:pg inline_services_core_test psql -h pg -U postgres test_db < init.sql
+docker run -e PGPASSWORD=example -i --rm --link common_service_db_inline_test:pg inline_services_core_test psql -h pg -U postgres test_db < init.sql
 
 echo 'migrating test database...'
-docker run -i --rm -e="DATABASE_URL=postgres://postgres@pg:5432/test_db"  --link common_service_db_inline_test:pg inline_services_core_test ./scripts/run_migrations.sh
+docker run -i --rm -e="DATABASE_URL=postgres://postgres:example@pg:5432/test_db"  --link common_service_db_inline_test:pg inline_services_core_test ./scripts/run_migrations.sh
 
 echo 'running tests...'
 docker create -v /specs --name sql_test_files_inline_common comum/deps /bin/true
 docker cp specs sql_test_files_inline_common:/
-docker run -t --rm --volumes-from sql_test_files_inline_common --link common_service_db_inline_test:pg comum/pgtap -h pg -u postgres -d test_db -t '/specs/sql-specs/*/*.sql'
+docker run -e PGPASSWORD=example -t --rm --volumes-from sql_test_files_inline_common --link common_service_db_inline_test:pg comum/pgtap -h pg -u postgres -d test_db -t '/specs/sql-specs/*/*.sql'
 
 echo 'removing test container...'
