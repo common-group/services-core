940091bfa99c0c978568bc1f986f3b2afb95a28b
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-03-23 16:14:33 -0300

    loading state for explore search input text

--- legacy/src/c/explore-search-filter-select.js
+++ legacy/src/c/explore-search-filter-select.js
@@ -16,13 +16,14 @@ export const ExploreSearchFilterSelect = {
 
         const onSearch = attrs.onSearch;
         const onSelect = attrs.onSelect;
+        const isLoading = attrs.isLoading;
         const itemToString = attrs.itemToString;
         const mobileLabel = attrs.mobileLabel;
         const hasItemSelected = attrs.selectedItem() !== null;
-        const selectedItem = hasItemSelected ? itemToString(attrs.selectedItem()) : attrs.noneSelected;
+        const noneSelected = attrs.noneSelected;
+        const selectedItem = hasItemSelected ? itemToString(attrs.selectedItem()) : noneSelected;
         const foundItems = attrs.foundItems() || [];
-        const openSearchControl = state.openSearchControl;
-        const openSearchControlDisplayStyle = openSearchControl() ? 'block' : 'none';
+        const openSearchControl = state.openSearchControl;        
         const onToggleSearchBox = (/** @type {Event} */ event) => {
             event.stopPropagation();
             openSearchControl.toggle();
@@ -51,35 +52,62 @@ export const ExploreSearchFilterSelect = {
                     }
                 })
             ]),
-            m('div.explore-filter-select.big.w-clearfix', { 'style': { 'display': openSearchControlDisplayStyle } }, [
-                m('a.modal-close.fa.fa-close.fa-lg.w-hidden-main.w-hidden-medium.w-inline-block[href="#"]', {
-                    onclick: onToggleSearchBox
-                }),
-                m('div.w-form', [
-                    m('form.position-relative', [
-                        m('a.btn-search.w-inline-block[href="#"]',
-                            m('img.header-lupa[src="https://uploads-ssl.webflow.com/57ba58b4846cc19e60acdd5b/57ba58b4846cc19e60acdda7_lupa.png"][alt=""]')
-                        ),
-                        m('input.text-field.positive.city-search.w-input[type="text"][autofocus][maxlength="256"][placeholder="Pesquise por cidade ou estado"]', {
-                            oninput: (/** @type {Event} */ event) => onSearch(event.target.value),
-                            onkeyup: (/** @type {Event} */ event) => onSearch(event.target.value),
+            (
+                openSearchControl() &&
+                (
+                    m('div.explore-filter-select.big.w-clearfix', { 'style': { 'display': 'block' } }, [
+                        m('a.modal-close.fa.fa-close.fa-lg.w-hidden-main.w-hidden-medium.w-inline-block[href="#"]', {
+                            onclick: onToggleSearchBox
                         }),
-                        m('div.table-outer.search-cities-pre-result', [
-                            foundItems.map(item => {
-                                return m('div.table-row.fontsize-smallest.fontcolor-secondary',
-                                    m('a.fontsize-smallest.link-hidden-light[href="#"]', {
-                                        onclick: (/** @type {Event} */ event) => {
-                                            event.preventDefault();
-                                            onSelect(item);
-                                            onToggleSearchBox(event);
-                                        }
-                                    }, itemToString(item))
-                                );
-                            })
+                        m('div.w-form', [
+                            m('form.position-relative', [
+                                m('a.btn-search.w-inline-block[href="#"]',
+                                    m('img.header-lupa[src="https://uploads-ssl.webflow.com/57ba58b4846cc19e60acdd5b/57ba58b4846cc19e60acdda7_lupa.png"][alt=""]')
+                                ),
+                                m('input.text-field.positive.city-search.w-input[type="text"][autofocus][maxlength="256"][placeholder="Pesquise por cidade ou estado"]', {
+                                    oninput: (/** @type {Event} */ event) => onSearch(event.target.value),
+                                    onkeyup: (/** @type {Event} */ event) => onSearch(event.target.value),
+                                }),
+                                m('div.table-outer.search-cities-pre-result', [
+                                    (
+                                        isLoading() ?
+                                            h.loader()
+                                        :   
+                                            (
+                                                foundItems.length === 0 ?
+                                                    (
+                                                        m('div.table-row.fontsize-smallest.fontcolor-secondary',
+                                                            m('a.fontsize-smallest.link-hidden-light[href="#"]', {
+                                                                onclick: (/** @type {Event} */ event) => {
+                                                                    event.preventDefault();
+                                                                    onSelect(null);
+                                                                    onToggleSearchBox(event);
+                                                                }
+                                                            }, noneSelected)
+                                                        )
+                                                    )
+                                                :
+                                                    (
+                                                        foundItems.map(item => {
+                                                            return m('div.table-row.fontsize-smallest.fontcolor-secondary',
+                                                                m('a.fontsize-smallest.link-hidden-light[href="#"]', {
+                                                                    onclick: (/** @type {Event} */ event) => {
+                                                                        event.preventDefault();
+                                                                        onSelect(item);
+                                                                        onToggleSearchBox(event);
+                                                                    }
+                                                                }, itemToString(item))
+                                                            );
+                                                        })
+                                                    )
+                                            )
+                                    )
+                                ])
+                            ]),
                         ])
-                    ]),
-                ])
-            ])
+                    ])
+                )
+            )
         ]);
     }
 }
