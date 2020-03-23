e2b7104f4df2da7d2084297b6490e0b12a97b1d2
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-03-23 11:20:15 -0300

    component explore filter select testing

--- legacy/src/c/explore-filter-select.js
+++ legacy/src/c/explore-filter-select.js
@@ -30,95 +30,146 @@ import _ from 'underscore';
   * @property {ExploreFilterState} state
   */
 
-const ColumnSplit = (values, start, finish, onSelect, isSelected) => _.map(values.slice(start, finish), item =>
-    m(`a.explore-filter-link[href="javascript:void(0);"]`, {
-        onclick: () => onSelect(item),
-        class: isSelected(item) ? 'selected' : ''
-    }, item.label)
-);
 
-export const ExploreFilterSelect = {
 
-    oninit(vnode) {
-        const showFilterSelect = h.RedrawToggleStream(false, true);
+export function ExploreFilterSelect(vnode) {
 
-        vnode.state = {
-            showFilterSelect,
-        };
-    },
+    return {
+        oninit(vnode) {
+            const showFilterSelect = h.RedrawToggleStream(false, true);
 
+            vnode.state = {
+                showFilterSelect,
+            };
+        },
+    
+        /**
+         * @param {ExploreFilterViewParams} viewParams
+         * @returns {m.Vnode}
+         */
+        view({state, attrs}) {
+    
+            const isSelected = attrs.isSelected;
+            const itemToString = attrs.itemToString;
+            const onSelect = attrs.onSelect;
+            const values = attrs.values;
+            const mobileLabel = attrs.mobileLabel;
+            const splitNumberColumns = attrs.splitNumberColumns || 1;
+            const showFilterSelect = state.showFilterSelect;
+            const onClickExploreFilter = (/** @type {Event} */ event) => {
+                showFilterSelect.toggle();
+                event.stopPropagation();
+            };
+            
+            return m('.explore-filter-wrapper', [
+                m('.explore-span-filter', {
+                    onclick: onClickExploreFilter
+                }, [
+                    m('div.explore-span-filter-name', [
+                        m('div.explore-mobile-label', mobileLabel),
+                        m('div.inline-block', itemToString()),
+                    ]),
+                    m('.inline-block.fa.fa-angle-down[aria-hidden="true"]', {
+                        onclick: onClickExploreFilter
+                    })
+                ]),
+                (
+                    showFilterSelect() && 
+                        (
+                            splitNumberColumns > 1 ?
+                                m(ExploreFilterSelectionColumns, {
+                                    isSelected,
+                                    onSelect,
+                                    values,
+                                    splitNumberColumns,
+                                    showFilterSelect,
+                                })
+                            :
+                                m(ExploreFilterSelectionSingleColumn, {
+                                    isSelected,
+                                    onSelect,
+                                    values,
+                                    showFilterSelect,
+                                })
+                        )
+                )
+            ]);
+        }
+    }
+};
+
+const ExploreFilterSelectionColumns = {
+        
     /**
      * @param {ExploreFilterViewParams} viewParams
      * @returns {m.Vnode}
      */
-    view({state, attrs}) {
-
+    view({attrs}) {
         const isSelected = attrs.isSelected;
-        const itemToString = attrs.itemToString;
         const onSelect = attrs.onSelect;
         const values = attrs.values;
-        const mobileLabel = attrs.mobileLabel;
         const splitNumberColumns = attrs.splitNumberColumns || 1;
-        const showFilterSelect = state.showFilterSelect;
-        
-        return m('.explore-filter-wrapper', [
-            m('.explore-span-filter', {
-                onclick: () => showFilterSelect.toggle()
-            }, [
-                m('div.explore-span-filter-name', [
-                    m('div.explore-mobile-label', mobileLabel),
-                    m('div.inline-block', itemToString()),
-                ]),
-                m('.inline-block.fa.fa-angle-down[aria-hidden="true"]', {
-                    onclick: (/** @type {Event} */ event) => {
-                        event.stopPropagation();
-                        showFilterSelect.toggle();
-                    }
+        const showFilterSelect = attrs.showFilterSelect;
+        const onSelectWithClose = (item) => {
+            showFilterSelect(false);
+            onSelect(item);                
+        };
+
+        return m('.explore-filter-select.big',
+            m('.explore-filer-select-row', [
+                _.range(0, splitNumberColumns).map(columnIndex => {
+                    return m('.explore-filter-select-col', [
+                        columnSplit(
+                            values, 
+                            Math.floor(values.length / splitNumberColumns) * columnIndex,
+                            Math.floor(values.length / splitNumberColumns) * (columnIndex + 1),
+                            onSelectWithClose,
+                            isSelected
+                        )
+                    ]);
+                }),
+                m('a.modal-close.fa.fa-close.fa-lg.w-hidden-main.w-hidden-medium.w-inline-block', {
+                    onclick: () => showFilterSelect(false)
                 })
-            ]),
-            (
-                showFilterSelect() && 
-                    (
-                        splitNumberColumns > 1 ?
-                            (
-                                m('.explore-filter-select.big',
-                                    m('.explore-filer-select-row', [
-                                        _.range(0, splitNumberColumns).map(columnIndex => {
-                                            return m('.explore-filter-select-col', [
-                                                ColumnSplit(
-                                                    values, 
-                                                    Math.floor(values.length / splitNumberColumns) * columnIndex,
-                                                    Math.floor(values.length / splitNumberColumns) * (columnIndex + 1),
-                                                    onSelect,
-                                                    isSelected
-                                                )
-                                            ]);
-                                        }),
-                                        m('a.modal-close.fa.fa-close.fa-lg.w-hidden-main.w-hidden-medium.w-inline-block', {
-                                            onclick: () => state.categoryToggle(false)
-                                        })
-                                    ])
-                                )
-                            )
-                        :
-                            (
-                                m('.explore-filter-select', [
-                                    values.map(item => {
-                                        return m(`a.explore-filter-link[href="javascript:void(0);"]`, {
-                                            onclick: () => {
-                                                showFilterSelect(false);
-                                                onSelect(item);
-                                            },
-                                            class: isSelected(item) ? 'selected' : ''
-                                        }, item.label);
-                                    }),
-                                    m('a.modal-close.fa.fa-close.fa-lg.w-hidden-main.w-hidden-medium.w-inline-block', {
-                                        onclick: () => showFilterSelect(false)
-                                    })
-                                ])
-                            )
-                    )
-            )
+            ])
+        );
+    }
+};
+
+const ExploreFilterSelectionSingleColumn = {
+    
+    /**
+     * @param {ExploreFilterViewParams} viewParams
+     * @returns {m.Vnode}
+     */
+    view({attrs}) {
+        const isSelected = attrs.isSelected;
+        const onSelect = attrs.onSelect;
+        const values = attrs.values;
+        const showFilterSelect = attrs.showFilterSelect;
+
+        return m('.explore-filter-select', [
+            values.map(item => {
+                return m(`a.explore-filter-link[href="javascript:void(0);"]`, {
+                    onclick: () => {
+                        showFilterSelect(false);
+                        onSelect(item);
+                    },
+                    class: isSelected(item) ? 'selected' : ''
+                }, item.label);
+            }),
+            m('a.modal-close.fa.fa-close.fa-lg.w-hidden-main.w-hidden-medium.w-inline-block', {
+                onclick: () => showFilterSelect(false)
+            })
         ]);
     }
 };
+
+function columnSplit(values, start, finish, onSelect, isSelected) {
+    return values.slice(start, finish).map(item => {
+        return m(`a.explore-filter-link[href="javascript:void(0);"]`, {
+            onclick: () => onSelect(item),
+            class: isSelected(item) ? 'selected' : ''
+        }, item.label);
+    });
+}
