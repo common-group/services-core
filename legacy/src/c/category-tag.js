681d09003c181b51ffaeca6f01b3947989b041d5
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-03-27 18:32:19 -0300

    project details links redirection to explore search

--- legacy/src/c/category-tag.js
+++ legacy/src/c/category-tag.js
@@ -4,14 +4,19 @@ import h from '../h';
 const categoryTag = {
     view: function({attrs}) {
         const project = attrs.project;
+        const categorySearchUrl = `/${window.I18n.locale}/explore?filter=all&category_id=${project().category_id}`;
 
-        return project ? m(`a.btn.btn-inline.btn-small.btn-transparent.link-hidden-light${attrs.isDark ? '.fontcolor-negative' : ''}[href="/${window.I18n.locale}/explore#by_category_id/${project().category_id}"]`, {
-            onclick: h.analytics.event({
-                cat: 'project_view',
-                act: 'project_category_link',
-                lbl: project().category_name,
-                project: project()
-            })
+        return project ? m(`a.btn.btn-inline.btn-small.btn-transparent.link-hidden-light${attrs.isDark ? '.fontcolor-negative' : ''}[href="${categorySearchUrl}"]`, {
+            onclick: (/** @type {Event} */ event) => {
+                h.analytics.event({
+                    cat: 'project_view',
+                    act: 'project_category_link',
+                    lbl: project().category_name,
+                    project: project()
+                })(event);
+                event.preventDefault();
+                m.route.set(categorySearchUrl);
+            }
         }, [
             m('span.fa.fa-tag'), ' ',
             project().category_name
