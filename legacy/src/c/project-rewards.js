9e8d9bb4d8d438cff544880525629de66768027a
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-05-04 14:27:12 -0300

    format project rewards

--- legacy/src/c/project-rewards.js
+++ legacy/src/c/project-rewards.js
@@ -7,14 +7,22 @@ import projectGoalsBox from './project-goals-box';
 
 const projectRewards = {
     view: function({attrs}) {
-        return m('.w-col.w-col-12', [projectVM.isSubscription(attrs.project) ? attrs.subscriptionData() ? m(
-
-            projectGoalsBox,
-            { goalDetails: attrs.goalDetails, subscriptionData: attrs.subscriptionData }
-        ) : h.loader() : '', m(projectRewardList, _.extend({}, {
-            rewardDetails: attrs.rewardDetails,
-            hasSubscription: attrs.hasSubscription
-        }, attrs.c_opts))]);
+        return m('.w-col.w-col-12', [
+            projectVM.isSubscription(attrs.project) ? 
+                attrs.subscriptionData() ? 
+                    m(projectGoalsBox, { 
+                        goalDetails: attrs.goalDetails, 
+                        subscriptionData: attrs.subscriptionData 
+                    })
+                    :
+                    h.loader()
+                :
+                '', 
+            m(projectRewardList, _.extend({}, {
+                rewardDetails: attrs.rewardDetails,
+                hasSubscription: attrs.hasSubscription
+            }, attrs.c_opts))
+        ]);
     }
 };
 
