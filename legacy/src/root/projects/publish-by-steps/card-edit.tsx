ab2c1cb318a3e9dd83ecfc6c3b753f8997e258a3
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-06-02 17:21:22 -0300

    publish by steps card edit

---
+++ legacy/src/root/projects/publish-by-steps/card-edit.tsx
@@ -0,0 +1,145 @@
+import m, { VnodeDOM } from 'mithril'
+import { Observable } from 'rxjs'
+import { pluck } from 'rxjs/operators'
+import { ProjectCardSimple } from '../../../c/projects/publish-by-steps/project-card-simple'
+import { ProjectDetails } from '../../../@types/project-details'
+import { Wcol } from '../../../c/std/Wcol'
+
+export type CardEditAttrs = {
+    project: ProjectDetails
+    isSaving: boolean
+    onSave(): void
+}
+
+export type CardEditState = {
+    selectedImageFile: File
+    inputFileElement: HTMLInputElement
+}
+
+export class CardEdit implements m.Component {
+    oninit({state} : m.Vnode<CardEditAttrs, CardEditState>) {
+        state.selectedImageFile = null
+        state.inputFileElement = null
+    }
+
+    view({ attrs, state } : m.Vnode<CardEditAttrs, CardEditState>) {
+        
+        const project = attrs.project
+
+        return (
+            <div class="section">
+                <div class="w-container">
+                    <div class="w-row">
+                        <div class="w-col w-col-8">
+                            <div class="card medium card-terciary u-radius w-form">
+                                <form id="email-form-4" name="email-form-4" data-name="Email Form 4">
+                                    <div class="title-dashboard">
+                                        <span class="fontsize-smallest"></span>
+                                        Escolha a imagem do seu projeto
+                                    </div>
+                                    <div class="u-marginbottom-40 w-row">
+                                        <div class="_w-sub-col w-col w-col-5">
+                                            <label for="name" class="field-label fontweight-semibold">
+                                                Imagem do projeto
+                                            </label>
+                                            <label for="name" class="field-label fontsize-smallest fontcolor-secondary">
+                                                Tamanho recomendado 720x400px
+                                            </label>
+                                        </div>
+                                        <div class="_w-sub-col w-col w-col-4">
+                                            <input 
+                                                oncreate={(vnode : VnodeDOM) => state.inputFileElement = vnode.dom as HTMLInputElement}
+                                                oninput={(event : Event) => {
+                                                    if (state.inputFileElement.files.length > 0) {
+                                                        if (project.large_image && project.large_image.indexOf('blob') >= 0) {
+                                                            URL.revokeObjectURL(project.large_image)
+                                                        }
+
+                                                        state.selectedImageFile = state.inputFileElement.files[0]
+                                                        project.large_image = URL.createObjectURL(state.selectedImageFile)
+                                                    }
+                                                }}
+                                                type="file" accept="image/*" style="display: none;"></input>
+                                            <a 
+                                                onclick={(event : Event) => {
+                                                    event.preventDefault()
+                                                    if (state.inputFileElement) {
+                                                        state.inputFileElement.click()
+                                                    }
+                                                }}
+                                                href="#" class="btn btn-small btn-dark">
+                                                Escolher arquivo
+                                            </a>
+                                        </div>
+                                        <div class="w-col w-col-3">
+                                            <div class="fontsize-smallest fontcolor-secondary">
+                                                {
+                                                    state.selectedImageFile !== null ?
+                                                        state.selectedImageFile.name
+                                                        :
+                                                        'Nenhum arquivo escolhido'
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="u-marginbottom-40 w-row">
+                                        <div class="_w-sub-col w-col w-col-5">
+                                            <label for="name" class="field-label fontweight-semibold">
+                                                Frase de efeito
+                                            </label>
+                                            <label for="name" class="field-label fontsize-smallest fontcolor-secondary">
+                                                Máximo de 100 caracteres
+                                            </label>
+                                        </div>
+                                        <div class="w-col w-col-7">
+                                            <textarea
+                                                maxlength="100" 
+                                                placeholder="Um resuminho do seu projeto em até 100 caracteres"
+                                                value={project.headline}
+                                                oninput={(event : Event) => project.headline = event.target.value}
+                                                class="text-field positive w-input">
+                                            </textarea>
+                                        </div>
+                                    </div>
+                                    <div class="u-marginbottom-40 w-row">
+                                        <div class="_w-sub-col w-col w-col-5">
+                                            <label for="name" class="field-label fontweight-semibold">
+                                                Vídeo do youtube (opcional)
+                                            </label>
+                                            <label for="name" class="field-label fontsize-smallest fontcolor-secondary">
+                                                Vídeo que é exibido na página principal da campanha
+                                            </label>
+                                        </div>
+                                        <div class="w-col w-col-7">
+                                            <input 
+                                                oninput={(event : Event) => project.video_url = event.target.value}
+                                                type="text" class="text-field positive w-input" maxlength="3000" name="field-12" data-name="Field 12" placeholder="www.youtube.com/seuvideo" id="field-12" required="" />
+                                        </div>
+                                    </div>
+                                    <div class="u-margintop-40 w-row">
+                                        <div class="w-col w-col-3"></div>
+                                        <div class="w-col w-col-6">
+                                            <a href="/dashboard/solidaria/description" class="btn btn-large">
+                                                Próximo &gt;
+                                            </a>
+                                        </div>
+                                        <div class="w-col w-col-3"></div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="w-col w-col-4">
+                            <div class="u-margintop-40">
+                                <ProjectCardSimple 
+                                    name={project.name} 
+                                    headline={project.headline}
+                                    image={project.large_image}
+                                    />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
--- legacy/src/root/projects/publish-by-steps/index.tsx
+++ legacy/src/root/projects/publish-by-steps/index.tsx
@@ -1,17 +1,50 @@
 import m from 'mithril';
+import h from '../../../h';
+import { CardEdit } from './card-edit';
+import { ProjectPublishByStepsVM } from '../../../vms/project-publish-by-steps-vm';
+import { ProjectDetails } from '../../../@types/project-details';
 
-// const hash = window.location.hash;
-// switch(hash) {
-//     case '#card': {
+// '/projects/:id/publish-by-steps/card': wrap({})
+// '/projects/:id/publish/description': 
+// '/projects/:id/publish/ask-about-reward': 
+// '/projects/:id/publish/rewards': 
+// '/projects/:id/publish/user': 
+// '/projects/:id/publish/to-do': 
+// '/projects/:id/publish/share': 
+// '/projects/:id/publish/share-reminder': 
 
-//     }
-// }
+type ProjectsPublishByStepsAttrs = {
+    project_id: number;
+}
 
-// return {
-// }
-const ProjectsPublishBySteps = {
-    view({}) {
-        return m('div', 'NOT IMPLEMENTED YET');
+type ProjectsPublishByStepsState = {
+    projectPublishByStepsVM: ProjectPublishByStepsVM;
+}
+
+type ProjectsPublishByStepsVnode = m.Vnode<ProjectsPublishByStepsAttrs, ProjectsPublishByStepsState>;
+
+class ProjectsPublishBySteps implements m.Component<ProjectsPublishByStepsAttrs, ProjectsPublishByStepsState> {
+    
+    oninit({attrs, state} : ProjectsPublishByStepsVnode) {
+        state.projectPublishByStepsVM = new ProjectPublishByStepsVM(attrs.project_id);
+    }
+
+    view({ state } : ProjectsPublishByStepsVnode) {
+
+        if (state.projectPublishByStepsVM.isLoadingProject) {
+            return h.loader();
+        } else {
+            const hash = window.location.hash;
+            switch(hash) {
+                case '#card': {
+                    return <CardEdit project={state.projectPublishByStepsVM.project} />
+                }
+    
+                default: {
+                    return <CardEdit project={state.projectPublishByStepsVM.project} />
+                }
+            }
+        }
     }
 };
 
