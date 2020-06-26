3b8974db84115ece8a923c6cc76e872ec10210b9
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-06-26 20:22:01 -0300

    logic for featured project cards medium and big removed

--- legacy/src/c/explore-projects-list.js
+++ legacy/src/c/explore-projects-list.js
@@ -9,18 +9,14 @@ export const ExploreProjectsList = {
         const projects = attrs.projects;
         const isSearch = attrs.isSearch;
         const filterKeyName = attrs.filterKeyName;
-        const checkForMinScoredProjects = collection => _.size(_.filter(collection, p => p.score >= 1)) >= 3;
         const isContributedByFriendsFilter = (filterKeyName === 'contributed_by_friends');
-        const projectsCount = attrs.projects.collection().length;
 
         return m('.w-section.section', [
             m('.w-container', [
                 m('.w-row', [
-                    m('.w-row', _.map(projects.collection(), (project, idx) => {
+                    m('.w-row', _.map(projects.collection(), project => {
                         let cardType = 'small';
                         let ref = 'ctrse_explore';
-
-                        let widowProjects = [];
 
                         if (isSearch) {
                             ref = 'ctrse_explore_pgsearch';
@@ -28,23 +24,7 @@ export const ExploreProjectsList = {
                             ref = 'ctrse_explore_friends';
                         } else if (filterKeyName === 'all') {
                             if (project.score >= 1) {
-                                if (idx === 0) {
-                                    cardType = 'big';
-                                    ref = 'ctrse_explore_featured_big';
-                                    widowProjects = [projectsCount - 1, projectsCount - 2];
-                                } else if (idx === 1 || idx === 2) {
-                                    if (checkForMinScoredProjects(projects.collection())) {
-                                        cardType = 'medium';
-                                        ref = 'ctrse_explore_featured_medium';
-                                        widowProjects = [];
-                                    } else {
-                                        cardType = 'big';
-                                        ref = 'ctrse_explore_featured_big';
-                                        widowProjects = [projectsCount - 1];
-                                    }
-                                } else {
-                                    ref = 'ctrse_explore_featured';
-                                }
+                                ref = 'ctrse_explore_featured';
                             }
                         } else if (filterKeyName === 'saved_projects') {
                             ref = 'ctrse_explore_saved_project';
@@ -52,7 +32,7 @@ export const ExploreProjectsList = {
                             ref = 'ctrse_explore_projects_we_love';
                         }
 
-                        return (_.indexOf(widowProjects, idx) > -1 && !projects.isLastPage()) ? '' : m(projectCard, {
+                        return m(projectCard, {
                             project,
                             ref,
                             type: cardType,
