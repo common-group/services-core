170c36ae81105d6a4592a38d30b12de94527b0ef
Antonio Roberto Silva <forevertonny@gmail.com>
2020-07-09 03:58:58 +0000

    use -q when subtree pull/push

--- .circleci/subtrees-push.sh
+++ .circleci/subtrees-push.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 #ssh-keyscan -t rsa ${SUBTREE_PUSH_DOMAIN} >> "${HOME}/.ssh/known_hosts"
@@ -16,48 +16,39 @@ git remote add hook-service-api $HOOK_SERVICE_SUBTREE_REMOTE
 git remote add notification-service-api $NOTIFICATION_SERVICE_SUBTREE_REMOTE
 git remote add payment-service-api $PAYMENT_SERVICE_SUBTREE_REMOTE
 
-trap tail_log EXIT
-tail_log()
-{
-    tail  ~/push_subtree.log
-}
 
 echo 'pushing catarse.js...'
-git subtree pull --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH &> ~/push_subtree.log
-tail  ~/push_subtree.log
+git subtree pull -q --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH
+git subtree push -q --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH
 
 echo 'pushing catarse...'
-git subtree pull --prefix=services/catarse catarse $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/catarse catarse $SUBTREE_BRANCH &> ~/push_subtree.log
+git subtree pull -q --prefix=services/catarse catarse $SUBTREE_BRANCH
+git subtree push -q --prefix=services/catarse catarse $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing service-core-db...'
-git subtree pull --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH &> ~/push_subtree.log
+git subtree pull -q --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH
+git subtree push -q --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing common-api...'
-git subtree pull --prefix=services/common-api common-api $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/common-api common-api $SUBTREE_BRANCH &> ~/push_subtree.log
+git subtree pull -q --prefix=services/common-api common-api $SUBTREE_BRANCH
+git subtree push -q --prefix=services/common-api common-api $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing proxy...'
-git subtree pull --prefix=services/proxy proxy $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/proxy proxy $SUBTREE_BRANCH &> ~/push_subtree.log
+git subtree pull -q --prefix=services/proxy proxy $SUBTREE_BRANCH
+git subtree push -q --prefix=services/proxy proxy $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing hook-service-api...'
-git subtree pull --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
-tail  ~/push_subtree.log
+git subtree pull -q --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH
+git subtree push -q --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH
 
 echo 'pushing notification-service-api...'
-git subtree pull --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
-tail  ~/push_subtree.log
+git subtree pull -q --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH
+git subtree push -q --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH
 
 echo 'pushing payment-service-api...'
-git subtree pull --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
-git subtree push --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
-tail  ~/push_subtree.log
+git subtree pull -q --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH
+git subtree push -q --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH
