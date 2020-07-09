298c0a347c2db78d0701794f81044683c80fd2eb
Antonio Roberto Silva <forevertonny@gmail.com>
2020-07-09 04:28:09 +0000

    rollback with subtree push here

--- .circleci/subtrees-push.sh
+++ .circleci/subtrees-push.sh
@@ -18,37 +18,37 @@ git remote add payment-service-api $PAYMENT_SERVICE_SUBTREE_REMOTE
 
 
 echo 'pushing catarse.js...'
-git subtree pull -q --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH
 git subtree push -q --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH
 
 echo 'pushing catarse...'
-git subtree pull -q --prefix=services/catarse catarse $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/catarse catarse $SUBTREE_BRANCH
 git subtree push -q --prefix=services/catarse catarse $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing service-core-db...'
-git subtree pull -q --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH
 git subtree push -q --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing common-api...'
-git subtree pull -q --prefix=services/common-api common-api $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/common-api common-api $SUBTREE_BRANCH
 git subtree push -q --prefix=services/common-api common-api $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing proxy...'
-git subtree pull -q --prefix=services/proxy proxy $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/proxy proxy $SUBTREE_BRANCH
 git subtree push -q --prefix=services/proxy proxy $SUBTREE_BRANCH
 tail  ~/push_subtree.log
 
 echo 'pushing hook-service-api...'
-git subtree pull -q --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH
 git subtree push -q --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH
 
 echo 'pushing notification-service-api...'
-git subtree pull -q --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH
 git subtree push -q --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH
 
 echo 'pushing payment-service-api...'
-git subtree pull -q --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH
+#git subtree pull -q --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH
 git subtree push -q --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH
