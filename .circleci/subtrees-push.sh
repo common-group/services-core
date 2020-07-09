79737b71f338e178aa9707c14f64e677e1b4ffc5
Antonio Roberto Silva <forevertonny@gmail.com>
2020-07-09 02:52:05 +0000

    set git global config for catarse circleci and pull subtrees before push

--- .circleci/subtrees-push.sh
+++ .circleci/subtrees-push.sh
@@ -20,33 +20,41 @@ tail_log()
 }
 
 echo 'pushing catarse.js...'
+git subtree pull --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/catarse.js catarse.js $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
 
 echo 'pushing catarse...'
+git subtree pull --prefix=services/catarse catarse $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/catarse catarse $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
 
 echo 'pushing service-core-db...'
+git subtree pull --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/service-core-db service-core-db $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
 
 echo 'pushing common-api...'
+git subtree pull --prefix=services/common-api common-api $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/common-api common-api $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
 
 echo 'pushing proxy...'
+git subtree pull --prefix=services/proxy proxy $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/proxy proxy $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
 
 echo 'pushing hook-service-api...'
+git subtree pull --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/hook-service-api hook-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
 
 echo 'pushing notification-service-api...'
+git subtree pull --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/notification-service-api notification-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
 
 echo 'pushing payment-service-api...'
+git subtree pull --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
 git subtree push --prefix=services/payment-service-api payment-service-api $SUBTREE_BRANCH &> ~/push_subtree.log
 tail  ~/push_subtree.log
