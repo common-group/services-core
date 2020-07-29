21d20b76a17afac5b75e65f9747b3ea0235728dd
Gilberto Ribeiro <gilbertoribeiropazdarosa@gmail.com>
2020-07-29 17:56:54 -0300

    ✨ validação de conta bancária

--- services/catarse.js/webpack.config.js
+++ services/catarse.js/webpack.config.js
@@ -74,7 +74,7 @@ module.exports = {
     plugins: isProd ? [new UglifyJsPlugin({
         sourceMap: true,
         uglifyOptions: {
-            compress: true
+            compress: false
         }
     })] : [],
 };
