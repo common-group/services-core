9a44938e809a502407f80e092e26ae3f4dbbb2b5
Letícia Fernandes <leticiafs1994@hotmail.com>
2020-10-22 09:31:33 -0300

    🐞 Corrige utm de compartilhamento thank_you mobile

--- services/catarse.js/legacy/src/c/project-share-box.js
+++ services/catarse.js/legacy/src/c/project-share-box.js
@@ -9,6 +9,7 @@ const projectShareBox = {
         };
     },
     view: function({state, attrs}) {
+        const utm = attrs.utm || 'project_share';
         return m('.pop-share.fontcolor-primary', {
             style: 'display: block;'
         }, [
@@ -19,7 +20,7 @@ const projectShareBox = {
                 m('.fontsize-small.fontweight-semibold.u-marginbottom-30', 'Compartilhe este projeto')
             ]),
             m('.w-widget.w-widget-twitter.w-hidden-small.w-hidden-tiny.share-block', [
-                m(`iframe[allowtransparency="true"][width="120px"][height="22px"][frameborder="0"][scrolling="no"][src="//platform.twitter.com/widgets/tweet_button.8d007ddfc184e6776be76fe9e5e52d69.en.html#_=1442425984936&count=horizontal&dnt=false&id=twitter-widget-1&lang=en&original_referer=https%3A%2F%2Fwww.catarse.me%2Fpt%2F${attrs.project().permalink}&size=m&text=Confira%20o%20projeto%20${attrs.project().name}%20no%20%40catarse&type=share&url=https%3A%2F%2Fwww.catarse.me%2Fpt%2F${attrs.project().permalink}%3Fref%3Dtwitter%26utm_source%3Dtwitter.com%26utm_medium%3Dsocial%26utm_campaign%3Dproject_share&via=catarse"]`)
+                m(`iframe[allowtransparency="true"][width="120px"][height="22px"][frameborder="0"][scrolling="no"][src="//platform.twitter.com/widgets/tweet_button.8d007ddfc184e6776be76fe9e5e52d69.en.html#_=1442425984936&count=horizontal&dnt=false&id=twitter-widget-1&lang=en&original_referer=https%3A%2F%2Fwww.catarse.me%2Fpt%2F${attrs.project().permalink}&size=m&text=Confira%20o%20projeto%20${attrs.project().name}%20no%20%40catarse&type=share&url=https%3A%2F%2Fwww.catarse.me%2Fpt%2F${attrs.project().permalink}%3Fref%3Dtwitter%26utm_source%3Dtwitter.com%26utm_medium%3Dsocial%26utm_campaign%3D${utm}&via=catarse"]`)
             ]),
             m('a.w-hidden-small.widget-embed.w-hidden-tiny.fontsize-small.link-hidden.fontcolor-secondary[href="javascript:void(0);"]', {
                 onclick: state.displayEmbed.toggle
@@ -34,13 +35,13 @@ const projectShareBox = {
             ]) : ''),
             attrs.project().permalink ? m(facebookButton, {
                 mobile: true,
-                url: `https://www.catarse.me/${attrs.project().permalink}?ref=facebook&utm_source=facebook.com&utm_medium=social&utm_campaign=project_share`
+                url: `https://www.catarse.me/${attrs.project().permalink}?ref=facebook&utm_source=facebook.com&utm_medium=social&utm_campaign=${utm}`
             }) : '',
-            m(`a.w-hidden-main.w-hidden-medium.btn.btn-medium.btn-tweet.u-marginbottom-20[href="https://twitter.com/intent/tweet?text=Acabei%20de%20apoiar%20o%20projeto%20${encodeURIComponent(attrs.project().name)}%20https://www.catarse.me/${attrs.project().permalink}%3Fref%3Dtwitter%26utm_source%3Dtwitter.com%26utm_medium%3Dsocial%26utm_campaign%3Dproject_share"][target="_blank"]`, [
+            m(`a.w-hidden-main.w-hidden-medium.btn.btn-medium.btn-tweet.u-marginbottom-20[href="https://twitter.com/intent/tweet?text=Acabei%20de%20apoiar%20o%20projeto%20${encodeURIComponent(attrs.project().name)}%20https://www.catarse.me/${attrs.project().permalink}%3Fref%3Dtwitter%26utm_source%3Dtwitter.com%26utm_medium%3Dsocial%26utm_campaign%3D${utm}"][target="_blank"]`, [
                 m('span.fa.fa-twitter'), ' Tweet'
             ]),
             m('a.w-hidden-main.w-hidden-medium.btn.btn-medium[data-action="share/whatsapp/share"]', {
-                href: `whatsapp://send?text=${encodeURIComponent(`https://www.catarse.me/${attrs.project().permalink}/?ref=whatsapp&utm_source=whatsapp&utm_medium=social&utm_campaign=project_share`)}`
+                href: `whatsapp://send?text=${encodeURIComponent(`https://www.catarse.me/${attrs.project().permalink}/?ref=whatsapp&utm_source=whatsapp&utm_medium=social&utm_campaign=${utm}`)}`
             }, [m('span.fa.fa-whatsapp'), ' Whatsapp'])
         ]);
     }
