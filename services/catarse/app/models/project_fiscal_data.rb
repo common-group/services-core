ee9f781e491a8c3cbeef04d65596ac6efd16cdb5
stephannv <3025661+stephannv@users.noreply.github.com>
2020-10-24 16:35:24 -0300

    💎💚🔧 Atualiza Rails para 6.1.0.rc2 e reorganiza parte do código

--- services/catarse/app/models/project_fiscal_data.rb
+++ services/catarse/app/models/project_fiscal_data.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-class ProjectFiscalData < ActiveRecord::Base
+class ProjectFiscalData < ApplicationRecord
   self.table_name = 'public.project_fiscal_data_tbl'
+
   belongs_to :project
   belongs_to :user
 
@@ -9,6 +10,3 @@ class ProjectFiscalData < ActiveRecord::Base
     self
   end
 end
-
-#dont know why self.table_name didn't work
-ProjectFiscalData.table_name = 'public.project_fiscal_data_tbl'
