415d80f4be778bdef17baa6773e45f4d44408221
stephannv <3025661+stephannv@users.noreply.github.com>
2020-11-20 09:55:28 -0300

    🐞 Melhora mensagem de retenção de IRRF

--- services/catarse.js/legacy/src/root/users/edit/#balance/index.tsx
+++ services/catarse.js/legacy/src/root/users/edit/#balance/index.tsx
@@ -2,8 +2,9 @@ import { withHooks } from 'mithril-hooks'
 import { UserBalanceAmount, UserBalanceAmountProps } from './user-balance-amount/user-balance-amount'
 import { UserBalanceTransactionsWithServices } from './user-balance-transactions'
 import { UserBalanceWithdrawHistoryWithServices } from './user-balance-withdraw-history'
-import { UserDetails } from '../../../../entities'
+import { UserDetails } from '@/entities'
 import { UserBalanceAmountWithServices } from './user-balance-amount'
+import { I18nText } from '@/shared/components/i18n-text'
 
 export type UserBalanceProps = {
     user: UserDetails
@@ -14,6 +15,7 @@ export const UserBalance = withHooks<UserBalanceProps>(_UserBalance)
 function _UserBalance({user} : UserBalanceProps) {
     return (
         <div id='balance-area'>
+            <IrrfRetentionMessage user={user}/>
             <UserBalanceAmountWithServices user={user} />
             <UserBalanceWithdrawHistoryWithServices user={user} />
             <div class='divider'></div>
@@ -22,4 +24,18 @@ function _UserBalance({user} : UserBalanceProps) {
             <div class='w-section section card-terciary before-footer'></div>
         </div>
     )
+}
+
+const IrrfRetentionMessage = withHooks<UserBalanceProps>(_IrrfRetentionMessage)
+
+function _IrrfRetentionMessage({user}: UserBalanceProps) {
+    if (user.account_type === 'pj') {
+        return (
+            <div class="w-container">
+                <div class="card card-message u-radius fontsize-small">
+                    <I18nText scope="users.balance.withdraw_irrf_message_html" trust={true}/>
+                </div>
+            </div>
+        )
+    }
 }
