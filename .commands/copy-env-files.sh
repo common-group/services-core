5530c0ae6351632b5fa980dd47b94fd25058dc2e
Giulliano Bueno <giubueno@gmail.com>
2021-02-02 17:19:50 +0100

    Add .sh to copy environment variables needed to run the project (newcomers).

---
+++ .commands/copy-env-files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+cp .analytics_service_api.env.sample .analytics_service_api.env
+cp .catarse_api.env.sample .catarse_api.env
+cp .catarse_moment_service_api.env.sample .catarse_moment_service_api.env
+cp .common_api.env.sample .common_api.env
+cp .common_proxy.env.sample .common_proxy.env
+cp .community_service_api.env.sample .community_service_api.env
+cp .hook_service_api.env.sample .hook_service_api.env
+cp .notification_dispatcher.env.sample .notification_dispatcher.env
+cp .notification_service_api.env.sample .notification_service_api.env
+cp .payment_service_api.env.sample .payment_service_api.env
+cp .payment_stream_processor.env.sample .payment_stream_processor.env
+cp .platform_service_api.env.sample .platform_service_api.env
+cp .project_service_api.env.sample .project_service_api.env
+cp .read_api.env.sample .read_api.env
+cp .recommender_service_api.env.sample .recommender_service_api.env
+cp .service_core_db.env.sample .service_core_db.env
+cp .service_core_migrator.env.sample .service_core_migrator.env
+cp .subscriptions_scheduler.env.sample .subscriptions_scheduler.env
