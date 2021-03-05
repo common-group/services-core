qyl2021/ruby-catarse

💎 Atualiza dependências fev/2021

spec/support/carrierwave.rb
@@ -1,0 +1,11 @@
+if Rails.env.test?
+  module CarrierWave
+    module Downloader
+      class Base
+        def skip_ssrf_protection?(uri)
+          true # SSRF protection isn't necessary because we avoid external requests with webmock
+        end
+      end
+    end
+  end
+end

common-group/services-core

services/catarse/spec/support/carrierwave.rb
@@ -1,0 +1,11 @@
+if Rails.env.test?
+  module CarrierWave
+    module Downloader
+      class Base
+        def skip_ssrf_protection?(_uri)
+          true # SSRF protection isn't necessary because we avoid external requests with webmock
+        end
+      end
+    end
+  end
+end