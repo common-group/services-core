3bc817f45920d93c0f5e40454171a86a81efa06f
Gabriel Rocha <37886504+gabrielras@users.noreply.github.com>
2021-05-04 15:00:19 -0200

    ✨ Implementa rota de valores parcelados (#850)

---
+++ services/catarse/app/api/catarse/v2/billing/installment_simulations_api.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Catarse
+  module V2
+    module Billing
+      class InstallmentSimulationsAPI < Grape::API
+        params do
+          requires :total_amount_cents, type: Integer
+        end
+
+        get '/installment_simulations' do
+          total_amount_cents = params['total_amount_cents']
+          result = ::Billing::InstallmentSimulator.calculate(total_amount_cents)
+
+          result
+        end
+      end
+    end
+  end
+end
