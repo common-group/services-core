c7d4826e310f50833bbf33ec8299d6487b63e3c9
Gabriel Rocha <37886504+gabrielras@users.noreply.github.com>
2021-05-05 15:25:03 -0200

    ✨ Bloqueia valores menores que o permitido na simulação de parcelamento. (#851)

--- services/catarse/spec/api/catarse/v2/billing/installment_simulations_api_spec.rb
+++ services/catarse/spec/api/catarse/v2/billing/installment_simulations_api_spec.rb
@@ -11,13 +11,26 @@ RSpec.describe Catarse::V2::Billing::InstallmentSimulationsAPI, type: :api do
     before do
       allow(CatarseSettings).to receive(:get_without_cache).with(:pagarme_max_installments).and_return(6)
       allow(CatarseSettings).to receive(:get_without_cache).with(:pagarme_interest_rate).and_return(2.5)
+      allow(CatarseSettings).to receive(:get_without_cache)
+        .with(:pagarme_minimum_value_for_installment).and_return(1500)
     end
 
-    it 'returns installment_simulations' do
-      get '/v2/billing/installment_simulations', params: params
-      result = Billing::InstallmentSimulator.calculate(params['total_amount_cents']).to_json
+    context 'when the amount is greater than the minimum amount for installation' do
+      it 'returns installment_simulations' do
+        get '/v2/billing/installment_simulations', params: params
+        result = Billing::InstallmentSimulator.new.calculate(params['total_amount_cents']).to_json
 
-      expect(response.body).to eq(result)
+        expect(response.body).to eq(result)
+      end
+    end
+
+    context 'when the amount is less than the minimum amount for installation' do
+      it 'returns installment_simulations' do
+        get '/v2/billing/installment_simulations', params: { 'total_amount_cents' => 1000 }
+        expected_response = { installments_count: 1, total_amount_cents: 1000, installment_amount_cents: 1000 }
+
+        expect(response.body).to eq([expected_response].to_json)
+      end
     end
 
     it 'return status 200 - ok' do
