17b6f4e0090964aa6bb6bb73f109f9b83c276dbd
Gabriel Rocha <37886504+gabrielras@users.noreply.github.com>
2021-05-21 14:41:24 -0200

    ✨ Valida que o usuário do pagamento é dono do cartão de crédito. (#895)

--- services/catarse/spec/queries/billing/credit_cards/safelist_query_spec.rb
+++ services/catarse/spec/queries/billing/credit_cards/safelist_query_spec.rb
@@ -6,20 +6,17 @@ RSpec.describe Billing::CreditCards::SafelistQuery, type: :query do
   subject(:query) { described_class.new }
 
   describe '#call' do
-    let(:paid_credit_card) { create(:billing_credit_card) }
+    let!(:payment) { create(:billing_payment, :paid, :with_credit_card) }
 
     before do
-      create(:billing_payment, :paid, credit_card: paid_credit_card)
-
       not_paid_states = Billing::PaymentStateMachine.states - ['paid']
       not_paid_states.each do |state|
-        credit_card = create(:billing_credit_card)
-        create(:billing_payment, state: state, credit_card: credit_card)
+        create(:billing_payment, :with_credit_card, state: state)
       end
     end
 
     it 'returns credit cards used on paid payments' do
-      expect(query.call).to eq [paid_credit_card]
+      expect(query.call).to eq [payment.credit_card]
     end
   end
 end
