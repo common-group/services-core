efbddd0960129e8fbc5dba943379c8d2c5231055
Gabriel Rocha <gabrielras12@hotmail.com>
2021-05-24 10:52:53 -0300

    ✨ Substitui httpparty pelo faraday.

--- services/catarse/app/clients/pagar_me/client.rb
+++ services/catarse/app/clients/pagar_me/client.rb
@@ -2,30 +2,40 @@
 
 module PagarMe
   class Client
+    require 'faraday'
+    require 'faraday_middleware'
     include HTTParty
 
     base_uri 'https://api.pagar.me/1/'
 
     def find_transaction(transaction_id)
-      response = self.class.get("/transactions/#{transaction_id}", body: { api_key: api_key })
-      response.parsed_response
+      response = connection.post("https://api.pagar.me/1/transactions/#{transaction_id}") do |req|
+        req.body = { api_key: api_key }.to_json
+      end
+
+      response.body
     end
 
     def create_transaction(transaction_params)
       # TODO: handler errors (Timeout, internal server error)
-      response = self.class.post('/transactions', body: transaction_params.merge(api_key: api_key))
-      response.parsed_response
+      response = connection.post('https://api.pagar.me/1/transactions') do |req|
+        req.body = transaction_params.merge(api_key: api_key).to_json
+      end
+
+      response.body
     end
 
     def capture_transaction(transaction_id)
-      response = self.class.post("/transactions/#{transaction_id}/capture", body: { api_key: api_key })
+      response = connection.post("https://api.pagar.me/1/transactions/#{transaction_id}/capture") do |req|
+        req.body = { api_key: api_key }.to_json
+      end
 
       unless response.success?
         error_options = { level: :fatal, extra: { data: response.parsed_response } }
         Sentry.capture_message('Transaction cannot be captured on gateway', error_options)
       end
 
-      response
+      response.body
     end
 
     def refund_transaction(transaction_id)
@@ -45,6 +55,13 @@ module PagarMe
     end
 
     private
+
+    def connection
+      Faraday.new(headers: { 'Content-Type' => 'application/json' }) do |conn|
+        conn.request :json
+        conn.response :json
+      end
+    end
 
     def api_key
       # TODO: Get api key from Vault
