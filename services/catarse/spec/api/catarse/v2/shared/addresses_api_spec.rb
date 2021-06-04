c36be08c282f2cd984d60a26f67dbe67a7a3d9b0
stephannv <3025661+stephannv@users.noreply.github.com>
2021-06-04 10:33:14 -0300

    ✨ Implementa autenticação na nova API

--- services/catarse/spec/api/catarse/v2/shared/addresses_api_spec.rb
+++ services/catarse/spec/api/catarse/v2/shared/addresses_api_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Catarse::V2::Shared::AddressesAPI, type: :api do
+  mock_request_authentication
+
+  include_examples 'authenticate routes'
+
   describe 'POST /v2/shared/addresses' do
     let(:address) { build(:shared_address) }
     let(:address_params) do
@@ -21,13 +25,13 @@ RSpec.describe Catarse::V2::Shared::AddressesAPI, type: :api do
     end
 
     it 'returns created address' do
-      post '/v2/shared/addresses', params: { address: address_params }
+      post '/api/v2/shared/addresses', params: { address: address_params }
 
       expect(response.body).to eq(address.to_json)
     end
 
     it 'return status 201 - created' do
-      post '/v2/shared/addresses', params: { address: address_params }
+      post '/api/v2/shared/addresses', params: { address: address_params }
 
       expect(response).to have_http_status(:created)
     end
