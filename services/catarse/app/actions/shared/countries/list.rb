e1a5fdc7a15c79dbecdd066c8f7f346b01c8ebae
Gabriel Rocha <gabrielras12@hotmail.com>
2021-06-10 10:54:04 -0300

    ✨ Cria rota de listagem de países.

---
+++ services/catarse/app/actions/shared/countries/list.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Shared
+  module Countries
+    class List < Actor
+      output :countries, type: Array
+
+      def call
+        self.countries = ISO3166::Country.translations.map { |attr| { code: attr.first, name: attr.last } }
+      end
+    end
+  end
+end
