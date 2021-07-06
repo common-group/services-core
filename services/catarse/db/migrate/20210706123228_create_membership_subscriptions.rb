3f7df8f19d4fa7ce533e34c7600c1f0327ac80db
stephannv <3025661+stephannv@users.noreply.github.com>
2021-07-06 15:13:37 -0300

    ✨ Cria modelo de assinaturas

---
+++ services/catarse/db/migrate/20210706123228_create_membership_subscriptions.rb
@@ -0,0 +1,20 @@
+class CreateMembershipSubscriptions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :membership_subscriptions, id: :uuid do |t|
+      t.references :project, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :tier, null: false, foreign_key: { to_table: :membership_tiers}, type: :uuid
+      t.references :billing_option, null: false, foreign_key: { to_table: :membership_billing_options }, type: :uuid
+      t.integer :cadence_in_months, null: false
+      t.string :state, null: false
+      t.monetize :amount
+
+      t.timestamps
+    end
+
+    add_index :membership_subscriptions,
+      %i[user_id tier_id],
+      unique: true,
+      name: 'idx_user_tier_uniq'
+  end
+end
