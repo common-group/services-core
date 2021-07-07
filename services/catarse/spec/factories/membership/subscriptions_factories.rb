6414a988ffe45fa1b5cf6ef7f1aacd70e8bb542b
stephannv <3025661+stephannv@users.noreply.github.com>
2021-07-07 09:20:26 -0300

    ✨ Adiciona máquina de estado às assinaturas

--- services/catarse/spec/factories/membership/subscriptions_factories.rb
+++ services/catarse/spec/factories/membership/subscriptions_factories.rb
@@ -2,11 +2,13 @@
 
 FactoryBot.define do
   factory :membership_subscription, class: 'Membership::Subscription' do
+    traits_for_enum :state, Membership::SubscriptionStateMachine.states
+
     association :project
     association :user
     tier { association :membership_tier, project: project }
     billing_option { association :membership_billing_option, tier: tier }
-    state { '#TODO' }
+    state { Membership::SubscriptionStateMachine.states.sample }
     cadence_in_months { 1 }
     amount { Faker::Number.number(digits: 4) }
   end
