4495d78d886a27f681c434eaec9328c6380f25f0
stephannv <3025661+stephannv@users.noreply.github.com>
2021-07-09 09:47:12 -0300

    ✨ Adiciona api de edição/remoção de níveis de apoio

---
+++ services/catarse/app/actions/membership/tiers/update.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Membership
+  module Tiers
+    class Update < Actor
+      input :id, type: String
+      input :attributes, type: Hash
+
+      output :tier, type: Membership::Tier
+
+      def call
+        self.tier = Membership::Tier.find(id)
+
+        tier.update!(attributes)
+      end
+    end
+  end
+end
---
+++ services/catarse/spec/actions/membership/tiers/destroy_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Membership::Tiers::Destroy, type: :action do
+  describe 'Inputs' do
+    subject { described_class.inputs }
+
+    it { is_expected.to include(id: { type: String }) }
+  end
+
+  describe 'Outputs' do
+    subject { described_class.outputs }
+
+    it { is_expected.to be_empty }
+  end
+
+  describe '#call' do
+    subject(:result) { described_class.result(id: tier_id) }
+
+    context 'when tier doesn`t exist' do
+      let(:tier_id) { 'wrong-id' }
+
+      it { expect { result }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+
+    context 'when tier exists' do
+      let(:tier) { create(:membership_tier) }
+      let(:tier_id) { tier.id }
+
+      it { is_expected.to be_success }
+
+      it 'destroys tier' do
+        result
+
+        expect { tier.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+end
