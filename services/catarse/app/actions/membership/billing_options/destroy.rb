23d1365617e1232bb7db290dca0a935f80e53e91
stephannv <3025661+stephannv@users.noreply.github.com>
2021-07-12 15:39:57 -0300

    ✨ Adiciona API de opções de cobrança

---
+++ services/catarse/app/actions/membership/billing_options/destroy.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Membership
+  module BillingOptions
+    class Destroy < Actor
+      input :id, type: String
+
+      def call
+        billing_option = Membership::BillingOption.find(id)
+        billing_option.destroy!
+      end
+    end
+  end
+end
