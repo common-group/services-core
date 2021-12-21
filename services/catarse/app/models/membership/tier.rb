fe2d7b9bd0854d173b275f8f0192d7141d31429f
Gabriel Rocha <gabrielras12@hotmail.com>
2021-12-21 15:13:24 -0300

    ✨ Preenche atributo de ordenação na criação do Tier.

--- services/catarse/app/models/membership/tier.rb
+++ services/catarse/app/models/membership/tier.rb
@@ -15,5 +15,15 @@ module Membership
 
     validates :subscribers_limit, numericality: { greater_than: 0, only_integer: true }, allow_nil: true
     validates :order, numericality: { only_integer: true }
+
+    before_validation :fill_order, on: :create
+
+    private
+
+    def fill_order
+      return if order.present?
+
+      self.order = self.class.where(project_id: project_id).maximum(:order).to_i + 1
+    end
   end
 end
