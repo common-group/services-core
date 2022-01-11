07bea975741962fc533e8e9122185d82f7859c76
Gabriel Rocha <gabrielras12@hotmail.com>
2022-01-11 10:35:42 -0300

    ✨ Adiciona taxa aos boletos de projetos pontuais.

--- services/catarse/catarse.js/legacy/src/c/admin-contribution.js
+++ services/catarse/catarse.js/legacy/src/c/admin-contribution.js
@@ -1,11 +1,17 @@
 import m from 'mithril';
 import h from '../h';
+import _ from 'underscore';
 
 const adminContribution = {
     view: function({attrs}) {
         const contribution = attrs.item;
+        const contributionScope = _.partial(h.i18nScope, 'users.contribution_row');
+
         return m('.w-row.admin-contribution', [
-            m('.fontweight-semibold.lineheight-tighter.u-marginbottom-10.fontsize-small', `R$${contribution.value}`),
+            m('.fontweight-semibold.lineheight-tighter.u-marginbottom-10.fontsize-small', `R$${contribution.value}`,
+            (contribution.payment_method === 'BoletoBancario' ?
+              m('span.fontsize-smallest.fontcolor-secondary',
+                ` (+R$ ${h.formatNumber((contribution.slip_fee || 0), 2, 3)} ${window.I18n.t('slip_fee', contributionScope())} )`) : '')),
             m('.fontsize-smallest.fontcolor-secondary', h.momentify(contribution.created_at, 'DD/MM/YYYY HH:mm[h]')),
             m('.fontsize-smallest', [
                 'ID do Gateway: ',
