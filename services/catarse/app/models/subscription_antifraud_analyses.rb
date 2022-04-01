0b2cd1eef3c1d2c69ba43398b53a679a39208494
Antonio Roberto Silva <forevertonny@gmail.com>
2022-04-01 11:21:28 -0300

    🐞 Ajusta fonte de dados para gerar dados fiscais caso o projeto seja de assinaturas

---
+++ services/catarse/app/models/subscription_antifraud_analyses.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class SubscriptionAntifraudAnalyses < ApplicationRecord
+  include Shared::CommonWrapper
+
+  self.table_name = 'common_schema.antifraud_analyses'
+  self.primary_key = :id
+
+  belongs_to :subscription_payment, primary_key: 'catalog_payment_id'
+end
